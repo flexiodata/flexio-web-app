0231b097f84bd1d87574efe6a53635fa82fb87a3
Ben Williams <ben@flex.io>
2017-02-23 10:30:58 +0100

    put profiling code in separate file

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -49,25 +49,7 @@ $g_store->connection_enckey = '9i$8iw]aKmZzq12r8';
 if (isset($g_config->dir_home))
     $g_store->dir_home = $g_config->dir_home;
 
-
-function shutdown_profiler()
-{
-    $xhprof_data = xhprof_disable();
-
-    //$str = var_export($xhprof_data,true);
-    //file_put_contents('/tmp/xhprof.txt', $str);
-
-    include_once "/usr/share/php/xhprof_lib/utils/xhprof_lib.php";
-    include_once "/usr/share/php/xhprof_lib/utils/xhprof_runs.php";
-    $xhprof_runs = new XHProfRuns_Default();
-    $xhprof_runs->save_run($xhprof_data, "fx");
-}
-
-if (isset($g_config->profiling) && $g_config->profiling && function_exists('xhprof_enable'))
-{
-    xhprof_enable(XHPROF_FLAGS_CPU + XHPROF_FLAGS_MEMORY);
-    register_shutdown_function('shutdown_profiler');
-}
+//require_once __DIR__ . '/profiler.php'; // uncomment this line to enabled profiling
 
 function GET_HTTP_HOST()
 {
