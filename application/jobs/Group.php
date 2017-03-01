7020ab39df4373e085cac53ab1d4d4a931c03076
Aaron Williams <aaron@flex.io>
2017-03-01 10:44:20 -0600

    fixed file rename problem with SortJob.php

--- application/jobs/Group.php
+++ application/jobs/Group.php
@@ -17,7 +17,7 @@ namespace Flexio\Jobs;
 
 require_once __DIR__ . DIRECTORY_SEPARATOR . 'Base.php';
 
-class Group[ extends \Flexio\Jobs\Base
+class Group extends \Flexio\Jobs\Base
 {
     public function run()
     {
