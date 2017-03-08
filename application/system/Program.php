b03a338c0f4a8b873d33f3e0cd293cf97080cbf0
Aaron Williams <aaron@flex.io>
2017-03-08 17:09:45 -0600

    added Program.php for storing functions related to running external programs

---
+++ application/system/Program.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ *
+ * Copyright (c) 2017, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Benjamin I. Williams; David Z. Williams; Aaron L. Williams
+ * Created:  2017-03-08
+ *
+ * @package flexio
+ * @subpackage System
+ */
+
+
+namespace Flexio\System;
+
+
+class Program
+{
+}
+
