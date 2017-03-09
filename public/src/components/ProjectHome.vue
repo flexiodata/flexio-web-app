ee019c8e2dfa71491fd69af5dbbdecf998043f7c
David Z. Williams <dave@flex.io>
2017-03-09 11:55:07 -0500

    Line up nav with filter inputs in project content area.

--- public/src/components/ProjectHome.vue
+++ public/src/components/ProjectHome.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="flex flex-column flex-row-l items-stretch">
     <div class="flex flex-row flex-column-l flex-none items-stretch bb bb-0-l br-l b--black-10 pv0 pv3-l overflow-auto trans-pm">
-      <router-link v-for="item in nav_items" :to="item.route" class="flex-auto flex-none-l no-underline truncate tc tl-l link f5 fw6 black-60 mv0 mv1-l ph-1 pl3-l pr5-l pv2 pv3-l bb bl-l bb-0-l bw2 bw2-l css-nav">
+      <router-link v-for="item in nav_items" :to="item.route" class="flex-auto flex-none-l no-underline truncate tc tl-l link f5 fw6 black-60 mv0 mb1-l ph-1 pl3-l pr5-l pv2 pv3-l bb bl-l bb-0-l bw2 bw2-l css-nav">
         <i class="material-icons md-24 v-mid" :class="item.class">{{item.icon}}</i>
         <span class="dn dib-ns v-mid">{{item.name}}</span>
       </router-link>
