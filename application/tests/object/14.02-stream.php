e6b0e9ee9d7fc9d5cd5cdb4f4bcc25d52b5baac3
Aaron Williams <aaron@flex.io>
2017-03-13 21:19:03 -0500

    added stub test file for stream reader/writer tests

---
+++ application/tests/object/14.02-stream.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ *
+ * Copyright (c) 2016, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2016-04-20
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // SETUP
+        $model = TestUtil::getModel();
+
+
+
+        // TEST: stream read/writer
+    }
+}
