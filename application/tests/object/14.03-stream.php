278f02f9dfb83081ebbc7633a0057a71923d233d
Aaron Williams <aaron@flex.io>
2017-03-14 23:22:44 -0500

    added stub test file for testing stream reader/writer across range of types and values

---
+++ application/tests/object/14.03-stream.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ * Copyright (c) 2016, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2016-04-20
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // SETUP
+        $model = TestUtil::getModel();
+
+
+
+        // TEST: general test across range of types and values
+
+    }
+}
