e58f33ed606c06413fbf7a068c0022db8dbfb278
David Z. Williams <dave@flex.io>
2017-03-22 17:27:16 -0400

    Added output blankslate.

--- public/src/components/PipeTransfer.vue
+++ public/src/components/PipeTransfer.vue
@@ -59,7 +59,7 @@
         </div>
         <div class="mv4">
           <div class="f6 fw6 ttu">Use the REST API</div>
-          <p class="tl lh-copy mid-gray f6">Flex.io pipes can be run and have their results returned via an AJAX call to the a pipe's REST API endpoint. <a href="https://www.flex.io/docs/api/#pipes" class="blue" target="_blank">Learn more...</a></p>
+          <p class="tl lh-copy mid-gray f6">Flex.io pipes can be run and have their results returned via an AJAX call to a pipe's REST API endpoint. <a href="https://www.flex.io/docs/api/#pipes" class="blue" target="_blank">Learn more...</a></p>
           <code class="db tl pre">$.ajax({
   type: 'POST',
   url: 'https://www.flex.io/api/v1/pipes/pipe-alias/run?stream=0',
