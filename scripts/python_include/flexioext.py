9f64f0c0eda8475c91218d17cf1b96fd8de4c829
Ben Williams <ben@flex.io>
2017-03-31 23:06:16 +0200

    support for tables coming together

--- scripts/python_include/flexioext.py
+++ scripts/python_include/flexioext.py
@@ -1,15 +1,16 @@
-import sys as fx_sys
-import json as fx_json
+import sys
+import json
+import csv
 
 fx_header = ''
-for fx_i in range(0,1000):
-    fx_header += fx_sys.stdin.buffer.read(1).decode('utf-8')
+for i in range(0,1000):
+    fx_header += sys.stdin.buffer.read(1).decode('utf-8')
     if fx_header[-4:] == "\r\n\r\n":
         break
 if fx_header[-4:] != "\r\n\r\n":
-    fx_sys.stdout.write('{"content_type":"application/octet-stream"}\r\n\r\nMissing payload header')
-    fx_sys.exit()
-fx_header = fx_json.loads(fx_header)
+    sys.stdout.write('{"content_type":"application/octet-stream"}\r\n\r\nMissing payload header')
+    sys.exit()
+fx_header = json.loads(fx_header)
 
 
 class Input(object):
@@ -22,7 +23,7 @@ class Input(object):
 
     @property
     def stream(self):
-        return fx_sys.stdin
+        return sys.stdin
 
     @property
     def content_type(self):
@@ -32,6 +33,7 @@ class Output(object):
     def __init__(self):
         self.header_written = False
         self._content_type = 'application/octet-stream'
+        self.header = {"content_type": self._content_type}
 
     @property
     def content_type(self):
@@ -40,15 +42,21 @@ class Output(object):
     @content_type.setter
     def content_type(self, value):
         self._content_type = value
+        self.header["content_type"] = self._content_type
 
     @property
     def stream(self):
         if not self.header_written:
-            str = fx_json.dumps({"content_type": self._content_type}) + "\r\n\r\n"
-            fx_sys.stdout.write(str)
-            fx_sys.stdout.flush()
+            str = json.dumps(self.header) + "\r\n\r\n"
+            sys.stdout.write(str)
+            sys.stdout.flush()
             self.header_written = True
-        return fx_sys.stdout
+        return sys.stdout
+
+    def create_table(self, structure):
+        self.content_type = "application/vnd.flexio.table"
+        self.header["structure"] = structure
+        return csv.writer(self.stream)
 
 input = Input()
 output = Output()
