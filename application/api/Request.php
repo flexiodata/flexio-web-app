335b908a6d39d1bb6b57afe3275cd806bcef2eb4
Aaron Williams <aaron@flex.io>
2017-04-11 16:53:51 -0500

    removed validator and api get/set functions from Request class

--- application/api/Request.php
+++ application/api/Request.php
@@ -18,31 +18,15 @@ namespace Flexio\Api;
 
 class Request
 {
-    private $validator;
     private $requesting_user;
-    private $api_version;
-
     public function __construct()
     {
-        $this->validator = \Flexio\Base\Validator::create();
         $this->requesting_user = \Flexio\Object\User::USER_PUBLIC;
-        $this->api_version = 1;
     }
 
     public static function create() : \Flexio\Api\Request
     {
         return (new static);
-    }
-
-    public function setValidator(\Flexio\Base\Validator $validator)
-    {
-        $this->validator = $validator;
-        return $this;
-    }
-
-    public function getValidator() : \Flexio\Base\Validator
-    {
-        return $this->validator;
     }
 
     public function setRequestingUser(string $user) : \Flexio\Api\Request
@@ -54,16 +38,5 @@ class Request
     public function getRequestingUser() : string
     {
         return $this->requesting_user;
-    }
-
-    public function setApiVersion(int $version) : \Flexio\Api\Request
-    {
-        $this->api_version = $version;
-        return $this;
-    }
-
-    public function getApiVersion() : int
-    {
-        return $this->api_version;
     }
 }
