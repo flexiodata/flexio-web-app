4862c0cb7bea3cfb374e925bd91d13c81f21632b
David Z. Williams <dave@flex.io>
2017-04-11 11:23:03 -0400

    Added row cache to StreamGrid2 component.

--- public/src/components/StreamGrid2.vue
+++ public/src/components/StreamGrid2.vue
@@ -24,34 +24,99 @@
     props: ['stream-eid', 'content-url', 'task-json'],
     watch: {
       streamEid() {
-        this.tryInitialFetch()
+        this.tryFetch()
       }
     },
     data() {
       return {
+        inited: false,
         start: 0,
         limit: 100,
+        total_row_count: 0,
         columns: [],
-        rows: []
+        rows: [],
+        cached_rows: {}
       }
     },
     computed: {
       fetch_url() {
-        return this.contentUrl+'?metadata=true&start='+this.start+'&limit=+'+this.limit
+        var url = this.contentUrl+'?start='+this.start+'&limit=+'+this.limit
+        return this.inited ? url : url + '&metadata=true'
       }
     },
     mounted() {
-      this.tryInitialFetch()
+      this.tryFetch()
     },
     methods: {
-      tryInitialFetch() {
+      tryFetch() {
+        if (this.tryRowCache())
+          return
+
         var me = this
         $.getJSON(this.fetch_url).then(response => {
-          me.columns = response.columns
-          me.rows = response.rows
+          me.total_row_count = response.total_count
+
+          // store our column info
+          if (!me.inited && _.isArray(response.columns))
+            me.columns = [].concat(response.columns)
+
+          // store the current set of rows
+          me.rows = [].concat(response.rows)
+
+          // cache the current set of rows
+          var start = me.start
+          var limit = me.limit
+          var row_count = me.total_row_count
+          var temp_cached_rows = {}
+          var idx = 0
+
+          for (var r = start; r < start+limit && r < row_count; ++r)
+          {
+            temp_cached_rows[r] = me.rows[idx]
+            idx++
+          }
+
+          // add the temporary cached rows to our stored cached rows
+          me.cached_rows = _.assign({}, me.cached_rows, temp_cached_rows)
+
+          // set our init flag to true so we don't get columns after this
+          me.inited = true
         }, response => {
 
         })
+      },
+      tryRowCache() {
+        if (!this.inited)
+          return false
+
+        var missing_rows = false
+        var start = this.start
+        var limit = this.limit
+        var row_count = this.total_row_count
+        var temp_rows = []
+
+        for (var r = start; r < start+limit && r < row_count; ++r)
+        {
+          if (this.cached_rows.hasOwnProperty(r))
+          {
+            temp_rows.push(this.cached_rows[r])
+          }
+           else
+          {
+            missing_rows = true
+            break
+          }
+        }
+
+        // let the callee know that we were missing at least one row
+        if (missing_rows)
+          return false
+
+        // update our current set of rows
+        this.rows = [].concat(temp_rows)
+
+        // let the callee know we found all of the rows in the row cache
+        return true
       }
     }
   }
@@ -63,6 +128,7 @@
     font-size: 13px;
     color: #333;
   }
+
   .sg-th,
   .sg-td {
     width: 120px;
