47c74fdb2703a73d803dbc68cc85ba624a4fad33
Aaron Williams <aaron@flex.io>
2017-05-03 17:32:46 -0500

    added scaffolding for acl model tests

---
+++ application/tests/model/04.01-acl.php
@@ -0,0 +1,30 @@
+<?php
+/**
+ *
+ * Copyright (c) 2017, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2017-05-03
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // SETUP
+        $model = TestUtil::getModel();
+
+
+
+        // TODO: add tests for ACL
+    }
+}
