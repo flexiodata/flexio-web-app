9ee4e8a6a9b657439d51c936e0e6e4aa012c5a90
David Z. Williams <dave@flex.io>
2017-05-25 23:33:27 -0400

    Pipe and connection header colors.

--- public/src/components/PipeHeaderItem.vue
+++ public/src/components/PipeHeaderItem.vue
@@ -1,5 +1,5 @@
 <template>
-  <article class="flex flex-row items-center pv3 ph2 bb b--black-10 pointer no-select trans-pm">
+  <article class="flex flex-row items-center pv3 ph2 bb b--black-10 pointer no-select fw6 dark-gray trans-pm">
     <div class="w-two-thirds w-50-ns truncate">
       <div class="mh2">Name</div>
     </div>
