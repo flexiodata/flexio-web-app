3e4a9f22b3e321d74a64f5152961025c3efaac7a
David Z. Williams <dave@flex.io>
2017-05-26 20:51:08 -0400

    Temporariliy added onboarding tile stuff until I can figure out a way to make a consistent mechanism that can be used by both the examples app as well as the main app.

---
+++ public/src/components/HighlightStepList.vue
@@ -0,0 +1,73 @@
+<template>
+  <div class="flex flex-row">
+    <div
+      class="flex flex-row items-center"
+      v-for="(step, index) in steps"
+    >
+      <img :src="getImgSrc(step)" class="db br2" :class="cls" :style="style" v-if="isConnection(step)">
+      <div class="flex flex-row items-center justify-center br2 white" :class="[cls, task_cls, getTaskBgColor(step)]" :style="style" v-else>
+        <i class="db material-icons child" :class="icon_cls">{{getTaskIcon(step)}}</i>
+      </div>
+      <div class="f2 mh1 moon-gray material-icons rotate-270" v-if="index < steps.length - 1">arrow_drop_down</div>
+    </div>
+  </div>
+</template>
+
+<script>
+  import * as tasks from '../constants/task-info'
+  import * as connections from '../constants/connection-info'
+
+  export default {
+    props: {
+      'steps': {
+        type: Array,
+        default: []
+      },
+      'size': {
+        type: String,
+        default: 'medium'
+      }
+    },
+    computed: {
+      cls() {
+        return this.size == 'large' ? 'h3 w3' : ''
+      },
+      task_cls() {
+        return this.size == 'large' ? 'h3 w3' : ''
+      },
+      icon_cls() {
+        return this.size == 'large' ? 'f1' : 'f2'
+      },
+      style() {
+        return this.size == 'large' ? '' : 'height: 3rem; width: 3rem'
+      }
+    },
+    methods: {
+      getConnectionInfo(type) {
+        return _.find(connections, { connection_type: type })
+      },
+      getTaskInfo(type) {
+        return _.find(tasks, { type })
+      },
+      isConnection(type) {
+        var cinfo = this.getConnectionInfo(type)
+        return !_.isNil(cinfo)
+      },
+      getImgSrc(type) {
+        var cinfo = this.getConnectionInfo(type)
+
+        // return connection icon
+        if (!_.isNil(cinfo))
+          return _.get(cinfo, 'icon', '')
+      },
+      getTaskIcon(type) {
+        var tinfo = this.getTaskInfo(type)
+        return _.get(tinfo, 'icon', '')
+      },
+      getTaskBgColor(type) {
+        var tinfo = this.getTaskInfo(type)
+        return _.get(tinfo, 'bg_color', '')
+      }
+    }
+  }
+</script>
---
+++ public/src/components/OnboardingTileItem.vue
@@ -0,0 +1,51 @@
+<template>
+  <article class="flex flex-column justify-between w5a br2 pv4 ph3 ph4-ns ma3 bg-nearer-white ba b--black-10">
+    <div class="tl">
+      <highlight-step-list
+        class="justify-center"
+        size="medium"
+        :steps="highlight_steps"
+      ></highlight-step-list>
+      <div class="mv3 center bb b--black-10"></div>
+      <div class="mt3 mb2 f4">{{title}}</div>
+      <div class="mt3 mb3 lh-copy measure f6 black-70">
+        {{description}}
+      </div>
+    </div>
+    <div class="tc mt2">
+      <router-link
+        class="db center link no-underline tc mw4 f6 f6-ns ttu b br1 white bg-blue darken-10 ph2 ph3-ns pv2"
+        :to="'/'+item.slug"
+      >
+        <span>Use this</span>
+      </router-link>
+    </div>
+  </article>
+</template>
+
+<script>
+  import HighlightStepList from './HighlightStepList.vue'
+
+  export default {
+    props: {
+      'item': {
+        type: Object,
+        required: true
+      }
+    },
+    components: {
+      HighlightStepList
+    },
+    computed: {
+      title() {
+        return _.get(this.item, 'title', '')
+      },
+      description() {
+        return _.get(this.item, 'short_description', '')
+      },
+      highlight_steps() {
+        return _.get(this.item, 'highlight_steps', [])
+      }
+    }
+  }
+</script>
