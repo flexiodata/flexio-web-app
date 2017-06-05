712efa7b55db743989d6b893c49714058168ba74
David Z. Williams <dave@flex.io>
2017-06-05 13:05:01 -0400

    Updating onboarding modal with new examples.

--- public/src/components/OnboardingModal.vue
+++ public/src/components/OnboardingModal.vue
@@ -1,15 +1,24 @@
 <template>
   <ui-modal
     ref="dialog"
-    title=" "
-    dismiss-on="close-button"
     class="ui-modal-onboarding"
+    :remove-header="true"
+    :remove-close-button="true"
+    :dismissible="false"
     @hide="updateUserConfig"
   >
-    <div class="w-100 tc">
-      <div class="f2 mb3" style="margin-top: -1rem">Welcome to Flex.io</div>
-      <div class="mv3 f5 fw6 black-60">Click on one of the example templates to get started building your first pipe.</div>
-      <div class="mv4 center" style="max-width: 1280px">
+    <div class="relative w-100 tc">
+      <div class="absolute top-0 right-0">
+        <ui-icon-button
+          icon="close"
+          type="secondary"
+          @click="close('close')"
+        ></ui-icon-button>
+      </div>
+      <div class="f2 pt4 mbv">Welcome to Flex.io</div>
+      <div class="mt3 f5 fw6 black-60 lh-copy">Below are three simple templates to show how pipes work.</div>
+      <div class="mb3 f5 fw6 black-60 lh-copy">Click on one below to get started, and then you can modify them with your own inputs and commands.</div>
+      <div class="mv3 center" style="max-width: 1280px">
         <div class="flex flex-column flex-row-l">
           <onboarding-tile-item
             v-for="(item, index) in items"
@@ -18,8 +27,8 @@
           ></onboarding-tile-item>
         </div>
       </div>
-      <div class="mv3 fw6 black-60">
-        You can also watch a quick 3-minute getting started video in the <a class="black-60" href="/docs/web-app/#overview" target="_blank">Help Docs</a>.
+      <div class="mv3 f7 fw6 black-60">
+        Prefer to live dangerously? Skip these templates and <a href="#" class="blue" @click.prevent="close('skip')">jump straight into the deep end</a>. Click on the chat at the bottom right to call a lifeguard. :)
       </div>
     </div>
   </ui-modal>
@@ -33,20 +42,20 @@
   import Btn from './Btn.vue'
 
   const items = {
-    'backup-files-from-dropbox-to-amazon-s3': {
-      title: 'Backup Files from Dropbox to Amazon S3',
-      short_description: 'Sometimes it is useful to automatically backup your files from one web-based file storage system (e.g., Dropbox) to another (e.g., Amazon S3). You can quickly set up a pipe to automate this process and schedule your backup as needed.',
-      highlight_steps: [ctypes.CONNECTION_TYPE_DROPBOX, ctypes.CONNECTION_TYPE_AMAZONS3]
+    'transform-an-image-with-python': {
+      title: 'Transform an Image with Python',
+      short_description: 'This template take a web-based image and shrinks an image using python.  Then, it emails the thumbnail as an attachment.',
+      highlight_steps: [ctypes.CONNECTION_TYPE_HTTP, ttypes.TASK_TYPE_EXECUTE, ctypes.CONNECTION_TYPE_EMAIL]
     },
-    'create-a-search-application-for-a-static-website': {
-      title: 'Create a Search Application for a Static Website',
-      short_description: 'Static website (e.g., GitHub Pages) are fantastic; they are simple, fast and secure. However, the one thing you often lose is the ability to generate dynamic content.  With a Flex.io pipe, you can claw back some of those database-like features.  For this example, we created a small application within a blog post that enabled users to quickly search a large CSV file of podcast episode information.',
+    'convert-a-dirty-csv-to-clean-json': {
+      title: 'Convert a Dirty CSV to Clean JSON',
+      short_description: 'This template takes a CSV file and concatenates various columns.  Then, it removes and renames the columns and ultimately converts it to a tidy JSON output.',
+      highlight_steps: [ctypes.CONNECTION_TYPE_HTTP, ttypes.TASK_TYPE_CONVERT, ctypes.CONNECTION_TYPE_EMAIL]
+    },
+    'add-search-to-a-static-website': {
+      title: 'Add Search to a Static Website',
+      short_description: 'This pipe powers a search application on a static site via API.  The search term is inputted on the site and is passed to the pipe as a variable. The pipe then returns a filtered set based on the term that is used by the site to display the result.',
       highlight_steps: [ctypes.CONNECTION_TYPE_HTTP, ttypes.TASK_TYPE_FILTER, ctypes.CONNECTION_TYPE_STDOUT]
-    },
-    'reformat-a-csv-file-to-a-specified-template': {
-      title: 'Reformat a CSV file to a Specified Template',
-      short_description: "Often, when you go to upload a file or merge it into a system, you'll need to use a specified format. In this example, we'll assume our file requires only three fields called: `name`, `address` and `email`.  The pipe will take the full file and create some calculations that concatenate columns.  Finally, it'll remove superfluous columns and rename a field to get everything in the appropriate format.",
-      highlight_steps: [ctypes.CONNECTION_TYPE_HTTP, ttypes.TASK_TYPE_SELECT, ttypes.TASK_TYPE_EMAIL_SEND]
     }
   }
 
@@ -77,9 +86,9 @@
       open() {
         this.$refs['dialog'].open()
       },
-      close() {
+      close(close_type) {
+        analytics.track('Closed Onboarding Modal', { close_type })
         this.$refs['dialog'].close()
-        this.updateUserConfig()
       },
       updateUserConfig() {
         var cfg = _.get(this.getActiveUser(), 'config', {})
@@ -88,8 +97,6 @@
         cfg['app.prompt.tour.shown'] = true
 
         this.$store.dispatch('updateUser', { eid: this.active_user_eid, attrs: { config: cfg } })
-
-        analytics.track('Closed Onboarding Modal')
 
         setTimeout(() => {
           analytics.identify(this.active_user_eid, { closed_onboarding_modal: 1 })
