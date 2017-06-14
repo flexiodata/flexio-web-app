1f25dca67fc0165e367205a5e77f09fcbaa2cf2d
David Z. Williams <dave@flex.io>
2017-06-14 17:12:53 -0400

    Added batch files for easier releases.

---
+++ public/release-minor.bat
@@ -0,0 +1,4 @@
+call npm version minor
+call yarn run build:release
+call erase dist\*.map
+call erase dist\css\*.map
---
+++ public/release-patch.bat
@@ -0,0 +1,4 @@
+call npm version patch
+call yarn run build:release
+call erase dist\*.map
+call erase dist\css\*.map
