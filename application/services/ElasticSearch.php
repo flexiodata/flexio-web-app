07fcfd15f3f313372bc004a0fe72556f96dffd00
Aaron Williams <aaron@flex.io>
2017-06-26 12:42:13 -0500

    added basic connection functionality to ElasticSearch service

--- application/services/ElasticSearch.php
+++ application/services/ElasticSearch.php
@@ -21,6 +21,10 @@ require_once __DIR__ . DIRECTORY_SEPARATOR . 'Abstract.php';
 class ElasticSearch implements \Flexio\Services\IConnection
 {
     private $is_ok = false;
+    private $host = '';
+    private $port = '';
+    private $user = '';
+    private $password = '';
 
 
     ////////////////////////////////////////////////////////////
@@ -41,11 +45,19 @@ class ElasticSearch implements \Flexio\Services\IConnection
     {
         $this->close();
 
-        if (!isset($params['token']))
+        if (isset($params['port']))
+            $params['port'] = (string)$params['port'];
+
+        $validator = \Flexio\Base\Validator::create();
+        if (($params = $validator->check($params, array(
+                'host' => array('type' => 'string', 'required' => true),
+                'port' => array('type' => 'string', 'required' => true),
+                'username' => array('type' => 'string', 'required' => true),
+                'password' => array('type' => 'string', 'required' => true)
+            ))->getParams()) === false)
             return false;
 
-        $apikey = $params['token'];
-        $this->initialize($apikey);
+        $this->initialize($params['host'], intval($params['port']), $params['username'], $params['password']);
         return $this->isOk();
     }
 
@@ -57,6 +69,10 @@ class ElasticSearch implements \Flexio\Services\IConnection
     public function close()
     {
         $this->is_ok = false;
+        $this->host = '';
+        $this->port = '';
+        $this->user = '';
+        $this->password = '';
     }
 
     public function listObjects(string $path = '') : array
@@ -102,9 +118,68 @@ class ElasticSearch implements \Flexio\Services\IConnection
     // additional functions
     ////////////////////////////////////////////////////////////
 
-    private function initialize(string $apikey)
+    private function initialize(string $host, int $port, string $username, string $password) : bool
     {
         $this->close();
+
+        $this->host = $host;
+        $this->port = $port;
+        $this->username = $username;
+        $this->password = $password;
+
+        if ($this->testConnection() === false)
+            return false;
+
         $this->is_ok = true;
+        return $this->is_ok;
+    }
+
+    private function testConnection() : bool
+    {
+        // test the connection
+        try
+        {
+            $url = $this->getHostUrlString();
+            $auth = $this->getBasicAuthString();
+
+            $ch = curl_init();
+
+            curl_setopt($ch, CURLOPT_URL, $url);
+            curl_setopt($ch, CURLOPT_HTTPHEADER, ['Authorization: Basic '. $auth]);
+            curl_setopt($ch, CURLOPT_HTTPGET, true);
+            curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+            curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+            $result = curl_exec($ch);
+            curl_close($ch);
+
+            $result = json_decode($result,true);
+
+            if (!is_array($result))
+                return false;
+            if (!isset($result['version']))
+                return false;
+
+            $version = $result['version'];
+            if (!isset($version['number']))
+                return false;
+
+            // TODO: do we want/need to require a minimum version?
+
+            return true;
+        }
+        catch (\Exception $e)
+        {
+            return false;
+        }
+    }
+
+    private function getHostUrlString() : string
+    {
+        return $this->host . ':' . string($this->port);
+    }
+
+    private function getBasicAuthString() : string
+    {
+        return base64_encode($this->username . ':' . $this->password);
     }
 }
