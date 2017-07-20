8ff666a766620bab3199c4a54746403fb554c681
Aaron Williams <aaron@flex.io>
2017-07-20 13:51:22 -0500

    added basic tests for settype job

---
+++ application/tests/jobs/18.01-settype.php
@@ -0,0 +1,83 @@
+<?php
+/**
+ *
+ * Copyright (c) 2016, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2016-06-07
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // TODO: placeholder job to test basic functionality; fill out tests
+
+        // SETUP
+        $task = json_decode('
+        [
+            {
+                "type": "flexio.create",
+                "params": {
+                    "mime_type": "'.\Flexio\Base\ContentType::MIME_TYPE_FLEXIO_TABLE.'",
+                    "columns": [
+                        { "name": "field1", "type": "character", "width": 6, "scale": 0 },
+                        { "name": "field2", "type": "character", "width": 6, "scale": 2 }
+                    ],
+                    "content": [
+                        ["1","1.2"],
+                        ["2","2.3"],
+                        ["3","3.6"]
+                    ]
+                }
+            },
+            {
+                "type": "flexio.settype",
+                "params": {
+                    "columns": "${columns}",
+                    "type": "${type}",
+                    "scale": "${scale}",
+                    "decimal": "{$decimal}"
+                }
+            }
+        ]
+        ',true);
+
+
+
+        // TEST: SetType Job
+
+        // BEGIN TEST
+        $params = [
+            "columns" => ["field1"],
+            "type" => "numeric",
+            "scale" => 10,
+            "decimal" => 2
+        ];
+        $process = \Flexio\Object\Process::create()->setTask($task)->setParams($params)->run(false);
+        $actual = TestUtil::getProcessSingleOutputRowResult($process);
+        $expected = [["1.00","1.2"],["2.00","2.3"],["3.00","3.4"]];
+        TestCheck::assertString('A.1', 'SetType Job; check basic functionality',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $params = [
+            "columns" => ["field2"],
+            "type" => "numeric",
+            "scale" => 10,
+            "decimal" => 0
+        ];
+        $process = \Flexio\Object\Process::create()->setTask($task)->setParams($params)->run(false);
+        $actual = TestUtil::getProcessSingleOutputRowResult($process);
+        $expected = [["1","1"],["2","2"],["3","3"]];
+        TestCheck::assertString('A.2', 'SetType Job; check basic functionality',  $actual, $expected, $results);
+    }
+}
