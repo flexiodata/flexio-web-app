37b9b6daf4ca9eda8dd3af242491890df3562cf6
Aaron Williams <aaron@flex.io>
2017-09-06 18:59:30 -0500

    added initial HttpRequest job implementation

--- application/jobs/HttpRequest.php
+++ application/jobs/HttpRequest.php
@@ -20,12 +20,90 @@ class HttpRequest extends \Flexio\Jobs\Base
 {
     public function run()
     {
-
-        // TODO: create an HTTP request; pass on the streams
-
+        $job_definition = $this->getProperties();
+        $params = $job_definition['params'];
 
         // pass on the streams
         $this->getOutput()->merge($this->getInput());
+
+        // get the parameters
+        $method = $params['method'] ?? false;
+        $url = $params['url'] ?? false;
+        $headers = $params['headers'] ?? array();
+
+        if ($method === false || $url === false)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::MISSING_PARAMETER);
+
+        $ch = curl_init();
+
+        // configure the URL
+        curl_setopt($ch, CURLOPT_URL, $url);
+
+        // configure the method
+        $method = strtolower($method);
+        switch ($method)
+        {
+            default:
+                throw new \Flexio\Base\Exception(\Flexio\Base\Error::INVALID_PARAMETER);
+
+            case 'head':    curl_setopt($ch, CURLOPT_HTTPHEAD, true); break;
+            case 'get':     curl_setopt($ch, CURLOPT_HTTPGET, true); break;
+            case 'put':     curl_setopt($ch, CURLOPT_HTTPPUT, true); break;
+            case 'post':    curl_setopt($ch, CURLOPT_HTTPPOST, true); break;
+            case 'patch':   curl_setopt($ch, CURLOPT_HTTPPATCH, true); break;
+            case 'delete':  curl_setopt($ch, CURLOPT_HTTPDELETE, true); break;
+            case 'options': curl_setopt($ch, CURLOPT_HTTPOPTIONS, true); break;
+        }
+
+        // TODO: for now, configure some defaults; remove defaults; use info provided in header
+        curl_setopt($ch, CURLOPT_CONNECTTIMEOUT, 30);  // 30 seconds connection timeout
+        curl_setopt($ch, CURLOPT_USERAGENT, 'Flex.io');
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, false);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, false);
+        curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+
+        // make the call and get the result
+        $outstream_properties = array(
+            'name' => $url,
+            'path' => $url,
+            'mime_type' => \Flexio\Base\ContentType::MIME_TYPE_STREAM // default
+        );
+        $outstream = \Flexio\Object\Stream::create($outstream_properties);
+        $this->getOutput()->addStream($outstream);
+
+        // TODO: get header info?
+        //curl_setopt($ch, CURLOPT_HEADERFUNCTION, function ($ch, $data) use (&$streamwriter) {});
+
+        $streamwriter = \Flexio\Object\StreamWriter::create($outstream);
+        curl_setopt($ch, CURLOPT_WRITEFUNCTION, function($ch, $data) use (&$streamwriter) {
+            $length = strlen($data);
+            $streamwriter->write($data);
+            return $length;
+        });
+
+        $result = curl_exec($ch);
+
+        $streamwriter->close();
+        $outstream->setSize($streamwriter->getBytesWritten());
+
+        // TODO: get the mime type from the returned info
+
+        // cleanup
+        if ($result === false)
+        {
+            $http_response_code = curl_getinfo($ch, CURLINFO_HTTP_CODE);
+            curl_close($ch);
+
+            // TODO: a call that fails doesn't mean the request job itself failed;
+            // do we want to do anything other than pass on the info from the write
+            // function?
+        }
+         else
+        {
+            curl_close($ch);
+        }
     }
 
 
@@ -35,7 +113,9 @@ class HttpRequest extends \Flexio\Jobs\Base
     {
         "type": "flexio.httprequest",
         "params": {
-            "value": 1
+            "method": "head|get|put|post|patch|delete|options",
+            "url": "https://www.flex.io",
+            "headers: []
         }
     }
 EOD;
---
+++ application/tests/jobs/16.01-httprequest.php
@@ -0,0 +1,51 @@
+<?php
+/**
+ *
+ * Copyright (c) 2016, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2016-06-07
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // TODO: placeholder job to test basic functionality; fill out tests
+
+        // SETUP
+        $task = json_decode('
+        [
+            {
+                "type": "flexio.httprequest",
+                "params": {
+                    "method": "get",
+                    "url": "https://raw.githubusercontent.com/flexiodata/examples/master/functions/hello-world.py"
+                }
+            }
+        ]
+        ',true);
+
+
+
+        // TEST: HttpRequest Job
+
+        // BEGIN TEST
+        $params = [
+        ];
+        $process = \Flexio\Object\Process::create()->setTask($task)->setParams($params)->run(false);
+        $result = TestUtil::getProcessResult($process,0,34);
+        $actual = is_array($result) && isset($result[0]) ? $result[0] : '';
+        $expected = 'def flexio_handler(input, output):';
+        TestCheck::assertString('A.1', 'HttpRequest; check basic functionality',  $actual, $expected, $results);
+    }
+}
