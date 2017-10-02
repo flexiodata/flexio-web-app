e3ce21a4b25dcf05c2a3648a9330103be011e827
Ben Williams <ben@flex.io>
2017-10-02 22:48:43 +0200

    process run api now outputs the stdout stream contents

--- application/api/Process.php
+++ application/api/Process.php
@@ -316,51 +316,30 @@ class Process
 
          // run the process and get the output
         $process->run($background);
-        $output_streams = $process->getOutput()->getStreams();
 
-        // if a stream wasn't specified, echo the last stream; otherwise, try to
-        // return the requested stream
-        if ($stream_to_echo === false)
+        $stream = $process->getStdout();
+        $stream_info = $stream->get();
+        if ($stream_info === false)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::READ_FAILED);
+
+        $mime_type = $stream_info['mime_type'];
+        $start = 0;
+        $limit = PHP_INT_MAX;
+        $content = $stream->content($start, $limit);
+
+        if ($mime_type !== \Flexio\Base\ContentType::MIME_TYPE_FLEXIO_TABLE)
         {
-            $last_stream_idx = count($output_streams) - 1;
-            if ($last_stream_idx >= 0)
-                $stream_to_echo = $output_streams[$last_stream_idx]->getName();
-                 else
-                exit(0); // nothing was requested, but nothing to return
+            // return content as-is
+            header('Content-Type: ' . $mime_type);
+        }
+        else
+        {
+            // flexio table; return application/json in place of internal mime
+            header('Content-Type: ' . \Flexio\Base\ContentType::MIME_TYPE_JSON);
+            $content = json_encode($content);
         }
 
-        for ($i = 0; $i < count($output_streams); ++$i)
-        {
-            if ($output_streams[$i]->getName() == $stream_to_echo || (is_numeric($stream_to_echo) && $i == $stream_to_echo))
-            {
-                $stream = $output_streams[$i];
-                $stream_info = $stream->get();
-                if ($stream_info === false)
-                    throw new \Flexio\Base\Exception(\Flexio\Base\Error::READ_FAILED);
-
-                $mime_type = $stream_info['mime_type'];
-                $start = 0;
-                $limit = PHP_INT_MAX;
-                $content = $stream->content($start, $limit);
-
-                if ($mime_type !== \Flexio\Base\ContentType::MIME_TYPE_FLEXIO_TABLE)
-                {
-                    // return content as-is
-                    header('Content-Type: ' . $mime_type);
-                }
-                else
-                {
-                    // flexio table; return application/json in place of internal mime
-                    header('Content-Type: ' . \Flexio\Base\ContentType::MIME_TYPE_JSON);
-                    $content = json_encode($content);
-                }
-
-                echo($content);
-                exit(0);
-            }
-        }
-
-        header('HTTP/1.0 404 Not Found');
+        echo($content);
         exit(0);
     }
 
