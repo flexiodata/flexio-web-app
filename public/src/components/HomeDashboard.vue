70a494a92fe09b786aadaf37b26776b68339a97b
David Z. Williams <dave@flex.io>
2017-10-03 21:52:02 -0400

    Working on layout for dashboard.

--- public/src/components/HomeDashboard.vue
+++ public/src/components/HomeDashboard.vue
@@ -1,10 +1,16 @@
 <template>
   <div class="overflow-y-auto bg-nearer-white">
     <div class="ma4">
-      <help-items
-        :items="['quick-start', 'api-docs', 'templates', 'help']"
-        help-message="I need help getting started with Flex.io..."
-      ></help-items>
+      <div class="f2 mb2">Hey, Dave!</div>
+      <div class="f3">Welcome to the Flex.io Dashboard.</div>
+      <div class="mt4">
+        <div class="dib nl2">
+          <help-items
+            help-message="I need help getting started with Flex.io..."
+            :items="['quick-start', 'api-docs', 'templates', 'help']"
+          ></help-items>
+        </div>
+      </div>
     </div>
     <div class="ma4 mt0 bg-white css-dashboard-box">
       <chart-process-stats title="Pipes Run (Last 30 days)"></chart-process-stats>
