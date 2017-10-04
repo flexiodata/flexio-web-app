d689cf1cbef6c78864c27a7e0ff7c219639b977e
Aaron Williams <aaron@flex.io>
2017-10-04 14:58:00 -0500

    copy connection info in tbl_connection into a connection_info column

--- scripts/update/update1054_alterconnectiontable.php
+++ scripts/update/update1054_alterconnectiontable.php
@@ -50,10 +50,36 @@ try
 {
     // STEP 1: rename the token_expires column
     $sql = <<<EOT
-        ALTER TABLE tbl_acl RENAME COLUMN token_expires TO expires;
+        ALTER TABLE tbl_connection RENAME COLUMN token_expires TO expires;
 EOT;
     $db->exec($sql);
 
+
+    // STEP 2: add the new connection info column
+    $sql = <<<EOT
+        ALTER TABLE tbl_connection ADD connection_info TEXT DEFAULT '';
+EOT;
+$db->exec($sql);
+
+
+    // STEP 3: copy the existing connection info to the new connection_info column
+    copyConnectionInfo($db);
+
+
+    // STEP 4: drop the existing connection_info columns
+/*
+    $sql = <<<EOT
+        ALTER TABLE tbl_connection
+            DROP COLUMN host,
+            DROP COLUMN port,
+            DROP COLUMN username,
+            DROP COLUMN password,
+            DROP COLUMN token,
+            DROP COLUMN refresh_token,
+            DROP COLUMN database;
+EOT;
+$db->exec($sql);
+*/
 }
 catch(\Exception $e)
 {
@@ -68,3 +94,47 @@ $current_version = \Flexio\System\System::getUpdateVersionFromFilename(__FILE__)
 
 echo '{ "success": true, "msg": "Operation completed successfully." }';
 
+
+function copyConnectionInfo($db)
+{
+    // STEP 1: get a list of the existing connection info
+    $query_sql = "select
+                      eid,
+                      host,
+                      port,
+                      username,
+                      password,
+                      token,
+                      refresh_token,
+                      database
+                  from tbl_connection
+                ";
+    $result = $db->query($query_sql);
+
+    // STEP 2: for each of the connection info items, save the connection
+    // info in the connection_info column
+    $objects = array();
+    while ($result && ($row = $result->fetch()))
+    {
+        // unencrypt the parameters
+        $connection_info = array();
+        $connection_info['host'] = $row['host'];
+        $connection_info['port'] = $row['port'];
+        $connection_info['username'] = \Flexio\Base\Util::decrypt($row['username'], $GLOBALS['g_store']->connection_enckey);
+        $connection_info['password'] = \Flexio\Base\Util::decrypt($row['password'], $GLOBALS['g_store']->connection_enckey);
+        $connection_info['token'] = \Flexio\Base\Util::decrypt($row['token'], $GLOBALS['g_store']->connection_enckey);
+        $connection_info['refresh_token'] = \Flexio\Base\Util::decrypt($row['refresh_token'], $GLOBALS['g_store']->connection_enckey);
+        $connection_info['database'] = $row['database'];
+
+        // stringify and encrypt the connection info
+        $connection_str = json_encode($connection_info);
+        $connection_str_encrypted = \Flexio\Base\Util::encrypt($connection_str, $GLOBALS['g_store']->connection_enckey);
+
+        // set the connection_info
+        $update = array();
+        $update['connection_info'] = $connection_str_encrypted;
+        $update['created'] = $row['created'];
+        $update['updated'] = $row['updated'];
+        $db->update('tbl_connection', $update, 'eid = ' . $db->quote($eid));
+    }
+}
