2872b42e97d6557ecc6605c0879116a7b493100d
David Z. Williams <dave@flex.io>
2017-10-04 11:07:00 -0400

    Task usage list tweaks.

--- public/src/components/AdminTaskSummary.vue
+++ public/src/components/AdminTaskSummary.vue
@@ -1,35 +1,35 @@
 <template>
-  <div class="dib" v-if="store_stats.length == 0">
-    <div class="flex flex-row justify-center items-center min-h3 pa3">
+  <div v-if="is_fetching">
+    <div class="flex flex-row justify-center items-center min-h4 pa3">
       <spinner size="medium"></spinner>
       <span class="ml2 f5">Loading...</span>
     </div>
   </div>
   <div class="pa3" v-else>
-    <h2 class="ma0">Task usage</h2>
-    <div class="mt2 mb3 pv2 bt b--black-10">
+    <div class="f3 ma0 pb2 mb3" v-if="title.length > 0">{{title}}</div>
+    <div>
       <table class="w-100">
         <thead>
           <tr>
-            <th class="pa2 tl">Task Type</th>
-            <th class="pa2 tr">Avg. Time</th>
-            <th class="pa2 tr w-10">Failed</th>
-            <th class="pa2 tr w-10">Completed</th>
-            <th class="pa2 tr w-10">Total</th>
+            <th class="fw6 pa2 tl">Task Type</th>
+            <th class="fw6 pa2 tr">Avg. Time</th>
+            <th class="fw6 pa2 tr w-10">Failed</th>
+            <th class="fw6 pa2 tr w-10">Completed</th>
+            <th class="fw6 pa2 tr w-10">Total</th>
           </tr>
         </thead>
         <tbody>
           <tr
             v-for="(item, index) in store_stats"
-            :class="index % 2 ? 'bg-near-white' : ''"
+            :class="index % 2 ? 'bg-nearer-white' : ''"
             item="item"
             index="index"
           >
-            <td class="pa2 tl">{{item.task_type}}</td>
-            <td class="pa2 tr">{{ getAvgTime(item.average_time) }}</td>
-            <td class="pa2 tr w-10 dark-red">{{item.failed}}</td>
-            <td class="pa2 tr w-10 dark-green">{{item.completed}}</td>
-            <td class="pa2 tr w-10">{{item.total_count}}</td>
+            <td class="bt b--light-gray pa2 tl">{{item.task_type}}</td>
+            <td class="bt b--light-gray pa2 tr">{{ getAvgTime(item.average_time) }}</td>
+            <td class="bt b--light-gray pa2 tr w-10 dark-red">{{item.failed}}</td>
+            <td class="bt b--light-gray pa2 tr w-10 dark-green">{{item.completed}}</td>
+            <td class="bt b--light-gray pa2 tr w-10">{{item.total_count}}</td>
           </tr>
         </tbody>
       </table>
@@ -38,21 +38,37 @@
 </template>
 
 <script>
+  import { mapState } from 'vuex'
   import Spinner from 'vue-simple-spinner'
 
   export default {
+    props: {
+      'title': {
+        type: String,
+        default: 'Task usage'
+      }
+    },
     components: {
       Spinner
     },
     computed: {
+      // mix this into the outer object with the object spread operator
+      ...mapState({
+        'is_fetching': 'stats_processes_fetching',
+        'is_fetched': 'stats_processes_fetched'
+      }),
       store_stats() {
         return _.get(this.$store, 'state.statistics.tasks', [])
       }
     },
-    mounted() {
-      this.$store.dispatch('fetchStatistics', { type: 'tasks' })
+    created() {
+      this.tryFetchStats()
     },
     methods: {
+      tryFetchStats() {
+        if (!this.is_fetched)
+          this.$store.dispatch('fetchStatistics', { type: 'tasks' })
+      },
       getAvgTime(v) {
         return _.isString(v) ? parseFloat(v).toFixed(4) : '--'
       }
