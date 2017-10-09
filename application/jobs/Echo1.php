ee63b292d842308f4724f1bc4e27910683d469a4
Ben Williams <ben@flex.io>
2017-10-10 00:02:38 +0200

    added echo job

---
+++ application/jobs/Echo1.php
@@ -0,0 +1,67 @@
+<?php
+/**
+ *
+ * Copyright (c) 2016, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Benjamin I. Williams
+ * Created:  2017-10-09
+ *
+ * @package flexio
+ * @subpackage Jobs
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Jobs;
+
+
+class Echo1 extends \Flexio\Jobs\Base
+{
+    public function run(\Flexio\Object\Context &$context)
+    {
+        // process stdin
+        $stdin = $context->getStdin();
+        $stdout = $stdin->copy()->setPath(\Flexio\Base\Util::generateHandle());
+        $context->setStdout($stdout);
+
+        $job_definition = $this->getProperties();
+        $msg = $job_definition['params']['msg'] ?? '';
+
+        $streamwriter = \Flexio\Object\StreamWriter::create($stdout);
+        $stdout->setMimeType(\Flexio\Base\ContentType::MIME_TYPE_TXT);
+        $streamwriter->write($msg);
+    }
+
+
+    // job definition info
+    const MIME_TYPE = 'flexio.echo';
+    const TEMPLATE = <<<EOD
+    {
+        "type": "flexio.echo",
+        "params": {
+            "path": ""
+        }
+    }
+EOD;
+    // direction is "asc" or "desc"
+    const SCHEMA = <<<EOD
+    {
+        "type": "object",
+        "required": ["type","params"],
+        "properties": {
+            "type": {
+                "type": "string",
+                "enum": ["flexio.echo"]
+            },
+            "params": {
+                "type": "object",
+                "required": ["value"],
+                "properties": {
+
+                }
+            }
+        }
+    }
+EOD;
+}
