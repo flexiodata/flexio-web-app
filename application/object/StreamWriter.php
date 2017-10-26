4cf0f5385ade1107c1eb81bb7831e6fd167db805
Aaron Williams <aaron@flex.io>
2017-10-26 17:18:51 -0500

    added require directives

--- application/object/StreamWriter.php
+++ application/object/StreamWriter.php
@@ -16,6 +16,8 @@ declare(strict_types=1);
 namespace Flexio\Object;
 
 
+require_once dirname(__DIR__) . '/services/Abstract.php';
+
 class StreamWriter implements \Flexio\Object\IStreamWriter
 {
     private $writer = false;
