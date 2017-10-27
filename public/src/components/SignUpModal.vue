965d53ad38236413ca8b3e7478db8d90d8733a39
David Z. Williams <dave@flex.io>
2017-10-27 16:56:01 -0400

    Added sign up modal success.

--- public/src/components/SignUpModal.vue
+++ public/src/components/SignUpModal.vue
@@ -10,24 +10,7 @@
   >
     <div class="pointer f3 lh-solid b child black-30 hover-black-60 mt2 mr3 absolute top-0 right-0" @click="$emit('cancel')">&times;</div>
     <div class="pv3 ph2">
-      <sign-up-form
-        @sign-in-click="view = 'signin'"
-        @signed-up="$emit('signed-up')"
-        @signed-in="$emit('signed-up-signed-in')"
-        v-if="view === 'signup'"
-      />
-      <sign-in-form
-        @sign-up-click="view = 'signup'"
-        @forgot-password-click="view = 'forgotpassword'"
-        @signed-in="$emit('signed-in')"
-        v-else-if="view === 'signin'"
-      />
-      <forgot-password-form
-        @sign-up-click="view = 'signup'"
-        @sign-in-click="view = 'signin'"
-        @requested-password="$emit('requested-password')"
-        v-else-if="view === 'forgotpassword'"
-      />
+      <sign-up-modal-success />
     </div>
   </flexio-modal>
 </template>
@@ -37,6 +20,7 @@
   import SignUpForm from './SignUpForm.vue'
   import SignInForm from './SignInForm.vue'
   import ForgotPasswordForm from './ForgotPasswordForm.vue'
+  import SignUpModalSuccess from './SignUpModalSuccess.vue'
 
   export default {
     props: {
@@ -49,11 +33,18 @@
       FlexioModal,
       SignUpForm,
       SignInForm,
-      ForgotPasswordForm
+      ForgotPasswordForm,
+      SignUpModalSuccess
     },
     data() {
       return {
         view: this.initialView
+      }
+    },
+    methods: {
+      onSignedUp() {
+        this.$emit('signed-up-signed-in')
+        this.view = 'sign-up-success'
       }
     }
   }
