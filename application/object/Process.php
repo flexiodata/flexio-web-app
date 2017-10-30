51be6f67f8d07f3535e2d17524db1ec62475b5c9
Aaron Williams <aaron@flex.io>
2017-10-30 11:35:56 -0500

    simplified job type lookup

--- application/object/Process.php
+++ application/object/Process.php
@@ -879,32 +879,25 @@ class Process extends \Flexio\Object\Base
         // make sure the job is registered; note: this isn't strictly necessary,
         // but gives us a convenient way of limiting what jobs are available for
         // processing
-        $full_class_name = false;
+        $job_class_name = false;
         $manifest = self::manifest();
-        foreach ($manifest as $m)
-        {
-            if ($m['type'] !== $job_type)
-                continue;
+        $job_class_name = $manifest[$job_type] ?? false;
 
-            $full_class_name = $m['class'];
-            break;
-        }
-
-        if ($full_class_name === false)
+        if ($job_class_name === false)
             throw new \Flexio\Base\Exception(\Flexio\Base\Error::CREATE_FAILED);
 
         // try to find the job file
-        $class_name_parts = explode("\\", $full_class_name);
+        $class_name_parts = explode("\\", $job_class_name);
         if (!isset($class_name_parts[3]))
             throw new \Flexio\Base\Exception(\Flexio\Base\Error::CREATE_FAILED);
 
-        $class_file = \Flexio\System\System::getApplicationDirectory() . DIRECTORY_SEPARATOR . 'jobs' . DIRECTORY_SEPARATOR . $class_name_parts[3] . '.php';
-        if (!@file_exists($class_file))
+        $job_class_file = \Flexio\System\System::getApplicationDirectory() . DIRECTORY_SEPARATOR . 'jobs' . DIRECTORY_SEPARATOR . $class_name_parts[3] . '.php';
+        if (!@file_exists($job_class_file))
             throw new \Flexio\Base\Exception(\Flexio\Base\Error::CREATE_FAILED);
 
         // load the job's php file and instantiate the job object
-        include_once $class_file;
-        $job = $full_class_name::create($task);
+        include_once $job_class_file;
+        $job = $job_class_name::create($task);
 
         if ($job === false)
             throw new \Flexio\Base\Exception(\Flexio\Base\Error::CREATE_FAILED);
@@ -954,37 +947,37 @@ class Process extends \Flexio\Object\Base
     private static function manifest() : array
     {
         $manifest = array(
-            array('type' => \Flexio\Jobs\CalcField::MIME_TYPE, 'class' => '\Flexio\Jobs\CalcField'),
-            array('type' => \Flexio\Jobs\Comment::MIME_TYPE,   'class' => '\Flexio\Jobs\Comment'),
-            array('type' => \Flexio\Jobs\Convert::MIME_TYPE,   'class' => '\Flexio\Jobs\Convert'),
-            array('type' => \Flexio\Jobs\Create::MIME_TYPE,    'class' => '\Flexio\Jobs\Create'),
-            array('type' => \Flexio\Jobs\Distinct::MIME_TYPE,  'class' => '\Flexio\Jobs\Distinct'),
-            array('type' => \Flexio\Jobs\Duplicate::MIME_TYPE, 'class' => '\Flexio\Jobs\Duplicate'),
-            array('type' => \Flexio\Jobs\Echo1::MIME_TYPE,     'class' => '\Flexio\Jobs\Echo1'),
-            array('type' => \Flexio\Jobs\EmailSend::MIME_TYPE, 'class' => '\Flexio\Jobs\EmailSend'),
-            array('type' => \Flexio\Jobs\Request::MIME_TYPE,   'class' => '\Flexio\Jobs\Request'),
-            array('type' => \Flexio\Jobs\Output::MIME_TYPE,    'class' => '\Flexio\Jobs\Output'),
-            array('type' => \Flexio\Jobs\Filter::MIME_TYPE,    'class' => '\Flexio\Jobs\Filter'),
-            array('type' => \Flexio\Jobs\Replace::MIME_TYPE,   'class' => '\Flexio\Jobs\Replace'),
-            array('type' => \Flexio\Jobs\Grep::MIME_TYPE,      'class' => '\Flexio\Jobs\Grep'),
-            array('type' => \Flexio\Jobs\Group::MIME_TYPE,     'class' => '\Flexio\Jobs\Group'),
-            array('type' => \Flexio\Jobs\Input::MIME_TYPE,     'class' => '\Flexio\Jobs\Input'),
-            array('type' => \Flexio\Jobs\Limit::MIME_TYPE,     'class' => '\Flexio\Jobs\Limit'),
-            array('type' => \Flexio\Jobs\Merge::MIME_TYPE,     'class' => '\Flexio\Jobs\Merge'),
-            array('type' => \Flexio\Jobs\Nop::MIME_TYPE,       'class' => '\Flexio\Jobs\Nop'),
-            array('type' => \Flexio\Jobs\Fail::MIME_TYPE,      'class' => '\Flexio\Jobs\Fail'),
-            array('type' => \Flexio\Jobs\Prompt::MIME_TYPE,    'class' => '\Flexio\Jobs\Prompt'),
-            array('type' => \Flexio\Jobs\Execute::MIME_TYPE,   'class' => '\Flexio\Jobs\Execute'),
-            array('type' => \Flexio\Jobs\Exit1::MIME_TYPE,     'class' => '\Flexio\Jobs\Exit1'),
-            array('type' => \Flexio\Jobs\Rename::MIME_TYPE,    'class' => '\Flexio\Jobs\Rename'),
-            array('type' => \Flexio\Jobs\Render::MIME_TYPE,    'class' => '\Flexio\Jobs\Render'),
-            array('type' => \Flexio\Jobs\Search::MIME_TYPE,    'class' => '\Flexio\Jobs\Search'),
-            array('type' => \Flexio\Jobs\Select::MIME_TYPE,    'class' => '\Flexio\Jobs\Select'),
-            array('type' => \Flexio\Jobs\SetType::MIME_TYPE,   'class' => '\Flexio\Jobs\SetType'),
-            array('type' => \Flexio\Jobs\Sleep::MIME_TYPE,     'class' => '\Flexio\Jobs\Sleep'),
-            array('type' => \Flexio\Jobs\Sort::MIME_TYPE,      'class' => '\Flexio\Jobs\Sort'),
-            array('type' => \Flexio\Jobs\Transform::MIME_TYPE, 'class' => '\Flexio\Jobs\Transform'),
-            array('type' => \Flexio\Jobs\List1::MIME_TYPE,     'class' => '\Flexio\Jobs\List1' )
+            \Flexio\Jobs\CalcField::MIME_TYPE => '\Flexio\Jobs\CalcField',
+            \Flexio\Jobs\Comment::MIME_TYPE   => '\Flexio\Jobs\Comment',
+            \Flexio\Jobs\Convert::MIME_TYPE   => '\Flexio\Jobs\Convert',
+            \Flexio\Jobs\Create::MIME_TYPE    => '\Flexio\Jobs\Create',
+            \Flexio\Jobs\Distinct::MIME_TYPE  => '\Flexio\Jobs\Distinct',
+            \Flexio\Jobs\Duplicate::MIME_TYPE => '\Flexio\Jobs\Duplicate',
+            \Flexio\Jobs\Echo1::MIME_TYPE     => '\Flexio\Jobs\Echo1',
+            \Flexio\Jobs\EmailSend::MIME_TYPE => '\Flexio\Jobs\EmailSend',
+            \Flexio\Jobs\Request::MIME_TYPE   => '\Flexio\Jobs\Request',
+            \Flexio\Jobs\Output::MIME_TYPE    => '\Flexio\Jobs\Output',
+            \Flexio\Jobs\Filter::MIME_TYPE    => '\Flexio\Jobs\Filter',
+            \Flexio\Jobs\Replace::MIME_TYPE   => '\Flexio\Jobs\Replace',
+            \Flexio\Jobs\Grep::MIME_TYPE      => '\Flexio\Jobs\Grep',
+            \Flexio\Jobs\Group::MIME_TYPE     => '\Flexio\Jobs\Group',
+            \Flexio\Jobs\Input::MIME_TYPE     => '\Flexio\Jobs\Input',
+            \Flexio\Jobs\Limit::MIME_TYPE     => '\Flexio\Jobs\Limit',
+            \Flexio\Jobs\Merge::MIME_TYPE     => '\Flexio\Jobs\Merge',
+            \Flexio\Jobs\Nop::MIME_TYPE       => '\Flexio\Jobs\Nop',
+            \Flexio\Jobs\Fail::MIME_TYPE      => '\Flexio\Jobs\Fail',
+            \Flexio\Jobs\Prompt::MIME_TYPE    => '\Flexio\Jobs\Prompt',
+            \Flexio\Jobs\Execute::MIME_TYPE   => '\Flexio\Jobs\Execute',
+            \Flexio\Jobs\Exit1::MIME_TYPE     => '\Flexio\Jobs\Exit1',
+            \Flexio\Jobs\Rename::MIME_TYPE    => '\Flexio\Jobs\Rename',
+            \Flexio\Jobs\Render::MIME_TYPE    => '\Flexio\Jobs\Render',
+            \Flexio\Jobs\Search::MIME_TYPE    => '\Flexio\Jobs\Search',
+            \Flexio\Jobs\Select::MIME_TYPE    => '\Flexio\Jobs\Select',
+            \Flexio\Jobs\SetType::MIME_TYPE   => '\Flexio\Jobs\SetType',
+            \Flexio\Jobs\Sleep::MIME_TYPE     => '\Flexio\Jobs\Sleep',
+            \Flexio\Jobs\Sort::MIME_TYPE      => '\Flexio\Jobs\Sort',
+            \Flexio\Jobs\Transform::MIME_TYPE => '\Flexio\Jobs\Transform',
+            \Flexio\Jobs\List1::MIME_TYPE     => '\Flexio\Jobs\List1'
         );
 
         return $manifest;
