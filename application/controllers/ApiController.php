697736764c163538878cd799950031b5d49ba674
Aaron Williams <aaron@flex.io>
2017-11-02 11:03:13 -0500

    in the api, if the content that's posted has an 'application/json' content type but isn't JSON, make sure to respond with a correctly formatted api error response

--- application/controllers/ApiController.php
+++ application/controllers/ApiController.php
@@ -95,51 +95,75 @@ class ApiController extends \Flexio\System\FxControllerAction
         // is for calls that 'want' the json payload as their body, such as /pipe/:eid/run and
         // /process/:eid/run
 
-        if ($method != 'GET' && ini_get('enable_post_data_reading') == '1')
+
+        try
         {
-            $all_headers = getallheaders();
-
-            // TODO: currently, in some configurations (e.g. the test site but not localhost),
-            // two content-type headers are being returned; one of them is null, and the
-            // other has the correct value; for now, check both, but we should find out
-            // why two are being returned, get it so that only one is being returned, and
-            // then convert all keys to lowercase and do a lookup on the lowercase (headers
-            // are case insensitive, so there's no guarantee of case in the key, but to
-            // convert everything to lowercase, we have to make sure there's only one so
-            // we don't overwrite a good value for a given key with a bad one)
-            foreach ($all_headers as $k => $v)
+            if ($method != 'GET' && ini_get('enable_post_data_reading') == '1')
             {
-                $k = strtolower($k);
+                $all_headers = getallheaders();
 
-                // only check for the existence of 'application/json' here instead of doing a
-                // straight up string comparison -- the reason for this is that there are cases
-                // where the the content type looks like this: "application/json;charset=UTF-8"
-                if (strcasecmp($k, 'content-type') == 0 && strpos($v, 'application/json') !== false)
+                // TODO: currently, in some configurations (e.g. the test site but not localhost),
+                // two content-type headers are being returned; one of them is null, and the
+                // other has the correct value; for now, check both, but we should find out
+                // why two are being returned, get it so that only one is being returned, and
+                // then convert all keys to lowercase and do a lookup on the lowercase (headers
+                // are case insensitive, so there's no guarantee of case in the key, but to
+                // convert everything to lowercase, we have to make sure there's only one so
+                // we don't overwrite a good value for a given key with a bad one)
+                foreach ($all_headers as $k => $v)
                 {
-                    $input = file_get_contents('php://input');
-                    if (strlen($input) > 0)
+                    $k = strtolower($k);
+
+                    // only check for the existence of 'application/json' here instead of doing a
+                    // straight up string comparison -- the reason for this is that there are cases
+                    // where the the content type looks like this: "application/json;charset=UTF-8"
+                    if (strcasecmp($k, 'content-type') == 0 && strpos($v, 'application/json') !== false)
                     {
-                        $urlencoding_test = substr($input, 0, 3);
-                        if ($urlencoding_test == '%7B' || $urlencoding_test == '%7b')
-                            $input = rawurldecode($input);
-
-                        $obj = @json_decode($input, true);
-
-                        if ($obj === null)
+                        $input = file_get_contents('php://input');
+                        if (strlen($input) > 0)
                         {
-                            header('HTTP/1.1 500 Internal Server Error (JSON parse error)');
-                            exit(0);
+                            $urlencoding_test = substr($input, 0, 3);
+                            if ($urlencoding_test == '%7B' || $urlencoding_test == '%7b')
+                                $input = rawurldecode($input);
+
+                            $obj = @json_decode($input, true);
+
+                            // note: input might not be parsable if the content type was specified
+                            // as 'application/json' but the data that was posted is something else;
+                            // in this case, throw a normal invalid parameter response
+                            if ($obj === null)
+                                throw new \Flexio\Base\Exception(\Flexio\Base\Error::INVALID_PARAMETER);
+
+                            $post_params = $obj; // if we're posting JSON, this will take the place of the post params
                         }
 
-                        $post_params = $obj; // if we're posting JSON, this will take the place of the post params
+                        break;
                     }
-
-                    break;
                 }
             }
-        }
 
-        // process the request
-        \Flexio\Api\Api::request($request, $query_params, $post_params);
+            // process the request
+            \Flexio\Api\Api::request($request, $query_params, $post_params);
+        }
+        catch (\Flexio\Base\Exception $e)
+        {
+            $info = $e->getMessage(); // exception info is packaged up in message
+            $info = json_decode($info,true);
+
+            $error = array();
+            $error['code'] = $info['code'];
+            $error['message'] = $info['message'];
+
+            if (IS_DEBUG() !== false)
+            {
+                $error['type'] = 'flexio exception';
+                $error['file'] = $e->getFile();
+                $error['line'] = $e->getLine();
+                $error['debug_message'] = $e->getMessage();
+                $error['trace'] = $e->getTrace();
+            }
+
+            \Flexio\Api\Response::sendError($error);
+        }
     }
 }
