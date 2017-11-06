323092253361acfa54bfc885e197872c75b11962
Ben Williams <ben@flex.io>
2017-11-06 16:33:17 +0100

    fxrender run.js support for pdf and paper size

--- scripts/docker/fxrender/run.js
+++ scripts/docker/fxrender/run.js
@@ -3,7 +3,8 @@ const commandLineArgs = require('command-line-args')
 
 const options = commandLineArgs([
    { name: 'url' },
-   { name: 'format' },
+   { name: 'format', defaultValue: "png" },
+   { name: 'paper', defaultValue: "Letter" },
    { name: 'width' },
    { name: 'height' },
    { name: 'scrollbars' },
@@ -12,15 +13,23 @@ const options = commandLineArgs([
 
 
 (async () => {
+  var output_filename = '/tmp/output.' + options.format;
+
+
   const browser = await puppeteer.launch({args:['--no-sandbox']});
   const page = await browser.newPage();
   await page.goto(options.url);
-  await page.screenshot({path: '/tmp/output.png'});
+
+  if (options.format == 'pdf') {
+    await page.pdf({path: output_filename, format: options.paper})
+  } else {
+    await page.screenshot({path: output_filename});
+  }
 
   await browser.close();
 
 
-  var rs = require('fs').createReadStream('/tmp/output.png');
+  var rs = require('fs').createReadStream(output_filename);
   rs.pipe(process.stdout);
   
 })();
