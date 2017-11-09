172b9419316b920877a301cd3ac0249539b23b1c
Aaron Williams <aaron@flex.io>
2017-11-09 15:30:57 -0600

    added Execute job test for javascript

--- application/tests/jobs/19.01-execute.php
+++ application/tests/jobs/19.01-execute.php
@@ -23,44 +23,57 @@ class Test
         // TODO: placeholder job to test basic functionality; fill out tests
 
         // SETUP
+        $create = json_decode('{
+            "type": "flexio.create",
+            "params": {
+                "name": "table",
+                "mime_type": "'.\Flexio\Base\ContentType::MIME_TYPE_FLEXIO_TABLE.'",
+                "columns": [
+                    { "name": "c1", "type": "character", "width": 3 }
+                ],
+                "content": [
+                    {"c1" : "a"},
+                    {"c1" : "b"}
+                ]
+            }
+        }',true);
 
+
+        // TEST: Basic python execute job
         $script = <<<EOD
 def flexio_handler(input,output):
     output.content_type = "text/plain"
-    output.write("Hello, world.")
+    output.write("Hello, World!")
 EOD;
-        $task = json_decode('
-        [
-            {
-                "type": "flexio.create",
-                "params": {
-                    "name": "table",
-                    "mime_type": "'.\Flexio\Base\ContentType::MIME_TYPE_FLEXIO_TABLE.'",
-                    "columns": [
-                        { "name": "c1", "type": "character", "width": 3 }
-                    ],
-                    "content": [
-                        {"c1" : "a"},
-                        {"c1" : "b"}
-                    ]
-                }
-            },
-            {
-                "type": "flexio.execute",
-                "params": {
-                    "lang": "python",
-                    "code": "'.base64_encode($script).'"
-                }
+        $task = array($create, json_decode('{
+            "type": "flexio.execute",
+            "params": {
+                "lang": "python",
+                "code": "'.base64_encode($script).'"
             }
-        ]
-        ',true);
-
-
-
-        // TEST: Execute Job
+        }',true));
         $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
         $actual = $process->getStdout()->content(0,50);
-        $expected = 'Hello, world.';
+        $expected = 'Hello, World!';
         TestCheck::assertString('A.1', 'Execute Job; check basic functionality',  $actual, $expected, $results);
+
+        // TEST: Basic javascript execute job
+        $script = <<<EOD
+exports.flexio_handler = function(input, output) {
+    output.content_type = "text/plain"
+    output.write('Hello, World!')
+}
+EOD;
+        $task = array($create, json_decode('{
+            "type": "flexio.execute",
+            "params": {
+                "lang": "javascript",
+                "code": "'.base64_encode($script).'"
+            }
+        }',true));
+        $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
+        $actual = $process->getStdout()->content(0,50);
+        $expected = 'Hello, World!';
+        TestCheck::assertString('A.2', 'Execute Job; check basic functionality',  $actual, $expected, $results);
     }
 }
