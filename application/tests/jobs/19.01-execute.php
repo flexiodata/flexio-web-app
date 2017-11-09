d6923b395223175c82fae67edc1a5044674415b2
Aaron Williams <aaron@flex.io>
2017-11-09 16:21:46 -0600

    added tests for integrity checks in the Execute job

--- application/tests/jobs/19.01-execute.php
+++ application/tests/jobs/19.01-execute.php
@@ -39,7 +39,10 @@ class Test
         }',true);
 
 
-        // TEST: Basic python execute job
+
+        // TEST: Base Execute Job Tests
+
+        // BEGIN TEST
         $script = <<<EOD
 def flexio_handler(input,output):
     output.content_type = "text/plain"
@@ -55,9 +58,9 @@ EOD;
         $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
         $actual = $process->getStdout()->content(0,50);
         $expected = 'Hello, World!';
-        TestCheck::assertString('A.1', 'Execute Job; check basic functionality',  $actual, $expected, $results);
+        TestCheck::assertString('A.1', 'Execute Job; check basic python execution functionality',  $actual, $expected, $results);
 
-        // TEST: Basic javascript execute job
+        // BEGIN TEST
         $script = <<<EOD
 exports.flexio_handler = function(input, output) {
     output.content_type = "text/plain"
@@ -74,6 +77,153 @@ EOD;
         $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
         $actual = $process->getStdout()->content(0,50);
         $expected = 'Hello, World!';
-        TestCheck::assertString('A.2', 'Execute Job; check basic functionality',  $actual, $expected, $results);
+        TestCheck::assertString('A.2', 'Execute Job; check basic javascript execution functionality',  $actual, $expected, $results);
+
+
+
+        // TEST: Base Execute Job Code SHA256 Integrity Check
+
+        // BEGIN TEST
+        $script = "exports.flexio_handler=function(input,output){output.content_type='text/plain';output.write('Hello,World!');}";
+        $task = array($create, json_decode('{
+            "type": "flexio.execute",
+            "params": {
+                "lang": "javascript",
+                "code": "'.base64_encode($script).'",
+                "integrity": "sha256-9beee4ffd5906d126ac456b262c5f6fac718062bab269886149f73773a31d9b7"
+            }
+        }',true));
+        $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
+        $actual = $process->getStdout()->content(0,50);
+        $expected = 'Hello,World!';
+        TestCheck::assertString('B.1', 'Execute Job; sha256 integrity check',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $script = "exports.flexio_handler=function(input,output){output.content_type='text/plain';output.write('Hello,World!');}";
+        $task = array($create, json_decode('{
+            "type": "flexio.execute",
+            "params": {
+                "lang": "javascript",
+                "code": "'.base64_encode($script).'",
+                "integrity": "sha256-9BEEE4FFD5906D126AC456B262C5F6FAC718062BAB269886149F73773A31D9B7"
+            }
+        }',true));
+        $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
+        $actual = $process->getStdout()->content(0,50);
+        $expected = 'Hello,World!';
+        TestCheck::assertString('B.2', 'Execute Job; sha256 integrity check with uppercase sha256 code',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $script = "exports.flexio_handler=function(input,output){output.content_type='text/plain';output.write('Hello,You!');}";
+        $task = array($create, json_decode('{
+            "type": "flexio.execute",
+            "params": {
+                "lang": "javascript",
+                "code": "'.base64_encode($script).'",
+                "integrity": "sha256-9beee4ffd5906d126ac456b262c5f6fac718062bab269886149f73773a31d9b7"
+            }
+        }',true));
+        $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
+        $actual = $process->getProcessStatus();
+        $expected = \Model::PROCESS_STATUS_FAILED;
+        TestCheck::assertString('B.3', 'Execute Job; check code integrity; sha256 integrity failure',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $script = "exports.flexio_handler=function(input,output){output.content_type='text/plain';output.write('Hello,World!');}";
+        $task = array($create, json_decode('{
+            "type": "flexio.execute",
+            "params": {
+                "lang": "python",
+                "code": "'.base64_encode($script).'",
+                "integrity": "sha512-9beee4ffd5906d126ac456b262c5f6fac718062bab269886149f73773a31d9b7"
+            }
+        }',true));
+        $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
+        $actual = $process->getProcessStatus();
+        $expected = \Model::PROCESS_STATUS_FAILED;
+        TestCheck::assertString('B.4', 'Execute Job; check code integrity; sha256 format (sha512 indicated) integrity failure',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $script = "exports.flexio_handler=function(input,output){output.content_type='text/plain';output.write('Hello,World!');}";
+        $task = array($create, json_decode('{
+            "type": "flexio.execute",
+            "params": {
+                "lang": "python",
+                "code": "'.base64_encode($script).'",
+                "integrity": "md5-a223b94fdf072f085adf67a4310abb59"
+            }
+        }',true));
+        $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
+        $actual = $process->getProcessStatus();
+        $expected = \Model::PROCESS_STATUS_FAILED;
+        TestCheck::assertString('B.5', 'Execute Job; check code integrity; md5 integrity failure; md5 not supported',  $actual, $expected, $results);
+
+
+
+        // TEST: Base Execute Job Code SHA384 Integrity Check
+
+        // BEGIN TEST
+        $script = "exports.flexio_handler=function(input,output){output.content_type='text/plain';output.write('Hello,World!');}";
+        $task = array($create, json_decode('{
+            "type": "flexio.execute",
+            "params": {
+                "lang": "javascript",
+                "code": "'.base64_encode($script).'",
+                "integrity": "sha384-621a93ffd4d3b57d8e786d6dab8ce59ef6e9a187a19c0b64a71d749370fed32e54ddbacd2bcdd931b6db525d883fc9b6"
+            }
+        }',true));
+        $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
+        $actual = $process->getStdout()->content(0,50);
+        $expected = 'Hello,World!';
+        TestCheck::assertString('C.1', 'Execute Job; sha384 integrity check',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $script = "exports.flexio_handler=function(input,output){output.content_type='text/plain';output.write('Hello,You!');}";
+        $task = array($create, json_decode('{
+            "type": "flexio.execute",
+            "params": {
+                "lang": "javascript",
+                "code": "'.base64_encode($script).'",
+                "integrity": "sha384-621a93ffd4d3b57d8e786d6dab8ce59ef6e9a187a19c0b64a71d749370fed32e54ddbacd2bcdd931b6db525d883fc9b6"
+            }
+        }',true));
+        $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
+        $actual = $process->getProcessStatus();
+        $expected = \Model::PROCESS_STATUS_FAILED;
+        TestCheck::assertString('C.2', 'Execute Job; check code integrity; sha384 integrity failure',  $actual, $expected, $results);
+
+
+
+        // TEST: Base Execute Job Code SHA512 Integrity Check
+
+        // BEGIN TEST
+        $script = "exports.flexio_handler=function(input,output){output.content_type='text/plain';output.write('Hello,World!');}";
+        $task = array($create, json_decode('{
+            "type": "flexio.execute",
+            "params": {
+                "lang": "javascript",
+                "code": "'.base64_encode($script).'",
+                "integrity": "sha512-8febacfe8bb5bbd5c821da18dfd915d48911af97ab98f83770cd201a6da385a444017fffdc5c183194fb8406b3ad1ffba5a25b44077d4b80629b96e9c77a37ed"
+            }
+        }',true));
+        $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
+        $actual = $process->getStdout()->content(0,50);
+        $expected = 'Hello,World!';
+        TestCheck::assertString('D.1', 'Execute Job; sha512 integrity check',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $script = "exports.flexio_handler=function(input,output){output.content_type='text/plain';output.write('Hello,You!');}";
+        $task = array($create, json_decode('{
+            "type": "flexio.execute",
+            "params": {
+                "lang": "javascript",
+                "code": "'.base64_encode($script).'",
+                "integrity": "sha512-8febacfe8bb5bbd5c821da18dfd915d48911af97ab98f83770cd201a6da385a444017fffdc5c183194fb8406b3ad1ffba5a25b44077d4b80629b96e9c77a37ed"
+            }
+        }',true));
+        $process = \Flexio\Object\Process::create()->setTask($task)->run(false);
+        $actual = $process->getProcessStatus();
+        $expected = \Model::PROCESS_STATUS_FAILED;
+        TestCheck::assertString('D.2', 'Execute Job; check code integrity; sha512 integrity failure',  $actual, $expected, $results);
     }
 }
