dcf240d3496849f9b6400df7d4106a478e408fbc
Aaron Williams <aaron@flex.io>
2017-11-13 14:07:33 -0600

    reorganized some code

--- application/api/Admin.php
+++ application/api/Admin.php
@@ -18,38 +18,6 @@ namespace Flexio\Api;
 
 class Admin
 {
-    public static function getConfiguration(\Flexio\Api\Request $request) : array
-    {
-        $params = $request->getQueryParams();
-        $requesting_user_eid = $request->getRequestingUser();
-
-        // only allow users from flex.io to get this info
-        $user = \Flexio\Object\User::load($requesting_user_eid);
-        if ($user === false)
-            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INSUFFICIENT_RIGHTS);
-
-        if ($user->isAdministrator() !== true)
-            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INSUFFICIENT_RIGHTS);
-
-        return self::checkServerSettings();
-    }
-
-    public static function getProcessTaskStats(\Flexio\Api\Request $request) : array
-    {
-        $params = $request->getQueryParams();
-        $requesting_user_eid = $request->getRequestingUser();
-
-        // only allow users from flex.io to get this info
-        $user = \Flexio\Object\User::load($requesting_user_eid);
-        if ($user === false)
-            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INSUFFICIENT_RIGHTS);
-
-        if ($user->isAdministrator() !== true)
-            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INSUFFICIENT_RIGHTS);
-
-        return \Flexio\System\System::getModel()->process->getProcessTaskStats();
-    }
-
     public static function getProcessUserStats(\Flexio\Api\Request $request) : array
     {
         $params = $request->getQueryParams();
@@ -145,6 +113,38 @@ class Admin
         }
 
         return $result;
+    }
+
+    public static function getProcessTaskStats(\Flexio\Api\Request $request) : array
+    {
+        $params = $request->getQueryParams();
+        $requesting_user_eid = $request->getRequestingUser();
+
+        // only allow users from flex.io to get this info
+        $user = \Flexio\Object\User::load($requesting_user_eid);
+        if ($user === false)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INSUFFICIENT_RIGHTS);
+
+        if ($user->isAdministrator() !== true)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INSUFFICIENT_RIGHTS);
+
+        return \Flexio\System\System::getModel()->process->getProcessTaskStats();
+    }
+
+    public static function getConfiguration(\Flexio\Api\Request $request) : array
+    {
+        $params = $request->getQueryParams();
+        $requesting_user_eid = $request->getRequestingUser();
+
+        // only allow users from flex.io to get this info
+        $user = \Flexio\Object\User::load($requesting_user_eid);
+        if ($user === false)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INSUFFICIENT_RIGHTS);
+
+        if ($user->isAdministrator() !== true)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INSUFFICIENT_RIGHTS);
+
+        return self::checkServerSettings();
     }
 
     private static function checkServerSettings() : array
