f0beb0517e75e53d8446ed09933f62ad44f35b97
David Z. Williams <dave@flex.io>
2017-11-16 15:24:28 -0500

    Updated autofill buttons for new commands.

--- public/src/constants/pipe-autofill.js
+++ public/src/constants/pipe-autofill.js
@@ -1,10 +1,9 @@
 import {
-  TASK_TYPE_INPUT,
-  TASK_TYPE_OUTPUT,
+  TASK_TYPE_REQUEST,
   TASK_TYPE_CONVERT,
   TASK_TYPE_EMAIL_SEND,
   TASK_TYPE_EXECUTE,
-  TASK_TYPE_COMMENT
+  TASK_TYPE_RENDER
 } from '../constants/task-type'
 
 import {
@@ -14,32 +13,54 @@ import {
 
 export default [
   {
-    label: 'Add Input',
+    label: 'Request from Web',
     task_json: {
-      'type': TASK_TYPE_INPUT,
+      'type': TASK_TYPE_REQUEST,
       'params': {
+        'method': 'GET',
+        'url': 'https://static.pexels.com/photos/51387/mount-everest-himalayas-nuptse-lhotse-51387.jpeg'
       }
     }
   },{
-    label: 'Add Output',
+    label: 'Render a webpage',
     task_json: {
-      'type': TASK_TYPE_OUTPUT,
+      'type': TASK_TYPE_RENDER,
       'params': {
+        'url': 'https://www.flex.io',
+        'format': 'png',
+        'width': '800',
+        'height': '600',
+        'scrollbars': false
       }
     }
   },{
-    label: 'Input from Web',
+    label: 'Execute Python Code',
     task_json: {
-      'type': TASK_TYPE_INPUT,
+      'type': TASK_TYPE_EXECUTE,
       'params': {
-        'items': [
-          {
-            'path': 'https://static.pexels.com/photos/51387/mount-everest-himalayas-nuptse-lhotse-51387.jpeg'
-          }
-        ]
-      },
-      'metadata': {
-        'connection_type': CONNECTION_TYPE_HTTP
+        'lang': 'python',
+        // "Hello, World!" example code
+        'code': 'ZGVmIGZsZXhpb19oYW5kbGVyKGNvbnRleHQpOg0KICAgIGNvbnRleHQub3V0cHV0LmNvbnRlbnRfdHlwZSA9ICJ0ZXh0L3BsYWluIg0KICAgIGNvbnRleHQub3V0cHV0LndyaXRlKCdIZWxsbyBXb3JsZCEnKQ=='
+      }
+    }
+  },{
+    label: 'Execute Javascript Code',
+    task_json: {
+      'type': TASK_TYPE_EXECUTE,
+      'params': {
+        'lang': 'javascript',
+        // "Hello, World!" example code
+        'code': 'ZXhwb3J0cy5mbGV4aW9faGFuZGxlciA9IGZ1bmN0aW9uKGNvbnRleHQpIHsNCiAgICBjb250ZXh0Lm91dHB1dC5jb250ZW50X3R5cGUgPSAidGV4dC9wbGFpbiINCiAgICBjb250ZXh0Lm91dHB1dC53cml0ZSgnSGVsbG8sIFdvcmxkIScpDQp9'
+      }
+    }
+  },{
+    label: 'Send Email',
+    task_json: {
+      'type': TASK_TYPE_EMAIL_SEND,
+      'params': {
+        'to': ['john.smith@example.com'],
+        'subject': 'My Subject',
+        'body_text': 'This is a test of Flex.io'
       }
     }
   },{
@@ -55,26 +76,6 @@ export default [
         'output': {
           'format': 'json'
         }
-      }
-    }
-  },{
-    label: 'Execute Python Code',
-    task_json: {
-      'type': TASK_TYPE_EXECUTE,
-      'params': {
-        'lang': 'python',
-        // "Hello, World!" example code
-        'code': 'ZGVmIGZsZXhpb19oYW5kbGVyKGlucHV0LCBvdXRwdXQpOgogICAgd3JpdGVyID0gb3V0cHV0LmNyZWF0ZShuYW1lPSdIZWxsbycpCiAgICBpZiAnbWVzc2FnZScgaW4gaW5wdXQuZW52OgogICAgICAgIHdyaXRlci53cml0ZShpbnB1dC5lbnZbJ21lc3NhZ2UnXSkKICAgIGVsc2U6ICAKICAgICAgICB3cml0ZXIud3JpdGUoJ0hlbGxvLCBXb3JsZCEnKQ=='
-      }
-    }
-  },{
-    label: 'Send Email',
-    task_json: {
-      'type': TASK_TYPE_EMAIL_SEND,
-      'params': {
-        'to': ['john.smith@example.com'],
-        'subject': 'My Subject',
-        'body_text': 'This is a test of Flex.io'
       }
     }
   }
