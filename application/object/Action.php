dbfc9158c5c74232e1e7940307f1626e39c64cd8
Aaron Williams <aaron@flex.io>
2017-11-16 17:05:49 -0600

    use appropriate return transfer in action identify request

--- application/object/Action.php
+++ application/object/Action.php
@@ -101,7 +101,7 @@ class Action
         curl_setopt($ch, CURLOPT_POSTFIELDS, $post_data);
         curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, false);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
         curl_setopt($ch, CURLOPT_HTTP_VERSION, CURL_HTTP_VERSION_1_1);
 
         $result = curl_exec($ch);
