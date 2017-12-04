ea943becb59b4e42f3b45a9c22005c597cf15857
Ben Williams <ben@flex.io>
2017-12-04 14:12:01 +0100

    handle insertion of nulls

--- application/services/StorageFs.php
+++ application/services/StorageFs.php
@@ -276,12 +276,19 @@ class StorageFileReaderWriter implements \Flexio\Base\IStreamReader, \Flexio\Bas
             $row = array_values($row);
             foreach ($row as &$val)
             {
-                $val = (string)$val;
-                if (strpos($val, "'") !== false)
+                if (is_null($val))
                 {
-                    $val = str_replace("'", "''", $val);
+                    $val = 'null';
                 }
-                $val = "'$val'";
+                 else
+                {
+                    $val = (string)$val;
+                    if (strpos($val, "'") !== false)
+                    {
+                        $val = str_replace("'", "''", $val);
+                    }
+                    $val = "'$val'";
+                }
             }
             
             if ($first)
