2641ec394b223b2dd028464317b9741a793b0930
David Z. Williams <dave@flex.io>
2017-12-13 14:29:00 -0500

    Added VfsResource.

--- public/src/api/resources.js
+++ public/src/api/resources.js
@@ -13,48 +13,20 @@ var SUFFIX = '{/p1}{/p2}{/p3}{/p4}'
 // api root
 export const API_ROOT = '/api/v1'
 
-// auth
-export const LoginResource = Vue.resource(API_ROOT+'/login')
-export const LogoutResource = Vue.resource(API_ROOT+'/logout')
-export const SignupResource = Vue.resource(API_ROOT+'/signup'+SUFFIX)
-
-// validation
-export const ValidateResource = Vue.resource(API_ROOT+'/validate'+SUFFIX)
-
-// rights
-export const RightsResource = Vue.resource(API_ROOT+'/rights{/eid}'+SUFFIX)
-
-// help
-export const HelpResource = Vue.resource(API_ROOT+'/help/conversation')
-
-// user
-export const UserResource = Vue.resource(API_ROOT+'/users{/eid}'+SUFFIX)
-//export const UserActivateResource = Vue.resource(API_ROOT+'/users/activate')
-//export const UserSendVerificationResource = Vue.resource(API_ROOT+'/users/resendverify')
-
-// project
-export const ProjectResource = Vue.resource(API_ROOT+'/projects{/eid}'+SUFFIX)
-
-// connection
+export const LoginResource      = Vue.resource(API_ROOT+'/login')
+export const LogoutResource     = Vue.resource(API_ROOT+'/logout')
+export const SignupResource     = Vue.resource(API_ROOT+'/signup'+SUFFIX)
+export const ValidateResource   = Vue.resource(API_ROOT+'/validate'+SUFFIX)
+export const RightsResource     = Vue.resource(API_ROOT+'/rights{/eid}'+SUFFIX)
+export const HelpResource       = Vue.resource(API_ROOT+'/help/conversation')
+export const UserResource       = Vue.resource(API_ROOT+'/users{/eid}'+SUFFIX)
+export const ProjectResource    = Vue.resource(API_ROOT+'/projects{/eid}'+SUFFIX)
 export const ConnectionResource = Vue.resource(API_ROOT+'/connections{/eid}'+SUFFIX)
-
-// pipe
-export const PipeResource = Vue.resource(API_ROOT+'/pipes{/eid}'+SUFFIX)
-
-// process
-export const ProcessResource = Vue.resource(API_ROOT+'/processes{/eid}'+SUFFIX)
-
-// admin
-export const AdminResource = Vue.resource(API_ROOT+'/admin/statistics{/type}')
-
-// statistics
+export const PipeResource       = Vue.resource(API_ROOT+'/pipes{/eid}'+SUFFIX)
+export const ProcessResource    = Vue.resource(API_ROOT+'/processes{/eid}'+SUFFIX)
+export const AdminResource      = Vue.resource(API_ROOT+'/admin/statistics{/type}')
 export const StatisticsResource = Vue.resource(API_ROOT+'/statistics{/type}')
-
-// stream
-export const StreamResource = Vue.resource(API_ROOT+'/streams{/eid}')
-
-// test
-export const TestResource = Vue.resource(API_ROOT+'/tests'+SUFFIX)
-
-// trash
-export const TrashResource = Vue.resource(API_ROOT+'/trash'+SUFFIX)
+export const StreamResource     = Vue.resource(API_ROOT+'/streams{/eid}')
+export const TestResource       = Vue.resource(API_ROOT+'/tests'+SUFFIX)
+export const TrashResource      = Vue.resource(API_ROOT+'/trash'+SUFFIX)
+export const VfsResource        = Vue.resource(API_ROOT+'/vfs{/action}'+SUFFIX)
