a5c7ce41e9dd5ac341c13501397daedb1f3d4501
Ben Williams <ben@flex.io>
2018-01-08 19:33:19 +0100

    json merging working

--- application/jobs/Merge.php
+++ application/jobs/Merge.php
@@ -37,9 +37,9 @@ class Merge extends \Flexio\Jobs\Base
         $input = $process->getStreams();
 
         $merge_mode = 'table';
-        foreach ($input as $instream)
+        foreach ($input as $stream)
         {
-            if ($instream->getMimeType() === \Flexio\Base\ContentType::FLEXIO_TABLE)
+            if ($stream->getMimeType() === \Flexio\Base\ContentType::FLEXIO_TABLE)
                 continue;
 
             $merge_mode = 'stream';
@@ -84,8 +84,22 @@ class Merge extends \Flexio\Jobs\Base
 
 
 
-        $merge_mode = 'stream';
-        
+        $merge_mode = 'table';
+
+        foreach ($streams as $stream)
+        {
+            $mime_type = $stream->getMimeType();
+            if ($merge_mode == 'table' && $mime_type == \Flexio\Base\ContentType::JSON)
+                $merge_mode = 'json';
+
+            if ($mime_type != \Flexio\Base\ContentType::JSON && $mime_type != \Flexio\Base\ContentType::FLEXIO_TABLE)
+            {
+                $merge_mode = 'stream';
+                break;
+            }
+        }
+
+
         switch ($merge_mode)
         {
             default:
@@ -112,9 +126,9 @@ class Merge extends \Flexio\Jobs\Base
 
         // write to the output
         $streamwriter = $outstream->getWriter();
-        foreach ($streams as $instream)
+        foreach ($streams as $stream)
         {
-            $streamreader = $instream->getReader();
+            $streamreader = $stream->getReader();
             while (true)
             {
                 $data = $streamreader->read();
@@ -128,8 +142,81 @@ class Merge extends \Flexio\Jobs\Base
         $outstream->setSize($streamwriter->getBytesWritten());
     }
 
-    private function mergeJson(\Flexio\IFace\IProcess $process)
+    private function mergeJson(array $streams, \Flexio\Iface\IStream $outstream)
     {
+        // set the default output mime type; for now, use text, but should be
+        // based on content
+        $outstream_properties = [
+            'mime_type' => \Flexio\Base\ContentType::JSON
+        ];
+        
+        $outstream->set($outstream_properties);
+
+        $result = null;
+
+        // write to the output
+        $streamwriter = $outstream->getWriter();
+        foreach ($streams as $stream)
+        {
+            $json = null;
+
+            $content_type = $stream->getMimeType();
+            if ($content_type == \Flexio\Base\ContentType::FLEXIO_TABLE)
+            {
+                $json = [];
+                $reader = $stream->getReader();
+                while (($row = $reader->readRow()) !== false)
+                    $json[] = $row;
+            }
+             else if ($content_type == \Flexio\Base\ContentType::JSON)
+            {
+                $data = '';
+                $reader = $stream->getReader();
+                while (($chunk = $reader->read(16384)) !== false)
+                    $data .= $chunk;
+                
+                $json = @json_decode($data);
+            }
+
+            if (is_null($json))
+            {
+                throw new \Flexio\Base\Exception(\Flexio\Base\Error::GENERAL, "JSON decoding error");
+            }
+
+            if ($result === null)
+            {
+                $result = $json;
+            }
+             else
+            {
+                if (is_object($result) && is_object($json))
+                {
+                    $result = (object)array_merge((array)$result, (array)$json);
+                }
+                 else if (is_array($result) || is_array($json))
+                {
+                    if (is_object($result))
+                        $result = (array)$result;
+                    if (is_object($json))
+                        $json = (array)$json;
+                    if (!is_array($result))
+                        $json = [ $result ];
+                    if (!is_array($json))
+                        $json = [ $json ];
+                    $result = array_merge($result, $json);
+                }
+                 else
+                {
+                    $result .= $json;
+                }
+            }        
+        }
+
+        if ($result !== null)
+        {
+            $outstream->getWriter()->write(json_encode($result));
+            $outstream->setSize($streamwriter->getBytesWritten());
+        }
     }
     
     private function mergeTables(\Flexio\IFace\IProcess $process)
@@ -156,9 +243,9 @@ class Merge extends \Flexio\Jobs\Base
             $row_template[$s['name']] = null;
 
         // insert the rows from each of the streams
-        foreach ($input as $instream)
+        foreach ($input as $stream)
         {
-            $streamreader = $instream->getReader();
+            $streamreader = $stream->getReader();
             while (true)
             {
                 $row = $streamreader->readRow();
