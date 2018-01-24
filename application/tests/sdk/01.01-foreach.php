1802e7c03b68747be1ad834e2fd87d8b3181e157
Aaron Williams <aaron@flex.io>
2018-01-24 17:33:19 -0600

    added scaffolding for 'foreach' SDK test

---
+++ application/tests/sdk/01.01-foreach.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * Copyright (c) 2018, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2018-01-24
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // TEST: SDK template tests
+
+        // BEGIN TEST
+        $script = TestUtil::getTestSDKSetup() . <<<EOD
+EOD;
+        $actual = TestUtil::execSDKJS($script);
+        $expected = "";
+        TestCheck::assertString('A.1', 'SDK; check template functionality',  $actual, $expected, $results);
+    }
+}
+
