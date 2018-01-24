c5b0b1808c264b4a7c091fd65bbcb0fcda378d34
Aaron Williams <aaron@flex.io>
2018-01-24 14:59:49 -0600

    added some SDK tests

--- application/tests/sdkjs/01.01-basic.php
+++ application/tests/sdkjs/01.01-basic.php
@@ -20,6 +20,9 @@ class Test
 {
     public function run(&$results)
     {
+        // TEST: basic SDK tests
+
+        // BEGIN TEST
         $script = TestUtil::getTestSDKSetup() . <<<EOD
 Flexio.pipe()
 .echo("Hello, World.")
@@ -30,6 +33,35 @@ EOD;
         $actual = TestUtil::execSDKJS($script);
         $expected = "Hello, World.\n";
         TestCheck::assertString('A.1', 'SDK; check basic functionality',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $script = TestUtil::getTestSDKSetup() . <<<EOD
+Flexio.pipe()
+.request('https://git.io/vFBSw') // short url for names-and-ip-addresses.csv
+.convert('csv', 'table')         // convert to a table to apply a filter
+.filter('id = "1"')              // filter for a particular row
+.run(function(err, response) {
+    console.log(response.text)
+})
+EOD;
+        $actual = TestUtil::execSDKJS($script);
+        $expected = "[{\"id\":\"1\",\"first_name\":\"Kimbra\",\"last_name\":\"Pettigrew\",\"email\":\"kpettigrew0@dedecms.com\",\"gender\":\"Female\",\"ip_address\":\"253.123.30.10\"}]\n";
+        TestCheck::assertString('A.2', 'SDK; check basic functionality',  $actual, $expected, $results);
+
+
+        // BEGIN TEST
+        $script = TestUtil::getTestSDKSetup() . <<<EOD
+Flexio.pipe()
+.javascript(function(context) {
+    context.output.write('Hello ' + context.query.name + '!')
+})
+.run({ query: { name: 'World' } }, function(err, response) {
+    console.log(response.text)
+})
+EOD;
+        $actual = TestUtil::execSDKJS($script);
+        $expected = "Hello World!\n";
+        TestCheck::assertString('A.3', 'SDK; check basic functionality',  $actual, $expected, $results);
     }
 }
 
