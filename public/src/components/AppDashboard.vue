48918caa6423b836d406f27a00de3be878885d51
David Z. Williams <dave@flex.io>
2018-01-29 10:01:35 -0500

    Renamed activity header.

--- public/src/components/AppDashboard.vue
+++ public/src/components/AppDashboard.vue
@@ -48,7 +48,7 @@
 
     <div class="ma4">
       <div class="flex flex-row items-center pv2">
-        <div class="flex-fill fw6 gray">Activity</div>
+        <div class="flex-fill fw6 gray">Pipe Activity</div>
         <!-- placeholder for consistent spacing -->
         <i class="invisible material-icons">chevron_right</i>
         <router-link
