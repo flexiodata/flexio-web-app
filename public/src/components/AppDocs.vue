3173859721e94b58cb613e7db622ab82c2b7c280
David Z. Williams <dave@flex.io>
2018-01-31 01:12:29 -0500

    Consistent styling of boxes in dashboard documentation.

--- public/src/components/AppDocs.vue
+++ public/src/components/AppDocs.vue
@@ -6,11 +6,11 @@
     <div class="flex flex-row flex-wrap ma3">
       <a
         target="_blank"
-        class="flex flex-column db w-100 mw6-l ma3 bg-white br2 pa3 pa4-ns no-underline ba bw1 b--black-10 hover-b--blue"
+        class="db w-100 mw6-l ma3 bg-white pa3 pa4-ns no-underline ba bw1 b--transparent hover-b--blue css-dashboard-box"
         :href="item.link.href"
         v-for="(item, index) in doc_items"
       >
-        <div class="f3 black">{{item.title}}</div>
+        <div class="f3 black lh-title">{{item.title}}</div>
         <p class="lh-copy black-70 flex-fill">
           {{item.description}}
         </p>
