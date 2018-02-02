7b289fd4f24efb280f48a733484dae2ab3577ea9
David Z. Williams <dave@flex.io>
2018-02-02 16:10:43 -0500

    Added back documentation to the main side nav.

--- public/src/components/AppHome.vue
+++ public/src/components/AppHome.vue
@@ -1,16 +1,9 @@
 <template>
   <div class="flex flex-column flex-row-l items-stretch">
     <div class="flex flex-row flex-column-l flex-none items-stretch bg-nearer-white bb bb-0-l br-l b--black-05 nt1-l pv0 pt4-l ph1-l overflow-auto trans-pm">
-      <router-link v-for="item in nav_items" :to="item.route" class="flex-auto flex-none-l no-underline truncate tc tl-l link f5 fw6 mt1 mb1-l ph1 pl3-l pr5-l pv2 bb bb-0-l bw1 bw2-l css-nav-item">
-        <div>
-          <i class="material-icons md-24 v-mid dib" :class="item.class">{{item.icon}}</i>
-          <span class="dn dib-ns v-mid">{{item.name}}</span>
-        </div>
-        <div class="dn db-l mt1 ml4" v-if="item.subitems && is_dashboard">
-          <router-link v-for="subitem in item.subitems" :to="subitem.route" class="db truncate tl-l link f7 pt2 mt1 ml1 css-nav-item">
-            &ndash; {{subitem.name}}
-          </router-link>
-        </div>
+      <router-link v-for="item in nav_items" :to="item.route" class="flex-auto flex-none-l no-underline truncate tc tl-l link f5 fw6 mt1 ph1 pl3-l pr5-l pv2 bb bb-0-l bw1 bw2-l css-nav-item">
+        <i class="material-icons md-24 v-mid dib" :class="item.class">{{item.icon}}</i>
+        <span class="dn dib-ns v-mid">{{item.name}}</span>
       </router-link>
     </div>
     <router-view class="flex-fill relative"></router-view>
@@ -27,13 +20,13 @@
     {
       route: '/learn',
       name: 'Build & Learn',
-      icon: 'help'
-    }/*,
+      icon: 'school'
+    },
     {
       route: '/docs',
       name: 'Documentation',
       icon: 'help'
-    }*/,
+    },
     {
       route: '/pipes',
       name: 'Pipes',
