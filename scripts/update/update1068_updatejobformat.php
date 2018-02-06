6bc97b45b82c1aa29fccc53ef7c0d8e6d56c1c99
Aaron Williams <aaron@flex.io>
2018-02-06 07:18:35 -0600

    added initial implementation to convert old task arrays into top-level sequence jobs; writing the updated task to the pipe and process is currently disabled until testing is finished

---
+++ scripts/update/update1068_updatejobformat.php
@@ -0,0 +1,164 @@
+<?php
+/*!
+ *
+ * Copyright (c) 2018, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2018-02-06
+ *
+ * @package flexio
+ * @subpackage Database_Update
+ */
+
+
+include_once __DIR__.'/../stub.php';
+
+
+if ($argc != 5)
+{
+    echo '{ "success": false, "msg": "Usage: php update*.php <host> <username> <password> <database_name>" }';
+    exit(0);
+}
+
+
+$params = array('host' => $argv[1],
+                'port' => 5432,
+                'username' => $argv[2],
+                'password' => $argv[3],
+                'dbname' => $argv[4]);
+
+try
+{
+    $db = \Flexio\Base\Db::factory('PDO_POSTGRES', $params);
+    $conn = $db->getConnection();
+}
+catch (\Exception $e)
+{
+    echo($e->getMessage());
+    $db = null;
+}
+
+if (is_null($db))
+{
+    echo '{ "success": false, "msg": "Could not connect to database." }';
+    exit(0);
+}
+
+
+try
+{
+    // STEP 1: update the pipe table
+    updatePipeTable($db);
+
+    // STEP 2: update the process table
+    updateProcessTable($db);
+
+    // STEP 3: update the process log table
+    updateProcessLogTable($db);
+}
+catch(\Exception $e)
+{
+    echo '{ "success": false, "msg":' . json_encode($e->getMessage()) . '}';
+    exit(0);
+}
+
+
+// update the version number
+$current_version = \Flexio\System\System::getUpdateVersionFromFilename(__FILE__);
+\Flexio\System\System::getModel()->setDbVersionNumber($current_version);
+
+echo '{ "success": true, "msg": "Operation completed successfully." }';
+
+
+
+function updatePipeTable($db)
+{
+    // STEP 1: get a list of pipes
+    $query_sql = 'select eid, task from tbl_pipe';
+    $result = $db->query($query_sql);
+
+    // STEP 2: for each pipe, get the task and add a top-level
+    // sequence job that wraps the old task array
+/*
+    {
+        "op": "sequence",
+        "params": {
+            "items": [] // old task array goes in the items array
+        }
+    }
+*/
+    $sequence_task = array(
+        'op' => 'sequence',
+        'params' => array(
+            'items' => array()
+        )
+    );
+
+    while ($result && ($row = $result->fetch()))
+    {
+        $pipe_eid = $row['eid'];
+        $pipe_task = json_decode($row['task']);
+        $sequence_task['params']['items'] = $pipe_task;
+
+        $updated_pipe_task = json_encode($sequence_task);
+        //writePipe($db, $pipe_eid, $updated_pipe_task);
+    }
+}
+
+function updateProcessTable($db)
+{
+    // STEP 1: get a list of processes
+    $query_sql = 'select eid, task from tbl_process';
+    $result = $db->query($query_sql);
+
+    // STEP 2: for each process, get the task and add a top-level
+    // sequence job that wraps the old task array
+    /*
+        {
+            "op": "sequence",
+            "params": {
+                "items": [] // old task array goes in the items array
+            }
+        }
+    */
+    $sequence_task = array(
+        'op' => 'sequence',
+        'params' => array(
+            'items' => array()
+        )
+    );
+
+    while ($result && ($row = $result->fetch()))
+    {
+        $process_eid = $row['eid'];
+        $process_task = json_decode($row['task']);
+        $sequence_task['params']['items'] = $process_task;
+
+        $updated_process_task = json_encode($sequence_task);
+        //writeProcess($db, $process_eid, $updated_process_task);
+    }
+}
+
+function writePipe($db, $pipe_eid, $task)
+{
+    $task = $db->quote($task);
+    $sql = "update tbl_pipe ".
+           "    set ".
+           "        task = $task ".
+           "    where eid = '$pipe_eid';";
+
+    $db->exec($sql);
+}
+
+function writeProcess($db, $process_eid, $task)
+{
+    $task = $db->quote($task);
+    $sql = "update tbl_process ".
+           "    set ".
+           "        task = $task ".
+           "    where eid = '$process_eid';";
+
+    $db->exec($sql);
+}
+
