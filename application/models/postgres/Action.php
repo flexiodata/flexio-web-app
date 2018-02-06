44dd0d5c547e780a3b82b44a7cf1e6b2a76a3cd3
Aaron Williams <aaron@flex.io>
2018-02-06 12:53:47 -0600

    work on event/action tracking; added comment with possible table schema

--- application/models/postgres/Action.php
+++ application/models/postgres/Action.php
@@ -19,6 +19,36 @@ class Action extends ModelBase
 {
     public function record(array $params) : bool
     {
+
+/*
+// TODO: update action model; possible table structure:
+
+--
+-- Table structure for table tbl_action
+--
+
+DROP TABLE IF EXISTS tbl_action;
+CREATE TABLE tbl_action (
+  id serial,
+  eid varchar(12) NOT NULL default '',           // the eid of the action
+  invoker_type varchar(3) NOT NULL default '',   // the type of agent used to invoke the action: api, email, scheduler, etc
+  invoked_from text default '',                  // where the action was invoked from; e.g. api, email, the scheduler, etc
+  invoked_by varchar(12) NOT NULL default '',    // user that invoked the action
+  action_type text default '',                   // the type of action being peformed
+  action_params json,                            // the parameters passed to the action
+  action_object varchar(12) NOT NULL default '', // the eid of the object being acted on
+  result_type text default '',                   // the type of result; success or failure
+  result_info json,                              // extra info about the result in case of failure
+  started timestamp NULL default NULL,           // when the action was invoked
+  finished timestamp NULL default NULL,          // when the action finished
+  created timestamp NULL default NULL,
+  updated timestamp NULL default NULL,
+  PRIMARY KEY (id),
+  UNIQUE (eid)
+);
+*/
+
+
         $db = $this->getDatabase();
         try
         {
