0aa5f239b0ffaf4230efd23944bc385975b0a1f8
Ben Williams <ben@flex.io>
2018-02-06 23:26:27 +0100

    table->csv casting in write job

--- application/jobs/Write.php
+++ application/jobs/Write.php
@@ -40,29 +40,31 @@ class Write extends \Flexio\Jobs\Base
         if (is_null($path))
             throw new \Flexio\Base\Exception(\Flexio\Base\Error::MISSING_PARAMETER, "Missing parameter 'path'");
 
-        // the write job's output is always just a copy of its input; do this first
+        $stream_properties = $instream->get();
+            
+        // the write job's "stdout" is always just a copy of its "stdin"; do this first
         $outstream->copyFrom($instream);
         $outstream = null;        
 
+        // now perform the write to 'path'
         $vfs = new \Flexio\Services\Vfs();
         $vfs->setProcess($process);
 
         // not all services support open/readers/writers; try that first
-
         try
         {
-            $service = $vfs->getServiceFromPath($path); // this will return or service, or throw
+            $create_params = [];
+            if (count($stream_properties['structure']) > 0)
+            {
+                $create_params['structure'] = $stream_properties['structure'];
+            }
+            $create_params['mime_type'] = $stream_properties['mime_type'];
 
+
+            $service = $vfs->getServiceFromPath($path); // this will return or service, or throw
             // check if the service only supports write(), and if so, go to the catch
             if (($service->getFlags() & \Flexio\IFace\IFileSystem::FLAG_RANDOM_ACCESS) == 0)
                 throw new \Exception(""); // go to the catch and use $vfs->write()
-
-            $stream_properties = $instream->get();
-            
-            $create_params = [];
-            if (count($stream_properties['structure']) > 0)
-                $create_params['structure'] = $stream_properties['structure'];
-            $create_params['mime_type'] = $stream_properties['mime_type'];
 
 
             if (!$vfs->createFile($path, $create_params))
@@ -93,10 +95,51 @@ class Write extends \Flexio\Jobs\Base
         }
         catch (\Exception $e)
         {
-            $reader = $instream->getReader();
-            $files = $vfs->write($path, function($length) use (&$reader) {
-                return $reader->read($length);
-            });
+            if (count($stream_properties['structure']) > 0)
+            {
+                $fp = fopen('php://memory', 'w');
+                $field_names = array_column($stream_properties['structure'], 'name');
+                fputcsv($fp, $field_names);
+
+
+                $reader = $instream->getReader();
+                $files = $vfs->write($path, function($length) use (&$reader, &$fp) {
+
+                    if ($fp === false)
+                        return false;
+
+                    while (ftell($fp) < $length)
+                    {
+                        $row = $reader->readRow();
+                        if ($row === false)
+                        {
+                            fseek($fp, 0);
+                            $contents = stream_get_contents($fp);
+                            fclose($fp);
+                            $fp = false;
+                            return $contents;
+                        }
+
+                        $row = array_values($row);
+                        fputcsv($fp, $row);
+                    }
+
+                    fseek($fp, 0);
+                    $contents = stream_get_contents($fp);
+                    fclose($fp);
+
+                    $fp = fopen('php://memory', 'w');
+                    fwrite($fp, substr($contents, $length));
+                    return substr($contents, 0, $length);
+                });
+            }
+             else
+            {
+                $reader = $instream->getReader();
+                $files = $vfs->write($path, function($length) use (&$reader) {
+                    return $reader->read($length);
+                });
+            }
         }
     }
 }
