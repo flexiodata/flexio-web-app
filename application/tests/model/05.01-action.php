46f3daaeb2a5c446e52ffedbbca35208d2935b93
Aaron Williams <aaron@flex.io>
2018-02-07 14:52:50 -0600

    removed bad test

--- application/tests/model/05.01-action.php
+++ application/tests/model/05.01-action.php
@@ -25,20 +25,6 @@ class Test
 
 
 
-        // TODO: add more tests
-
-        // TEST: Action\Model::record(); basic action recording
-
-        // BEGIN TEST
-        $action_params = array(
-            'user_eid' => \Flexio\Base\Eid::generate(),
-            'subject_eid' => \Flexio\Base\Eid::generate(),
-            'object_eid' => \Flexio\Base\Eid::generate(),
-            'action' => 'create',
-            'params' => json_encode(array("key" => "value")),
-        );
-        $actual = $model->action->record($action_params);
-        $expected = true;
-        TestCheck::assertBoolean('A.1', 'Action\Model::record(); basic action recording', $actual, $expected, $results);
+        // TODO: add tests
     }
 }
