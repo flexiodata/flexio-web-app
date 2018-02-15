c1e3b872db59bf2685d243522663c91b0efc83bb
Ben Williams <ben@flex.io>
2018-02-15 22:35:08 +0100

    dropbox mkdir compliancy

--- application/services/Dropbox.php
+++ application/services/Dropbox.php
@@ -137,7 +137,16 @@ class Dropbox implements \Flexio\IFace\IFileSystem
         while (false !== strpos($path,'//'))
             $path = str_replace('//','/',$path);
 
-        return $this->internalCreateFolder($path);
+        $error_summary = '';
+        if (!$this->internalCreateFolder($path, $error_summary))
+        {
+            if (substr($error_summary, 0, 14) == 'path/conflict/')
+                throw new \Flexio\Base\Exception(\Flexio\Base\Error::CREATE_FAILED, "Object already exists");
+                 else
+                throw new \Flexio\Base\Exception(\Flexio\Base\Error::CREATE_FAILED);
+        }
+
+        return true;
     }
     
     public function unlink(string $path) : bool
@@ -166,7 +175,7 @@ class Dropbox implements \Flexio\IFace\IFileSystem
         return ($httpcode >= 200 && $httpcode <= 299) ? true : false;
     }
 
-    private function internalCreateFolder(string $path)
+    private function internalCreateFolder(string $path, string &$error_summary)
     {
         $postdata = json_encode(array('path' => $path, 'autorename' => false));
 
@@ -182,9 +191,16 @@ class Dropbox implements \Flexio\IFace\IFileSystem
         curl_setopt($ch, CURLOPT_POSTFIELDS, $postdata);
 
         $result = curl_exec($ch);
+        $result = @json_decode($result, true);
 
         $httpcode = curl_getinfo($ch, CURLINFO_HTTP_CODE);
         curl_close($ch);
+
+        $error_summary = '';
+        if (isset($result['error_summary']))
+        {
+            $error_summary = $result['error_summary'];
+        }
 
         return ($httpcode >= 200 && $httpcode <= 299) ? true : false;
     }
