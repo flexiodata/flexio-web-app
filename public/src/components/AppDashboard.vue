6981501e99a12fe49baf3217d71b07c3b8145411
David Z. Williams <dave@flex.io>
2018-02-20 17:22:03 -0500

    Darker help items on dashboard.

--- public/src/components/AppDashboard.vue
+++ public/src/components/AppDashboard.vue
@@ -16,7 +16,7 @@
             <help-items
               help-message="I need help getting started with Flex.io..."
               :items="['quick-start', 'sdk-and-cli', 'api-docs', 'templates', 'help']"
-              :item-cls="'f6 fw6 ttu br2 ma1 pv3 w4 pointer moon-gray hover-blue bg-white hover-bg-near-white'"
+              :item-cls="'f6 fw6 ttu br2 ma1 pv3 w4 pointer silver hover-blue bg-white hover-bg-near-white'"
             ></help-items>
           </div>
         </div>
