31b6f8cae83d68962a42091517d1923458eb54f6
David Z. Williams <dave@flex.io>
2018-03-07 01:02:56 -0500

    Archived unused components.

--- public/src/components/DevModals.vue
+++ public/src/components/DevModals.vue
@@ -105,7 +105,6 @@
   import Btn from './Btn.vue'
   import AlertModal from './AlertModal.vue'
   import ConfirmModal from './ConfirmModal.vue'
-  import PipeDialog from './PipeDialog.vue'
   import EmailSupportModal from './EmailSupportModal.vue'
   import FlexioModal from './FlexioModal.vue'
   import SignUpModal from './SignUpModal.vue'
@@ -137,7 +136,6 @@
       Btn,
       AlertModal,
       ConfirmModal,
-      PipeDialog,
       EmailSupportModal,
       FlexioModal,
       SignUpModal
