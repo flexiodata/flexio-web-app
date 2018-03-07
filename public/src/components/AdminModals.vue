c3168e673bd62cd9953bf97c6dec4e2542314392
David Z. Williams <dave@flex.io>
2018-03-07 11:04:59 -0500

    Removed invalid click events in admin modals.

--- public/src/components/AdminModals.vue
+++ public/src/components/AdminModals.vue
@@ -2,26 +2,34 @@
   <div class="pa4 overflow-y-auto">
     <div class="mw8 center">
       <div class="pa1 w-100 mb3 mt4 bb b--black-10 fw6">Common Modals</div>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openAlertModal">Alert Modal</el-button>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openConfirmModal">Confirm Modal</el-button>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openEmailSupportModal">Email Support Modal</el-button>
-      <div class="pa1 w-100 mb3 mt4 bb b--black-10 fw6">Project Modals</div>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openNewProjectPropsModal">New Project Modal</el-button>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openEditProjectPropsModal">Edit Project Modal</el-button>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openDeleteProjectPropsModal">Delete Project Modal</el-button>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openMemberAddModal">Member Add Modal</el-button>
-      <div class="pa1 w-100 mb3 mt4 bb b--black-10 fw6">Connection Modals</div>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openNewConnectionModal">New Connection Modal</el-button>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openEditConnectionModal">Edit Connection Modal</el-button>
-      <div class="pa1 w-100 mb3 mt4 bb b--black-10 fw6">Pipe Modals</div>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openNewPipeDialog">New Pipe Modal</el-button>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openEditPipeDialog">Edit Pipe Modal</el-button>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openPipeScheduleModal">Pipe Schedule Modal</el-button>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openPipeShareModal">Pipe Share Modal</el-button>
-      <div class="pa1 w-100 mb3 mt4 bb b--black-10 fw6">Other Modals</div>
-      <el-button type="primary" class="ttu b" :disabled="true" @click="openTallModal">Tall Modal</el-button>
-      <el-button type="primary" class="ttu b" @click="openFlexioModal">Flexio Modal</el-button>
-      <el-button type="primary" class="ttu b" @click="openFlexioSignUpModal">Flexio Sign Up Modal</el-button>
+      <el-button type="primary" class="ttu b" :disabled="true">Alert Modal</el-button>
+      <el-button type="primary" class="ttu b" :disabled="true">Confirm Modal</el-button>
+      <el-button type="primary" class="ttu b" :disabled="true">Email Support Modal</el-button>
+      <div class="pa1 w-100 mb3 mt4 bb b--black-10 fw6
+      ">Project Modals</div>
+      <el-button type="primary" class="ttu b" :disabled="true">New Project Modal</el-button>
+      <el-button type="primary" class="ttu b" :disabled="true">Edit Project Modal</el-button>
+      <el-button type="primary" class="ttu b" :disabled="true">Delete Project Modal</el-button>
+      <el-button type="primary" class="ttu b" :disabled="true">Member Add Modal</el-button>
+      <div class="pa1 w-100 mb3 mt4 bb b--black-10 fw6
+      ">Connection Modals</div>
+      <el-button type="primary" class="ttu b" :disabled="true">New Connection Modal</el-button>
+      <el-button type="primary" class="ttu b" :disabled="true">Edit Connection Modal</el-button>
+      <div class="pa1 w-100 mb3 mt4 bb b--black-10 fw6
+      ">Pipe Modals</div>
+      <el-button type="primary" class="ttu b" :disabled="true">New Pipe Modal</el-button>
+      <el-button type="primary" class="ttu b" :disabled="true">Edit Pipe Modal</el-button>
+      <el-button type="primary" class="ttu b" :disabled="true">Pipe Schedule Modal</el-button>
+      <el-button type="primary" class="ttu b" :disabled="true">Pipe Share Modal</el-button>
+      <div class="pa1 w-100 mb3 mt4 bb b--black-10 fw6
+      ">Other Modals</div>
+      <el-button type="primary" class="ttu b" :disabled="true">Tall Modal</el-button>
+      <el-button type="primary" class="ttu b"
+      @click="openFlexioModal
+      ">Flexio Modal</el-button>
+      <el-button type="primary" class="ttu b"
+      @click="openFlexioSignUpModal
+      ">Flexio Sign Up Modal</el-button>
     </div>
 
     <flexio-modal
