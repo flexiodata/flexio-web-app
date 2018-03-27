a1742f7107f0026b7715ef1fdbbc4075c8a51150
Aaron Williams <aaron@flex.io>
2018-03-27 11:43:07 -0500

    added process summary implementation to mysql model

--- application/models/mysql/Process.php
+++ application/models/mysql/Process.php
@@ -112,10 +112,51 @@ class Process extends ModelBase
         }
     }
 
-    public function summary() : array
+    public function summary(array $filter) : array
     {
-        // TODO: implement
-        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNIMPLEMENTED);
+        // returns the number of processes per pipe per day for a particular owner,
+        // along with the average and total times for those processes
+
+        $db = $this->getDatabase();
+        $allowed_items = array('eid', 'eid_status', 'parent_eid', 'owned_by', 'created_min', 'created_max');
+        $filter_expr = \Filter::build($db, $filter, $allowed_items);
+        $limit_expr = \Limit::build($db, $filter);
+
+        try
+        {
+            $sql = "select owned_by as owned_by, ".
+            "              parent_eid as parent_eid, ".
+            "              created::DATE as created, ".
+            "              avg(extract(epoch from (finished - started))) as average_time, ".
+            "              sum(extract(epoch from (finished - started))) as total_time, ".
+            "              count(*) as total_count ".
+            "       from tbl_process tpr ".
+            "       where $filter_expr ".
+            "       group by owned_by, parent_eid, created::DATE ".
+            "       order by created, parent_eid $limit_expr";
+            $rows = $db->fetchAll($sql);
+         }
+         catch (\Exception $e)
+         {
+             throw new \Flexio\Base\Exception(\Flexio\Base\Error::READ_FAILED);
+         }
+
+        $output = array();
+        foreach ($rows as $row)
+        {
+            $parent_eid = '';
+            if (\Flexio\Base\Eid::isValid($row['parent_eid']))
+                $parent_eid = $row['parent_eid'];
+
+            $output[] = array('user_eid'     => $row['owned_by'],
+                              'pipe_eid'     => $parent_eid,
+                              'created'      => $row['created'],
+                              'total_count'  => $row['total_count'],
+                              'total_time'   => $row['total_time'],
+                              'average_time' => $row['average_time']);
+        }
+
+        return $output;
     }
 
     public function list(array $filter) : array
