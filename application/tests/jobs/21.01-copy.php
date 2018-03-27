69f20f6796a50873607179396af0236fc6149919
Ben Williams <ben@flex.io>
2018-03-27 09:05:38 +0200

    fixed copy test again

--- application/tests/jobs/21.01-copy.php
+++ application/tests/jobs/21.01-copy.php
@@ -50,8 +50,8 @@ class Test
         $process_copy = \Flexio\Jobs\Process::create()->execute($copy);
         $process_list_source = \Flexio\Jobs\Process::create()->execute($list_source);
         $process_list_target = \Flexio\Jobs\Process::create()->execute($list_target);
-        $actual_contents = \Flexio\Base\Util::getStreamContents($process_list_source->getStdout());
-        $expected_contents = \Flexio\Base\Util::getStreamContents($process_expected_contents->getStdout());
+        $actual_contents = \Flexio\Base\Util::getStreamContents($process_list_target->getStdout());
+        $expected_contents = \Flexio\Base\Util::getStreamContents($process_list_source->getStdout());
         \Flexio\Tests\Check::assertArray("A.1", 'Copy; compare contents of copied folder ' . $output_folder, $actual, $expected, $results);
     }
 }
