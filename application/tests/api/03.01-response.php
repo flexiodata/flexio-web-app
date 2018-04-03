798f08561f8c20ac6e351f05edb730e3e4164139
Aaron Williams <aaron@flex.io>
2018-04-03 15:26:52 -0500

    added some basic tests for api response object

--- application/tests/api/03.01-response.php
+++ application/tests/api/03.01-response.php
@@ -20,5 +20,96 @@ class Test
 {
     public function run(&$results)
     {
+        // SETUP
+
+
+        // TEST: tests for checking HTTP error code mappings
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::GENERAL;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 400;
+        \Flexio\Tests\Check::assertNumber('A.1', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::UNAUTHORIZED;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 401;
+        \Flexio\Tests\Check::assertNumber('A.2', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::INSUFFICIENT_RIGHTS;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 403;
+        \Flexio\Tests\Check::assertNumber('A.3', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::UNIMPLEMENTED;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 404;
+        \Flexio\Tests\Check::assertNumber('A.4', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::INVALID_VERSION;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 404;
+        \Flexio\Tests\Check::assertNumber('A.5', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::INVALID_PARAMETER;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 404;
+        \Flexio\Tests\Check::assertNumber('A.6', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::NO_OBJECT;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 404;
+        \Flexio\Tests\Check::assertNumber('A.7', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::CREATE_FAILED;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 422;
+        \Flexio\Tests\Check::assertNumber('A.8', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::WRITE_FAILED;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 422;
+        \Flexio\Tests\Check::assertNumber('A.9', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::RATE_LIMIT_EXCEEDED;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 422;
+        \Flexio\Tests\Check::assertNumber('A.10', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::UNDEFINED;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 500;
+        \Flexio\Tests\Check::assertNumber('A.11', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $flexio_error_code = \Flexio\Base\Error::NO_SERVICE;
+        $actual = \Flexio\Api2\Response::getHttpErrorCode($flexio_error_code);
+        $expected = 500;
+        \Flexio\Tests\Check::assertNumber('A.12', '\Flexio\Api2\Response::getHttpErrorCode(); check http error code mapping',  $actual, $expected, $results);
+
+
+
+        // TEST: tests for sending a content response
+
+        // BEGIN TEST
+        //\Flexio\Api2\Response::sendContent($content);
+
+
+        // TEST: tests for sending an error response
+
+        // BEGIN TEST
+        //\Flexio\Api2\Response::sendError($error);
+
+
     }
 }
