18bb7891032c13273bf13cbbbc874b3bc62396e2
Aaron Williams <aaron@flex.io>
2018-04-05 11:37:25 -0500

    added api tests for changing user credentials

--- application/tests/api/05.02-user.php
+++ application/tests/api/05.02-user.php
@@ -20,63 +20,91 @@ class Test
 {
     public function run(&$results)
     {
-        // ENDPOINT: POST /:userid/credentials
+        // ENDPOINT: POST /:userid/account/credentials
 
 
         // SETUP
         $apibase = \Flexio\Tests\Util::getTestHost() . '/api/v2';
-        $userid = \Flexio\Tests\Util::createUser();
-        $token = \Flexio\Tests\Util::createToken($userid);
+        $password1 = \Flexio\Tests\Util::generateHandle();
+        $userid1 = \Flexio\Tests\Util::createUser(null, null, $password1);
+        $token1 = \Flexio\Tests\Util::createToken($userid1);
+        $password2 = \Flexio\Tests\Util::generateHandle();
+        $userid2 = \Flexio\Tests\Util::createUser(null, null, $password2);
+        $token2 = \Flexio\Tests\Util::createToken($userid2);
 
-
-        // TEST: change password
-
-/*
-// TODO: old tests; convert over to new
 
         // TEST: change password
 
         // BEGIN TEST
-        $username = \Flexio\Base\Util::generateHandle();
-        $email = \Flexio\Tests\Util::createEmailAddress();
-        $password1 = \Flexio\Base\Util::generatePassword();
-        $password2 = \Flexio\Base\Util::generatePassword();
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/account/credentials",
+            // 'token' => '', // don't include a token
+            'content_type' => 'application/json',
+            'params' => '{
+                "old_password": "'.$password1.'",
+                "new_password": "'.$password2.'"
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $actual = $result['response'];
+        $expected = '
+        {
+            "error" : {
+                "code": "insufficient-rights"
+            }
+        }';
+        \Flexio\Tests\Check::assertInArray('A.1', 'POST /:userid/account/credentials; fail if requesting user doesn\'t have credentials',  $actual, $expected, $results);
 
-        $params = json_decode('
+        // BEGIN TEST
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/account/credentials",
+            'token' => $token2, // token for another user
+            'content_type' => 'application/json',
+            'params' => '{
+                "old_password": "'.$password1.'",
+                "new_password": "'.$password2.'"
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $actual = $result['response'];
+        $expected = '
         {
-            "user_name": "'.$username.'",
-            "email": "'.$email.'",
-            "password": "'.$password1.'",
-            "send_email": false,
-            "create_examples": false
-        }
-        ',true);
-        $request = \Flexio\Api1\Request::create();
-        $request->setPostParams($params);
-        $user_info = \Flexio\Api1\User::create($request);
-        $user_eid = $user_info['eid'];
-        $initial_password1_match = \Flexio\Tests\Util::getModel()->user->checkUserPasswordByEid($user_eid, $password1); // should match
-        $initial_password2_match = \Flexio\Tests\Util::getModel()->user->checkUserPasswordByEid($user_eid, $password2); // should not match
-        $params = json_decode('
-        {
-            "eid": "'.$user_eid.'",
-            "old_password": "'.$password1.'",
-            "new_password": "'.$password2.'"
-        }
-        ',true);
-        $request = \Flexio\Api1\Request::create();
-        $request->setRequestingUser($user_eid);
-        $request->setPostParams($params);
-        \Flexio\Api1\User::changepassword($request);
-        $updated_password1_match = \Flexio\Tests\Util::getModel()->user->checkUserPasswordByEid($user_eid, $password1); // should not match
-        $updated_password2_match = \Flexio\Tests\Util::getModel()->user->checkUserPasswordByEid($user_eid, $password2); // should match
-        $actual = ($initial_password1_match == true && $initial_password2_match == false && $updated_password1_match == false && $updated_password2_match == true);
+            "error" : {
+                "code": "insufficient-rights"
+            }
+        }';
+        \Flexio\Tests\Check::assertInArray('A.2', 'POST /:userid/account/credentials; fail if requesting user doesn\'t have access',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $password1_matches_before = \Flexio\Tests\Util::getModel()->user->checkUserPasswordByEid($userid1, $password1);
+        $password2_matches_before = \Flexio\Tests\Util::getModel()->user->checkUserPasswordByEid($userid1, $password2);
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/account/credentials",
+            'token' => $token1, // valid token for user
+            'content_type' => 'application/json',
+            'params' => '{
+                "old_password": "'.$password1.'",
+                "new_password": "'.$password2.'"
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+
+        $password1_matches_after = \Flexio\Tests\Util::getModel()->user->checkUserPasswordByEid($userid1, $password1);
+        $password2_matches_after = \Flexio\Tests\Util::getModel()->user->checkUserPasswordByEid($userid1, $password2);
+        $actual = ($password1_matches_before == true && $password2_matches_before == false && $password1_matches_after == false && $password2_matches_after == true);
         $expected = true;
-        \Flexio\Tests\Check::assertBoolean('B.1', '\Flexio\Api1\User::changepassword(); make sure that the password is changed',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertBoolean('A.3', 'POST /:userid/account/credentials; make sure that the password is changed',  $actual, $expected, $results);
 
-
-
-        // TEST: request password change
-*/
+        $actual = $result['response'];
+        $expected = '
+        {
+            "eid": "'.$userid1.'",
+            "eid_type": "USR",
+            "eid_status": "A"
+        }';
+        \Flexio\Tests\Check::assertInArray('A.4', 'POST /:userid/account/credentials; change password response',  $actual, $expected, $results);
     }
 }
