1eac02c81042b2a8a9faa907bdffd4f6ad24c78b
Aaron Williams <aaron@flex.io>
2018-04-06 15:07:50 -0500

    implemented file validation job; use schema validator to verify file properties

--- application/jobs/Validate.php
+++ application/jobs/Validate.php
@@ -21,6 +21,8 @@ namespace Flexio\Jobs;
     "op": "validate",
     "params": {
         "path": ""
+        "validator": {
+        }
     }
 }
 */
@@ -29,5 +31,37 @@ class Validate extends \Flexio\Jobs\Base
 {
     public function run(\Flexio\IFace\IProcess $process)
     {
+        parent::run($process);
+
+        $outstream = $process->getStdout();
+        $job_params = $this->getJobParameters();
+        $path = $job_params['path'] ?? null;
+        $validator = $job_params['validator'] ?? null;
+
+        if (is_null($path))
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::MISSING_PARAMETER, "Missing parameter 'path'");
+        if (is_null($validator))
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::MISSING_PARAMETER, "Missing parameter 'validator'");
+
+        // make sure the validator is a valid validator schema
+        if (\Flexio\Base\ValidatorSchema::checkSchema($validator)->hasErrors())
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INVALID_PARAMETER, "Invalid validator format");
+
+        $vfs = new \Flexio\Services\Vfs();
+        $vfs->setProcess($process);
+
+        $info = $vfs->getFileInfo($path);
+
+        $info_to_validate = array(
+            'name' => $info['name'] ?? '',
+            'path' => $info['path'] ?? '',
+            'size' => $info['size'] ?? null,
+            'modified' => $info['modified'] ?? '',
+            'mime_type' => $info['content_type'] ?? 'application/octet-stream'
+        );
+
+        if (\Flexio\Base\ValidatorSchema::check($info_to_validate, $validator)->hasErrors())
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INVALID_PARAMETER, "Invalid file");
     }
 }
+
