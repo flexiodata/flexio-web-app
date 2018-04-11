ddd647eaab5f1e032817e530eb819b78e6cdc516
Aaron Williams <aaron@flex.io>
2018-04-11 10:07:45 -0500

    added scaffolding for api tests for running pipes

---
+++ application/tests/api/08.04-pipe.php
@@ -0,0 +1,80 @@
+<?php
+/**
+ *
+ * Copyright (c) 2018, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2018-04-11
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // ENDPOINT: POST /:userid/pipes/:objeid/run
+
+
+        // SETUP
+        $apibase = \Flexio\Tests\Util::getTestHost() . '/api/v2';
+        $password1 = \Flexio\Base\Util::generatePassword();
+        $userid1 = \Flexio\Tests\Util::createUser(null, null, $password1);
+        $token1 = \Flexio\Tests\Util::createToken($userid1);
+        $password2 = \Flexio\Base\Util::generatePassword();
+        $userid2 = \Flexio\Tests\Util::createUser(null, null, $password2);
+        $token2 = \Flexio\Tests\Util::createToken($userid2);
+
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/pipes",
+            'token' => $token1,
+            'content_type' => 'application/json',
+            'params' => '{
+                "name": "Test Pipe",
+                "ename": "ename1"
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $response = json_decode($result['response'],true);
+        $objeid1 = $response['eid'] ?? '';
+
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid2/pipes",
+            'token' => $token2,
+            'content_type' => 'application/json',
+            'params' => '{
+                "name": "Test Pipe",
+                "ename": "ename2"
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $response = json_decode($result['response'],true);
+        $objeid2 = $response['eid'] ?? '';
+
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/pipes",
+            'token' => $token1,
+            'content_type' => 'application/json',
+            'params' => '{
+                "name": "Test Pipe",
+                "ename": "ename3"
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $response = json_decode($result['response'],true);
+        $objeid3 = $response['eid'] ?? '';
+
+
+        // TEST: run pipe
+    }
+}
