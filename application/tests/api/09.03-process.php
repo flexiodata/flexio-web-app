a1db884f64f6149a72aa079ab34ff50fd3720ef9
Aaron Williams <aaron@flex.io>
2018-04-12 10:07:33 -0500

    added api tests for deleting a process

--- application/tests/api/09.03-process.php
+++ application/tests/api/09.03-process.php
@@ -20,6 +20,135 @@ class Test
 {
     public function run(&$results)
     {
+        // ENDPOINT: DEL /:userid/processes/:objeid
+
+
+        // SETUP
+        $apibase = \Flexio\Tests\Util::getTestHost() . '/api/v2';
+        $password1 = \Flexio\Base\Util::generatePassword();
+        $userid1 = \Flexio\Tests\Util::createUser(null, null, $password1);
+        $token1 = \Flexio\Tests\Util::createToken($userid1);
+        $password2 = \Flexio\Base\Util::generatePassword();
+        $userid2 = \Flexio\Tests\Util::createUser(null, null, $password2);
+        $token2 = \Flexio\Tests\Util::createToken($userid2);
+
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/processes",
+            'token' => $token1,
+            'content_type' => 'application/json',
+            'params' => '{
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $response = json_decode($result['response'],true);
+        $objeid1 = $response['eid'] ?? '';
+
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid2/processes",
+            'token' => $token2,
+            'content_type' => 'application/json',
+            'params' => '{
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $response = json_decode($result['response'],true);
+        $objeid2 = $response['eid'] ?? '';
+
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/processes",
+            'token' => $token1,
+            'content_type' => 'application/json',
+            'params' => '{
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $response = json_decode($result['response'],true);
+        $objeid3 = $response['eid'] ?? '';
+
+
+        // TEST: delete process
+
+        // BEGIN TEST
+        $params = array(
+            'method' => 'DELETE',
+            'url' => "$apibase/$userid1/processes/$objeid1",
+            // 'token' => '' // no token included
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $actual = $result['response'];
+        $expected = '
+        {
+            "error": {
+                "code": "insufficient-rights"
+            }
+        }';
+        \Flexio\Tests\Check::assertInArray('A.1', 'DELETE /:userid/processes/:objeid; fail if requesting user doesn\'t have credentials',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $params = array(
+            'method' => 'DELETE',
+            'url' => "$apibase/$userid1/processes/$objeid2",
+            'token' => $token1
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $actual = $result['response'];
+        $expected = '
+        {
+            "error": {
+                "code": "no-object"
+            }
+        }';
+        \Flexio\Tests\Check::assertInArray('A.2', 'DELETE /:userid/processes/:objeid; fail if object isn\'t owned by specified owner',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $params = array(
+            'method' => 'DELETE',
+            'url' => "$apibase/$userid1/processes/$objeid1",
+            'token' => $token2 // token for another user
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $actual = $result['response'];
+        $expected = '
+        {
+            "error": {
+                "code": "insufficient-rights"
+            }
+        }';
+        \Flexio\Tests\Check::assertInArray('A.3', 'DELETE /:userid/processes/:objeid; fail if requesting user doesn\'t have rights',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $params = array(
+            'method' => 'DELETE',
+            'url' => "$apibase/$userid1/processes/$objeid1",
+            'token' => $token1
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $actual = $result['response'];
+        $expected = '
+        {
+            "eid": "'.$objeid1.'",
+            "eid_type": "PRC",
+            "eid_status": "D"
+        }';
+        \Flexio\Tests\Check::assertInArray('A.4', 'DELETE /:userid/processes/:objeid; delete process',  $actual, $expected, $results);
+        $params = array(
+            'method' => 'GET',
+            'url' => "$apibase/$userid1/processes/$objeid1",
+            'token' => $token1
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $actual = $result['response'];
+        $expected = '
+        {
+            "error": {
+                "code":"no-object",
+                "message":"Object not available"
+            }
+        }';
+        \Flexio\Tests\Check::assertInArray('A.5', 'DELETE /:userid/processes/:objeid; make sure a process is deleted',  $actual, $expected, $results);
     }
 }
 
