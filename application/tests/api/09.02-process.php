8958370ec662ab2e4d46125974386dfd26c8ecb6
Aaron Williams <aaron@flex.io>
2018-04-12 09:59:22 -0500

    added api tests for updating a process

--- application/tests/api/09.02-process.php
+++ application/tests/api/09.02-process.php
@@ -20,5 +20,168 @@ class Test
 {
     public function run(&$results)
     {
+        // ENDPOINT: POST /:userid/processes/:objeid
+
+
+        // SETUP
+        $apibase = \Flexio\Tests\Util::getTestHost() . '/api/v2';
+        $password1 = \Flexio\Base\Util::generatePassword();
+        $userid1 = \Flexio\Tests\Util::createUser(null, null, $password1);
+        $token1 = \Flexio\Tests\Util::createToken($userid1);
+        $password2 = \Flexio\Base\Util::generatePassword();
+        $userid2 = \Flexio\Tests\Util::createUser(null, null, $password2);
+        $token2 = \Flexio\Tests\Util::createToken($userid2);
+
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/processes",
+            'token' => $token1,
+            'content_type' => 'application/json',
+            'params' => '{
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $response = json_decode($result['response'],true);
+        $objeid1 = $response['eid'] ?? '';
+
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid2/processes",
+            'token' => $token2,
+            'content_type' => 'application/json',
+            'params' => '{
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $response = json_decode($result['response'],true);
+        $objeid2 = $response['eid'] ?? '';
+
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/processes",
+            'token' => $token1,
+            'content_type' => 'application/json',
+            'params' => '{
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $response = json_decode($result['response'],true);
+        $objeid3 = $response['eid'] ?? '';
+
+
+        // TEST: change process info
+
+        // BEGIN TEST
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/processes/$objeid1",
+            // 'token' => '', // no token included
+            'content_type' => 'application/json',
+            'params' => '{
+                "task": {
+                    "op": "echo",
+                    "params": {
+                        "msg": "finished"
+                    }
+                }
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $actual = $result['response'];
+        $expected = '
+        {
+            "error": {
+                "code": "insufficient-rights"
+            }
+        }';
+        \Flexio\Tests\Check::assertInArray('A.1', 'POST /:userid/processes/:objeid; fail if requesting user doesn\'t have credentials',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/processes/$objeid2",
+            'token' => $token1,
+            'content_type' => 'application/json',
+            'params' => '{
+                "task": {
+                    "op": "echo",
+                    "params": {
+                        "msg": "finished"
+                    }
+                }
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $actual = $result['response'];
+        $expected = '
+        {
+            "error": {
+                "code": "no-object"
+            }
+        }';
+        \Flexio\Tests\Check::assertInArray('A.2', 'POST /:userid/processes/:objeid; fail if object isn\'t owned by specified owner',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/processes/$objeid1",
+            'token' => $token2, // token for another user
+            'content_type' => 'application/json',
+            'params' => '{
+                "task": {
+                    "op": "echo",
+                    "params": {
+                        "msg": "finished"
+                    }
+                }
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $actual = $result['response'];
+        $expected = '
+        {
+            "error": {
+                "code": "insufficient-rights"
+            }
+        }';
+        \Flexio\Tests\Check::assertInArray('A.3', 'POST /:userid/processes/:objeid; fail if requesting user doesn\'t have rights',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $new_username = \Flexio\Base\Util::generateHandle();
+        $new_password = \Flexio\Base\Util::generatePassword();
+        $new_email = \Flexio\Tests\Util::createEmailAddress();
+        $params = array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid1/processes/$objeid1",
+            'token' => $token1, // valid token for user
+            'content_type' => 'application/json',
+            'params' => '{
+                "task": {
+                    "op": "echo",
+                    "params": {
+                        "msg": "finished"
+                    }
+                }
+            }'
+        );
+        $result = \Flexio\Tests\Util::callApi($params);
+        $actual = $result['response'];
+        $expected = '
+        {
+            "eid": "'.$objeid1.'",
+            "eid_type": "PRC",
+            "eid_status": "A",
+            "task": {
+                "op": "echo",
+                "params": {
+                    "msg": "finished"
+                }
+            },
+            "owned_by": {
+                "eid": "'.$userid1.'",
+                "eid_type": "USR"
+            }
+        }';
+        \Flexio\Tests\Check::assertInArray('A.4', 'POST /:userid/processes/:objeid; return updated process info',  $actual, $expected, $results);
     }
 }
