d00dee41111c409c9acaf6b78699f70d05cf93cb
David Z. Williams <dave@flex.io>
2018-04-13 11:45:29 -0400

    Added ServiceName component.

---
+++ public/src/components/ServiceName.vue
@@ -0,0 +1,26 @@
+<template>
+  <span>{{name}}</span>
+</template>
+
+<script>
+  import * as connections from '../constants/connection-info'
+
+  export default {
+    props: {
+      'type': {
+        type: String,
+        required: true
+      }
+    },
+    computed: {
+      name() {
+        return _.result(this, 'cinfo.service_name', '')
+      }
+    },
+    methods: {
+      cinfo() {
+        return _.find(connections, { connection_type: this.type })
+      }
+    }
+  }
+</script>
