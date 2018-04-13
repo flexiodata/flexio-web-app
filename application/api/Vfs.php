1ce394fe43bc9faa2af56b941ad43f1327c30f8a
Ben Williams <ben@flex.io>
2018-04-14 01:17:27 +0200

    writing a table to google sheets works

--- application/api/Vfs.php
+++ application/api/Vfs.php
@@ -134,11 +134,14 @@ class Vfs
             throw new \Flexio\Base\Exception(\Flexio\Base\Error::INSUFFICIENT_RIGHTS);
 
         $path = $request_url;
-        if (substr($path,0,12) != '/api/v1/vfs/')
+
+        $pos = strpos($path, '/vfs/');
+        if ($pos === false)
             throw new \Flexio\Base\Exception(\Flexio\Base\Error::INVALID_REQUEST);
 
         // grab path, including preceding slash
-        $path = substr($path,11);
+        $path = substr($path, $pos+4);
+    
 
         // TODO: need to patch through the owner info here
 
