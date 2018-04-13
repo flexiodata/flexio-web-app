30653c61ad09f9616f2cabcad95f6ba53dd51899
David Z. Williams <dave@flex.io>
2018-04-13 10:53:14 -0400

    Use single quotes.

--- public/src/store/modules/builder.js
+++ public/src/store/modules/builder.js
@@ -2,51 +2,53 @@ import _ from 'lodash'
 
 const state = {
   def: {
-    name: "Copy files from Google Drive to Dropbox",
-    description: "Copy files from Google Drive to Dropbox programmatically or on a regularly scheduled basis",
+    name: 'Copy files from Google Drive to Dropbox',
+    description: 'Copy files from Google Drive to Dropbox programmatically or on a regularly scheduled basis',
     keywords: [
-      "flex.io",
-      "web apps",
-      "programmatic",
-      "automation",
-      "syncing",
-      "integrations"
+      'flex.io',
+      'web apps',
+      'programmatic',
+      'automation',
+      'syncing',
+      'integrations'
     ],
     connections: [
-      "googledrive",
-      "dropbox"
+      'googledrive',
+      'dropbox'
     ],
     intro: "# Copy files from Google Drive to Dropbox\n\nWant an easier way to programmatically copy files from Google Drive to Dropbox? With this integration, files can be bulk copied from Google Drive to Dropbox in the original file format (e.g. PDF, PNG or DOCX). You'll never need to worry about keeping cloud storage folders in sync again.\n\n### How it Works\n\nWhen this pipe is run programmatically or on a regularly scheduled basis, Flex.io copies the specified files or folders from Google Drive to Dropbox so you can keep them in sync between your cloud services.\n\n### What You Need\n\nGoogle Drive account\nDropbox account\n",
     variables: {
       connection1: {
-        type: "connection",
-        connection_type: "googledrive"
+        type: 'connection',
+        connection_type: 'googledrive'
       },
       read_path: {
-        type: "string",
-        val: "/input-folder/file.txt",
+        type: 'file',
+        connection: 'connection1',
+        val: '/input-folder/file.txt',
         runtime: true
       },
       connection2: {
-        type: "connection",
-        connection_type: "dropbox"
+        type: 'connection',
+        connection_type: 'dropbox'
       },
       write_path: {
-        type: "string",
-        val: "/output-folder/file.txt",
+        type: 'string',
+        connection: 'connection2',
+        val: '/output-folder/file.txt',
         runtime: true
       }
     },
     task: [
       {
-        op: "read",
-        connection: "${connection1}",
-        path: "${read_path}"
+        op: 'read',
+        connection: '${connection1}',
+        path: '${read_path}'
       },
       {
-        op: "write",
-        connection: "${connection2}",
-        path: "${write_path}"
+        op: 'write',
+        connection: '${connection2}',
+        path: '${write_path}'
       }
     ]
   },
