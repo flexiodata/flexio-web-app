ce3205e30bc61b886f748ebb665a920deb2dafeb
David Z. Williams <dave@flex.io>
2018-04-13 10:46:46 -0400

    Moved builder definiton into store module.

--- public/src/components/BuilderPage.vue
+++ public/src/components/BuilderPage.vue
@@ -1,79 +1,26 @@
 <template>
   <div class="bg-nearer-white pv4">
     <div class="center mw7">
-      <h1 class="f3 mid-gray mb4">{{output.name}}</h1>
-      <div class="css-dashboard-box pa5 tc">
-        Action box
+      <h1 class="fw6 f3 mid-gray mb4">Test</h1>
+      <div class="css-dashboard-box pa2">
+        <div v-if="true">
+          <pre class="f7">{{test}}</pre>
+        </div>
+        <button class="ttu b blue" type="button" @click="$store.commit('BUILDER__GO_PREV_ITEM')" v-if="false">Prev</button>
+        <button class="ttu b blue" type="button" @click="$store.commit('BUILDER__GO_NEXT_ITEM')">Next</button>
       </div>
     </div>
   </div>
 </template>
 
 <script>
-  var def = {
-    name: "Copy files from Google Drive to Dropbox",
-    description: "Copy files from Google Drive to Dropbox programmatically or on a regularly scheduled basis",
-    keywords: [
-      "flex.io",
-      "web apps",
-      "programmatic",
-      "automation",
-      "syncing",
-      "integrations"
-    ],
-    connections: [
-      "googledrive",
-      "dropbox"
-    ],
-    intro: "# Copy files from Google Drive to Dropbox\n\nWant an easier way to programmatically copy files from Google Drive to Dropbox? With this integration, files can be bulk copied from Google Drive to Dropbox in the original file format (e.g. PDF, PNG or DOCX). You'll never need to worry about keeping cloud storage folders in sync again.\n\n### How it Works\n\nWhen this pipe is run programmatically or on a regularly scheduled basis, Flex.io copies the specified files or folders from Google Drive to Dropbox so you can keep them in sync between your cloud services.\n\n### What You Need\n\nGoogle Drive account\nDropbox account\n",
-    variables: {
-      connection1: {
-        type: "connection",
-        connection_type: "googledrive"
-      },
-      read_path: {
-        type: "string",
-        val: "/input-folder/file.txt",
-        runtime: true
-      },
-      connection2: {
-        type: "connection",
-        connection_type: "dropbox"
-      },
-      write_path: {
-        type: "string",
-        val: "/output-folder/file.txt",
-        runtime: true
-      }
-    },
-    task: [
-      {
-        op: "read",
-        connection: "${connection1}",
-        path: "${read_path}"
-      },
-      {
-        op: "write",
-        connection: "${connection2}",
-        path: "${write_path}"
-      }
-    ]
-  }
+  import { mapState, mapGetters } from 'vuex'
 
   export default {
-    watch: {
-      active_idx: {
-        handler: 'updateActiveItem',
-        immediate: true
-      }
-    },
     data() {
       return {
-        def,
-        active_idx: -1,
-        active_item: {},
         output: {
-          name: def.name,
+          name: '',
           task: {
             op: 'sequence',
             params: {
@@ -84,24 +31,15 @@
       }
     },
     computed: {
-      variable_keys() {
-        return _.keys(_.get(this.def, 'variables', {}))
-      },
-      active_key() {
-        return _.get(this.variable_keys, '['+this.active_idx+']', '')
+      ...mapState([
+        'builder'
+      ]),
+      test() {
+        return JSON.stringify(this.builder.active_item, null, 2)
       }
     },
-    methods: {
-      updateActiveItem(idx) {
-        var item = _.get(this.def, this.active_key, {})
-        this.active_item = _.assign({}, item)
-      },
-      goPrev() {
-        this.active_idx = Math.max(this.active_idx - 1, 0)
-      },
-      goNext() {
-        this.active_idx = Math.min(this.active_idx + 1, _.size(this.variable_keys) - 1)
-      }
+    mounted() {
+      this.$store.commit('BUILDER__INIT_ITEMS')
     }
   }
 </script>
--- public/src/store/modules/builder.js
+++ public/src/store/modules/builder.js
@@ -1,19 +1,95 @@
+import _ from 'lodash'
+
 const state = {
-  items: []
+  def: {
+    name: "Copy files from Google Drive to Dropbox",
+    description: "Copy files from Google Drive to Dropbox programmatically or on a regularly scheduled basis",
+    keywords: [
+      "flex.io",
+      "web apps",
+      "programmatic",
+      "automation",
+      "syncing",
+      "integrations"
+    ],
+    connections: [
+      "googledrive",
+      "dropbox"
+    ],
+    intro: "# Copy files from Google Drive to Dropbox\n\nWant an easier way to programmatically copy files from Google Drive to Dropbox? With this integration, files can be bulk copied from Google Drive to Dropbox in the original file format (e.g. PDF, PNG or DOCX). You'll never need to worry about keeping cloud storage folders in sync again.\n\n### How it Works\n\nWhen this pipe is run programmatically or on a regularly scheduled basis, Flex.io copies the specified files or folders from Google Drive to Dropbox so you can keep them in sync between your cloud services.\n\n### What You Need\n\nGoogle Drive account\nDropbox account\n",
+    variables: {
+      connection1: {
+        type: "connection",
+        connection_type: "googledrive"
+      },
+      read_path: {
+        type: "string",
+        val: "/input-folder/file.txt",
+        runtime: true
+      },
+      connection2: {
+        type: "connection",
+        connection_type: "dropbox"
+      },
+      write_path: {
+        type: "string",
+        val: "/output-folder/file.txt",
+        runtime: true
+      }
+    },
+    task: [
+      {
+        op: "read",
+        connection: "${connection1}",
+        path: "${read_path}"
+      },
+      {
+        op: "write",
+        connection: "${connection2}",
+        path: "${write_path}"
+      }
+    ]
+  },
+  items: [],
+  active_item: {}
 }
 
 const mutations = {
-  BUILDER__UPDATE_ITEMS (state, items) {
+  BUILDER__INIT_ITEMS (state) {
+    var idx = 0
+    var variables = _.get(state, 'def.variables', {})
 
+    state.items = _.mapValues(variables, (item) => {
+      return _.assign({}, item, { idx: idx++ })
+    })
+    state.active_item = _.find(state.items, { idx: 0 })
+  },
+
+  BUILDER__GO_PREV_ITEM (state) {
+    var active_idx = _.get(state, 'active_item.idx', 0)
+    var item = _.find(state.items, { idx: active_idx - 1 })
+
+    if (!_.isNil(item))
+      state.active_item = _.assign({}, item)
+  },
+
+  BUILDER__GO_NEXT_ITEM (state) {
+    var active_idx = _.get(state, 'active_item.idx', 1000)
+    var item = _.find(state.items, { idx:  active_idx + 1 })
+
+    if (!_.isNil(item))
+      state.active_item = _.assign({}, item)
   }
 }
 
-const actions = {
-}
+const actions = {}
+
+const getters = {}
 
 export default {
   state,
   mutations,
-  actions
+  actions,
+  getters
 }
 
