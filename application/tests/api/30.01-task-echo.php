b40f594f232c114f007a8e7f7358dfa10d0a2e46
Aaron Williams <aaron@flex.io>
2018-04-17 14:48:50 -0500

    added basic api test for echo function; developing pattern for task tests in api

---
+++ application/tests/api/30.01-task-echo.php
@@ -0,0 +1,63 @@
+<?php
+/**
+ *
+ * Copyright (c) 2018, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2018-04-17
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // ENDPOINT: POST /:userid/pipes/:objeid/run
+
+
+        // SETUP
+        $apibase = \Flexio\Tests\Util::getTestHost() . '/api/v2';
+        $password = \Flexio\Base\Password::generate();
+        $userid = \Flexio\Tests\Util::createUser(null, null, $password);
+        $token = \Flexio\Tests\Util::createToken($userid);
+
+
+
+        // TEST: run process with rights checks
+
+        // BEGIN TEST
+        $result = \Flexio\Tests\Util::callApi(array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid/pipes",
+            'token' => $token,
+            'content_type' => 'application/json',
+            'params' => '{
+                "task": {
+                    "op": "echo",
+                    "params": {
+                        "msg": "Hello, World!"
+                    }
+                }
+            }'
+        ));
+        $response = json_decode($result['response'],true);
+        $objeid = $response['eid'] ?? '';
+        $result = \Flexio\Tests\Util::callApi(array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid/pipes/$objeid/run",
+            'token' => $token
+        ));
+        $actual = $result['response'];
+        $expected = 'Hello, World!';
+        \Flexio\Tests\Check::assertString('A.1', 'POST /:userid/pipes/:objeid/run; return results of echo task',  $actual, $expected, $results);
+    }
+}
+
