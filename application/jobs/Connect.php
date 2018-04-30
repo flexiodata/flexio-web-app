df45f4a400f649a96e6d06f20ceab4b7e7c79f05
Aaron Williams <aaron@flex.io>
2018-04-30 14:32:58 -0500

    in the connect job, try to connect to the service and throw an exception if the connection fails

--- application/jobs/Connect.php
+++ application/jobs/Connect.php
@@ -32,26 +32,30 @@ class Connect extends \Flexio\Jobs\Base
 
         $params = $this->getJobParameters();
 
-
-        $handle = $params['handle'] ?? '';
-        // TODO: check for valid connection type; throw if not found
-        //if ($connection_type not in 'dropbox'....)
-        //  throw new \Flexio\Base\Exception(\Flexio\Base\Error::MISSING_PARAMETER, "Connection handle is missing or invalid");
-
+        $handle = $params['handle'] ?? false;
         unset($params['handle']);
 
-
-        $connection_type = $params['type'] ?? '';
-
-        // TODO: check for valid connection type; throw if not found
-        //if ($connection_type not in 'dropbox'....)
-        //  throw new \Flexio\Base\Exception(\Flexio\Base\Error::NOT_FOUND, "Connection type is missing or invalid");
-        
+        $connection_type = $params['type'] ?? false;
         unset($params['type']);
-
 
         $connection_properties = [ 'connection_type' => $connection_type, 'connection_info' => $params ];
 
+        // get the handle to use to reference the connection; must be a valid identifier (no strictly necessary
+        // here, but enforced to parallel the requirements of 'alias')
+        if ($handle === false)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::MISSING_PARAMETER, "Missing connection 'handle' parameter.");
+        if ($type === false)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::MISSING_PARAMETER, "Missing connection 'type' parameter.");
+
+        if (\Flexio\Base\Identifier::isValid($handle) === false)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INVALID_PARAMETER, "Invalid connection 'handle' parameter; 'handle' must be a valid identifier (lowercase string between 3 and 80 chars made up of only letters, numbers, hyphens and underscores)");
+
+        // attempt to connect to the service; throw an exception if we can't connect
+        $service = \Flexio\Services\Factory::create($connection_properties);
+        if (!$service)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::CONNECTION_FAILED, "Could not create connection");
+
+        // add the connection to the process
         $process->addLocalConnection($handle, $connection_properties);
     }
 }
