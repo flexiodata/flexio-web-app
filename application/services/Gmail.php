72b7dfe043a09b7a161c2ba1d4f4d2b8ca24af72
Ben Williams <ben@flex.io>
2018-05-14 20:53:10 +0200

    added gmail service

---
+++ application/services/Gmail.php
@@ -0,0 +1,265 @@
+<?php
+/**
+ *
+ * Copyright (c) 2018, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Benjamin I. Williams
+ * Created:  2018-05-14
+ *
+ * @package flexio
+ * @subpackage Services
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Services;
+
+
+class Gmail implements \Flexio\IFace\IConnection, \Flexio\IFace\IFileSystem
+{
+    private $access_token = '';
+    private $refresh_token = '';
+    private $updated = '';
+    private $expires = 0;
+
+    public static function create(array $params = null) // TODO: fix dual return types which is used for Oauth
+    {
+        if (!isset($params))
+            return new self;
+
+        return self::initialize($params);
+    }
+
+    public function authenticated() : bool
+    {
+        if (strlen($this->access_token) > 0)
+            return true;
+
+        return false;
+    }
+
+    ////////////////////////////////////////////////////////////
+    // IFileSystem interface
+    ////////////////////////////////////////////////////////////
+
+    public function getFlags() : int
+    {
+        return 0;
+    }
+
+    public function list(string $path = '', array $options = []) : array
+    {
+        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNIMPLEMENTED);
+    }
+
+    public function getFileInfo(string $path) : array
+    {
+        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNIMPLEMENTED);
+    }
+
+    public function exists(string $path) : bool
+    {
+        // TODO: implement
+        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNIMPLEMENTED);
+        return false;
+    }
+
+
+
+    public function createFile(string $path, array $properties = []) : bool
+    {
+        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNIMPLEMENTED);
+    }
+
+    public function createDirectory(string $path, array $properties = []) : bool
+    {
+        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNIMPLEMENTED);
+    }
+
+    public function unlink(string $path) : bool
+    {
+        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNIMPLEMENTED);
+    }
+
+    public function open($path) : \Flexio\IFace\IStream
+    {
+        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNIMPLEMENTED);
+    }
+
+    public function read(array $params, callable $callback)
+    {
+        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNIMPLEMENTED);
+    }
+
+    public function write(array $params, callable $callback)
+    {
+        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNIMPLEMENTED);
+    }
+
+    public function insert(array $params, array $rows)  // $rows is an array of rows
+    {
+        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNIMPLEMENTED);
+    }
+
+
+
+    public function getTokens() : array
+    {
+        return [ 'access_token' => $this->access_token,
+                 'refresh_token' => $this->refresh_token,
+                 'expires' => $this->expires ];
+    }
+
+    private function connect() : bool
+    {
+        return true;
+    }
+
+    private static function initialize(array $params)
+    {
+        $client_id = $GLOBALS['g_config']->googledrive_client_id ?? '';
+        $client_secret = $GLOBALS['g_config']->googledrive_client_secret ?? '';
+
+        if (strlen($client_id) == 0 || strlen($client_secret) == 0)
+            return null;
+
+        $oauth_callback = '';
+        if (isset($params['redirect']))
+            $oauth_callback = $params['redirect'];
+
+        // TODO: handle service error info
+
+        // note: returns an authenticated object, an authorization uri,
+        // or null if there's not enough information to move forward
+
+        // if we have an access token, we can create the object with the
+        // token; if we don't have an access token, we have to go through
+        // an authentication process to get it; if we're beginning the
+        // initialize process, the following will return a string with the
+        // authentication url; when initialization is complete the following
+        // will return an object with a serialized access token
+
+
+        // STEP 1: if we have an access token and it's not expired, create an object
+        // from the access token and return it
+        if (isset($params['access_token']) && strlen($params['access_token']) > 0)
+        {
+            $curtime = time();
+            $expires = $params['expires'] ?? 0;
+            if ($curtime < $expires)
+            {
+                // access token is valid (not expired); use it
+                $object = new self;
+                $object->access_token = $params['access_token'];
+                $object->refresh_token = $params['refresh_token'] ?? '';
+                $object->expires = $expires;
+                return $object;
+            }
+             else
+            {
+                // access token is expired -- try to refresh it
+                $oauth = self::createService($oauth_callback);
+                if (!$oauth)
+                    return null;
+
+                $access_token = $params['access_token'] ?? null;
+                if (!isset($params['refresh_token']) || strlen($params['refresh_token']) == 0)
+                    return null; // refresh token is missing
+                $refresh_token = $params['refresh_token'];
+
+                $token = new \OAuth\OAuth2\Token\StdOAuth2Token($access_token, $refresh_token);
+                if ($expires > 0)
+                    $token->setEndOfLife($params['expires']);
+
+                try
+                {
+                    $token = $oauth->refreshAccessToken($token);
+                    if (!$token)
+                        return null;
+                }
+                catch (\OAuth\Common\Http\Exception\TokenResponseException $e)
+                {
+                    // this happens when offline
+                    throw new \Flexio\Base\Exception(\Flexio\Base\Error::NO_SERVICE, "Could not refresh access token");
+                }
+
+                $object = new self;
+                $object->expires = $token->getEndOfLife();
+                $object->access_token = $token->getAccessToken();
+                $object->refresh_token = $token->getRefreshToken();
+                if ($object->refresh_token === null || strlen($object->refresh_token) == 0)
+                    $object->refresh_token = $refresh_token;
+
+                return $object;
+            }
+        }
+
+
+        $oauth = self::createService($oauth_callback);
+        if (!$oauth)
+            return null;
+
+        // STEP 3: if we have a code parameter, we have enough information
+        // to authenticate and get the token; do so and return the object
+        if (isset($params['code']))
+        {
+            $token = $oauth->requestAccessToken($params['code']);
+            if (!$token)
+                return null;
+            $object = new self;
+            $object->access_token = $token->getAccessToken();
+            $object->refresh_token = $token->getRefreshToken();
+            $object->expires = $token->getEndOfLife();
+            if (is_null($object->refresh_token)) $object->refresh_token = '';
+
+            return $object;
+        }
+
+
+        // STEP 4: we don't have a code parameter, so we need more
+        // information to authenticate; make sure we have state info,
+        // or we don't have enough information to complete the process
+        if (!isset($params['state']))
+            return null;
+
+        // we have state info, return the authorization uri so we can
+        // get a code and complete the process
+        $additional_params = array(
+            'state' => $params['state'],
+            'approval_prompt' => 'force'
+        );
+
+        return $oauth->getAuthorizationUri($additional_params)->getAbsoluteUri();
+    }
+
+    private static function createService($oauth_callback) // TODO: set parameter/return type
+    {
+        $client_id = $GLOBALS['g_config']->gmail_client_id ?? '';
+        $client_secret = $GLOBALS['g_config']->gmail_client_secret ?? '';
+
+        if (strlen($client_id) == 0 || strlen($client_secret) == 0)
+            return null;
+
+        $service_factory = new \OAuth\ServiceFactory();
+        $storage = new \OAuth\Common\Storage\Memory();
+
+        $credentials = new \OAuth\Common\Consumer\Credentials(
+            $client_id,
+            $client_secret,
+            $oauth_callback
+        );
+
+        // instantiate the google service using the credentials,
+        // http client and storage mechanism for the token
+        $service = $service_factory->createService('google', $credentials, $storage, array('gmail', 'gmail_imap_smtp'));
+        if (!isset($service))
+            return null;
+
+        // we want offline access (permanent until the owner revokes it) for running jobs
+        $service->setAccessType('offline');
+        return $service;
+    }
+
+}
+
