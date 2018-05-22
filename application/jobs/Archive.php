a31ff0733561ef6af73811fa46149d9dd8695509
Ben Williams <ben@flex.io>
2018-05-22 14:20:33 +0200

    added stub archive and unarchive jobs

---
+++ application/jobs/Archive.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *
+ * Copyright (c) 2018, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Benjamin I. Williams
+ * Created:  2018-05-22
+ *
+ * @package flexio
+ * @subpackage Jobs
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Jobs;
+
+
+
+class Archive extends \Flexio\Jobs\Base
+{
+    public function run(\Flexio\IFace\IProcess $process) : void
+    {
+        parent::run($process);
+
+        $instream = $process->getStdin();
+        $outstream = $process->getStdout();
+
+        $outstream->copyFrom($instream);
+
+        $params = $this->getJobParameters();
+        $msg = $params['msg'] ?? '';
+
+        if (is_array($msg) || is_object($msg))
+        {
+            $msg = @json_encode($msg);
+        }
+
+        echo $msg;
+    }
+}
---
+++ application/jobs/Unarchive.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ *
+ * Copyright (c) 2018, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Benjamin I. Williams
+ * Created:  2018-02-27
+ *
+ * @package flexio
+ * @subpackage Jobs
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Jobs;
+
+/*
+// DESCRIPTION:
+{
+    "op": "dump",  // string, required
+    "msg": ""      // string, required
+}
+
+// VALIDATOR:
+$validator = \Flexio\Base\Validator::create();
+if (($validator->check($params, array(
+        'op'         => array('type' => 'string',     'required' => true),
+        'msg'        => array('type' => 'string',     'required' => true)
+    ))->hasErrors()) === true)
+    throw new \Flexio\Base\Exception(\Flexio\Base\Error::INVALID_PARAMETER);
+*/
+
+class Dump extends \Flexio\Jobs\Base
+{
+    public function run(\Flexio\IFace\IProcess $process) : void
+    {
+        parent::run($process);
+
+        $instream = $process->getStdin();
+        $outstream = $process->getStdout();
+
+        $outstream->copyFrom($instream);
+
+        $params = $this->getJobParameters();
+        $msg = $params['msg'] ?? '';
+
+        if (is_array($msg) || is_object($msg))
+        {
+            $msg = @json_encode($msg);
+        }
+
+        echo $msg;
+    }
+}
