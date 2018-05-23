f27d77e3782e09a0b604edac4c35c1120d78227e
Ben Williams <ben@flex.io>
2018-05-23 18:27:24 +0200

    added openpyxl support to fxruntime Docker

--- application/jobs/Archive.php
+++ application/jobs/Archive.php
@@ -39,7 +39,7 @@ class Archive extends \Flexio\Jobs\Base
             $files = \Flexio\Base\Util::filterArrayEmptyValues(explode(',', $files));
 
         if ($format != 'zip')
-            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INVALID_PARAMETER, "Unknown value for 'format' parameter. ");
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INVALID_PARAMETER, "Unknown value for 'format' parameter.");
 
         $storage_tmpbase = $GLOBALS['g_config']->storage_root . DIRECTORY_SEPARATOR . 'tmp' . DIRECTORY_SEPARATOR;
         $archive_fname = $storage_tmpbase . "tmparchive-" . \Flexio\Base\Util::generateRandomString(30);
