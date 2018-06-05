ae97eee007565e052dcc7d668f634689b5401679
David Z. Williams <dave@flex.io>
2018-06-05 00:46:25 -0400

    Don't reduce button size in pipe header when stuck.

--- public/src/components/PipeDocument.vue
+++ public/src/components/PipeDocument.vue
@@ -525,8 +525,8 @@
   .sticky.js-is-stuck
     box-shadow: 0 4px 24px -8px rgba(0,0,0,0.2)
     h1
-      font-size: 1.25em
-    .el-button
-      padding: 9px 15px
+      font-size: 1.5em
+    //.el-button
+    //  padding: 9px 15px
 
 </style>
