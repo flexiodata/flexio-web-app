2955a2c482e34d89513de89b0c93b707c8c4c6c7
Aaron Williams <aaron@flex.io>
2018-06-07 17:04:14 -0500

    added some test data files

--- application/tests/jobs/08.10-convert-json.php
+++ application/tests/jobs/08.10-convert-json.php
@@ -18,19 +18,16 @@ namespace Flexio\Tests;
 
 class Test
 {
-    public function createConvertTask(string $delimiter, string $qualifier, bool $header)
+    public function createConvertTask()
     {
         $task = \Flexio\Tests\Task::create([
             [
                 "op" => "convert",
                 "input" => [
-                    "format" => "delimited",
-                    "delimiter" => "$delimiter",
-                    "qualifier" => "$qualifier",
-                    "header" => $header,
+                    "format" => "json"
                 ],
                 "output" => [
-                    "format" => "json"
+                    "format" => "table"
                 ]
             ]
         ]);
@@ -40,5 +37,76 @@ class Test
 
     public function run(&$results)
     {
+        // TEST: Convert JSON; empty file
+
+        // BEGIN TEST
+        $task = self::createConvertTask();
+        $stream = \Flexio\Tests\Util::createStream('/json/01.01-empty.json');
+        $process = \Flexio\Jobs\Process::create()->setStdin($stream)->execute($task);
+        $actual = $process->getStdout()->getStructure()->getNames();
+        $expected = [];
+        \Flexio\Tests\Check::assertArray('A.1', 'Convert JSON; empty file',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $task = self::createConvertTask();
+        $stream = \Flexio\Tests\Util::createStream('/json/01.01-empty.json');
+        $process = \Flexio\Jobs\Process::create()->setStdin($stream)->execute($task);
+        $actual = \Flexio\Tests\Content::getRows($process->getStdout());
+        $expected = [];
+        \Flexio\Tests\Check::assertArray('A.2', 'Convert JSON; empty file',  $actual, $expected, $results);
+
+
+        // TEST: Convert JSON; malformed file
+
+        // BEGIN TEST
+        $task = self::createConvertTask();
+        $stream = \Flexio\Tests\Util::createStream('/json/01.02-malformed.json');
+        $process = \Flexio\Jobs\Process::create()->setStdin($stream)->execute($task);
+        $actual = $process->getStdout()->getStructure()->getNames();
+        $expected = [];
+        \Flexio\Tests\Check::assertArray('B.1', 'Convert JSON; malformed file',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $task = self::createConvertTask();
+        $stream = \Flexio\Tests\Util::createStream('/json/01.02-malformed.json');
+        $process = \Flexio\Jobs\Process::create()->setStdin($stream)->execute($task);
+        $actual = \Flexio\Tests\Content::getRows($process->getStdout());
+        $expected = [];
+        \Flexio\Tests\Check::assertArray('B.2', 'Convert JSON; malformed file',  $actual, $expected, $results);
+
+
+        // TEST: Convert JSON; simple file
+
+        // BEGIN TEST
+        $task = self::createConvertTask();
+        $stream = \Flexio\Tests\Util::createStream('/json/01.03-simple.json');
+        $process = \Flexio\Jobs\Process::create()->setStdin($stream)->execute($task);
+        $actual = $process->getStdout()->getStructure()->getNames();
+        $expected = [];
+        \Flexio\Tests\Check::assertArray('C.1', 'Convert JSON; simple file',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $task = self::createConvertTask();
+        $stream = \Flexio\Tests\Util::createStream('/json/01.03-simple.json');
+        $process = \Flexio\Jobs\Process::create()->setStdin($stream)->execute($task);
+        $actual = \Flexio\Tests\Content::getRows($process->getStdout());
+        $expected = [];
+        \Flexio\Tests\Check::assertArray('C.2', 'Convert JSON; simple file',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $task = self::createConvertTask();
+        $stream = \Flexio\Tests\Util::createStream('/json/01.04-simple.json');
+        $process = \Flexio\Jobs\Process::create()->setStdin($stream)->execute($task);
+        $actual = $process->getStdout()->getStructure()->getNames();
+        $expected = [];
+        \Flexio\Tests\Check::assertArray('C.3', 'Convert JSON; simple file',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $task = self::createConvertTask();
+        $stream = \Flexio\Tests\Util::createStream('/json/01.04-simple.json');
+        $process = \Flexio\Jobs\Process::create()->setStdin($stream)->execute($task);
+        $actual = \Flexio\Tests\Content::getRows($process->getStdout());
+        $expected = [];
+        \Flexio\Tests\Check::assertArray('C.4', 'Convert JSON; simple file',  $actual, $expected, $results);
     }
 }
