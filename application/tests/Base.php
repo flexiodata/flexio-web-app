79f83622d236c0070f73ff505a5d28b2e1fb2321
Ben Williams <ben@flex.io>
2018-06-13 22:24:18 +0200

    added google cloud storage to test suite

--- application/tests/Base.php
+++ application/tests/Base.php
@@ -34,6 +34,7 @@ class Base
     public const STORAGE_BOX = 'testsuite-box';
     public const STORAGE_DROPBOX = 'testsuite-dropbox';
     public const STORAGE_GOOGLEDRIVE = 'testsuite-googledrive';
+    public const STORAGE_GOOGLECLOUDSTORAGE = 'testsuite-googlecloudstorage';
     public const STORAGE_SFTP = 'testsuite-sftp';
 
     public const STORAGE_GITHUB = 'testsuite-github';
