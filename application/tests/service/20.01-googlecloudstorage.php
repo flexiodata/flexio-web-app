eba34fdfa38f43f94f4290ce5d2858d9fda787c8
Ben Williams <ben@flex.io>
2018-06-13 22:08:26 +0200

    added test for GCS

---
+++ application/tests/service/20.01-googlecloudstorage.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ *
+ * Copyright (c) 2017, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2017-11-11
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // TODO: add additional tests
+
+        // TEST: service creation
+
+        // BEGIN TEST
+        $service = new \Flexio\Services\GoogleCloudStorage;
+        $actual = get_class($service);
+        $expected = 'Flexio\Services\GoogleCloudStorage';
+        \Flexio\Tests\Check::assertString('A.1', 'new \Flexio\Services\GoogleCloudStorage; basic file syntax check',  $actual, $expected, $results);
+    }
+}
