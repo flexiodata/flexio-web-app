20549a9c341f6cd0c70a85d8264bb6657e935e32
David Z. Williams <dave@flex.io>
2018-06-14 15:41:36 -0400

    Builder now serializes values to pipe.

--- public/src/components/PipeBuilderList.vue
+++ public/src/components/PipeBuilderList.vue
@@ -37,15 +37,28 @@
         immediate: true
       }
     },
+    data() {
+      return {
+        is_editing: false,
+        task_items: []
+      }
+    },
     computed: {
       ...mapState({
+        builder_prompts: state => state.builder.prompts,
         orig_pipe: state => state.pipe.orig_pipe
       })
     },
     methods: {
       updateFromTask(task) {
+        var tasks = []
         var prompts = []
-        _.each(task.items, (t) => {
+
+        if (this.is_editing) {
+          return
+        }
+
+        _.each(task.items, (t, task_idx) => {
           var item
 
           if (t.op == '') {
@@ -57,12 +70,18 @@
           }
 
           if (item) {
-            prompts = prompts.concat(item.prompts)
+            tasks.push(_.cloneDeep(t))
+
+            // associate prompts with tasks
+            _.each(item.prompts, (p) => {
+              prompts.push(_.assign({ task_idx }, p))
+            })
           }
         })
 
         this.$store.commit('builder/SET_MODE', 'build')
         this.$store.commit('builder/INIT_DEF', { prompts })
+        this.task_items = tasks
       },
       insertStep(idx) {
         var items = _.cloneDeep(this.value.items)
@@ -72,15 +91,28 @@
           this.$store.commit('builder/SET_ACTIVE_ITEM', idx)
         })
       },
-      itemChange(prompt, idx) {
-        // TODO: do we want to handle this here?
+      itemChange(prompt_values, prompt_idx) {
+        var p = this.builder_prompts[prompt_idx]
+        if (p) {
+          var t = _.get(this.task_items, '['+p.task_idx+ ']', null)
+          if (t) {
+            t = _.assign(t, prompt_values)
+            var items = _.cloneDeep(this.task_items)
+            _.set(items, '['+p.task_idx+ ']', t)
+            this.task_items = [].concat(items)
+            this.is_editing = true
+            this.$emit('input', { op: 'sequence', items })
+          }
+        }
       },
-      itemCancel(prompt_idx) {
+      itemCancel() {
         var value = _.get(this.orig_pipe, 'task', { op: 'sequence', items: [] })
+        this.is_editing = false
         this.$emit('input', value)
         this.$store.commit('builder/UNSET_ACTIVE_ITEM')
       },
-      itemSave(prompt_idx) {
+      itemSave() {
+        this.is_editing = false
         this.$store.commit('builder/UNSET_ACTIVE_ITEM')
       }
     }
