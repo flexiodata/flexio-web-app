8b354d5cc0744dac0ebae4ae956bce0701043992
David Z. Williams <dave@flex.io>
2018-06-20 11:45:37 -0400

    Removed numbers in pipe builder.

--- public/src/components/PipeBuilderList.vue
+++ public/src/components/PipeBuilderList.vue
@@ -5,6 +5,7 @@
       :container-id="containerId"
       :active-item-idx.sync="active_prompt_idx"
       :show-insert-buttons="true"
+      :show-numbers="false"
       @task-chooser-select-task="selectNewTask"
       @insert-step="insertStep"
       @delete-step="deleteStep"
