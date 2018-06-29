6ad85d585a6e0d933b0b3cdebd59721e6f2a76a9
David Z. Williams <dave@flex.io>
2018-06-29 16:09:27 -0400

    Added write task builder item.

---
+++ public/src/components/BuilderItemTaskWrite.vue
@@ -0,0 +1,209 @@
+<template>
+  <div>
+    <div
+      class="tl pb3"
+      v-if="title.length > 0"
+    >
+      <h3 class="fw6 f3 mid-gray mt0 mb2">{{title}}</h3>
+    </div>
+    <div
+      class="pb3 mid-gray marked"
+      v-html="description"
+      v-show="show_description"
+    >
+    </div>
+    <h4 class="mid-gray">1. Choose connection</h4>
+    <BuilderComponentConnectionChooser
+      class="mb3"
+      :connection-identifier.sync="connection_identifier"
+      :show-result="has_available_connection"
+    >
+      <el-button
+        slot="buttons"
+        plain
+        size="tiny"
+        class="ttu b"
+        @click="clearConnection"
+      >
+        Use Different Connection
+      </el-button>
+    </BuilderComponentConnectionChooser>
+    <template v-if="has_available_connection">
+      <h4 class="mid-gray">2. Choose location</h4>
+      <el-input
+        v-model="edit_values.path"
+      >
+        <el-button
+          slot="append"
+          class="ttu b"
+          type="primary"
+          size="small"
+          @click="show_file_chooser_dialog = true"
+        >
+          Browse
+        </el-button>
+      </el-input>
+    </template>
+
+    <!-- file chooser dialog -->
+    <el-dialog
+      custom-class="el-dialog--compressed-body"
+      title="Choose location"
+      width="51rem"
+      top="8vh"
+      :modal-append-to-body="false"
+      :visible.sync="show_file_chooser_dialog"
+    >
+      <BuilderComponentFileChooser
+        ref="file-chooser"
+        :connection-identifier="connection_identifier"
+        :allow-multiple="false"
+        :show-result="false"
+        v-if="show_file_chooser_dialog"
+      />
+      <span slot="footer" class="dialog-footer">
+        <el-button
+          class="ttu b"
+          @click="show_file_chooser_dialog = false"
+        >
+          Cancel
+        </el-button>
+        <el-button
+          class="ttu b"
+          type="primary"
+          @click="updatePath"
+        >
+          Choose location
+        </el-button>
+      </span>
+    </el-dialog>
+  </div>
+</template>
+
+<script>
+  import marked from 'marked'
+  import { mapGetters } from 'vuex'
+  import { CONNECTION_STATUS_AVAILABLE } from '../constants/connection-status'
+  import BuilderComponentConnectionChooser from './BuilderComponentConnectionChooser.vue'
+  import BuilderComponentFileChooser from './BuilderComponentFileChooser.vue'
+  import MixinConnection from './mixins/connection'
+
+  const getDefaultValues = () => {
+    return {
+      op: 'write',
+      path: ''
+    }
+  }
+
+  export default {
+    props: {
+      item: {
+        type: Object,
+        required: true
+      },
+      index: {
+        type: Number,
+        required: true
+      },
+      activeItemIdx: {
+        type: Number,
+        required: true
+      },
+      isNextAllowed: {
+        type: Boolean,
+        required: true
+      }
+    },
+    mixins: [MixinConnection],
+    components: {
+      BuilderComponentConnectionChooser,
+      BuilderComponentFileChooser
+    },
+    watch: {
+      item: {
+        handler: 'initSelf',
+        immediate: true,
+        deep: true
+      },
+      has_available_connection() {
+        this.$emit('update:isNextAllowed', this.has_available_connection)
+      },
+      connection_identifier() {
+        this.$emit('active-item-change', this.index)
+      },
+      edit_values: {
+        handler: 'onEditValuesChange',
+        immediate: true,
+        deep: true
+      }
+    },
+    data() {
+      return {
+        connection_identifier: '',
+        orig_values: getDefaultValues(),
+        edit_values: getDefaultValues(),
+        show_file_chooser_dialog: false
+      }
+    },
+    computed: {
+      show_description() {
+        return this.description.length > 0
+      },
+      title() {
+        return _.get(this.item, 'title', 'Read')
+      },
+      description() {
+        return marked(_.get(this.item, 'description', ''))
+      },
+      store_connection() {
+        return this.$_Connection_getConnectionByIdentifier(this.connection_identifier)
+      },
+      has_available_connection() {
+        return _.get(this.store_connection, 'connection_status', '') == CONNECTION_STATUS_AVAILABLE
+      }
+    },
+    methods: {
+      initSelf() {
+        var form_values = _.get(this.item, 'form_values', {})
+
+        // set connection identifier
+        var path = _.get(form_values, 'path', '')
+        if (path.length == 0) {
+          this.connection_identifier = ''
+        } else {
+          if (_.isArray(path)) {
+            path = _.get(path, '[0]', '')
+          }
+
+          path = path.substring(1)
+          var cid = path.substring(0, path.indexOf('/'))
+          this.connection_identifier = cid
+        }
+
+        var values = _.assign({}, getDefaultValues(), form_values)
+        this.orig_values = _.cloneDeep(values)
+        this.edit_values = _.cloneDeep(values)
+
+        this.$emit('update:isNextAllowed', this.has_available_connection)
+      },
+      clearConnection() {
+        this.connection_identifier = ''
+        this.edit_values = _.assign({}, this.edit_values, { path: '' })
+      },
+      onEditValuesChange() {
+        if (_.isEqual(this.edit_values, this.orig_values)) {
+          return
+        }
+
+        this.$emit('active-item-change', this.index)
+        this.$emit('item-change', this.edit_values, this.index)
+      },
+      updatePath() {
+        var files = this.$refs['file-chooser'].getSelectedFiles()
+        files = _.get(files, '[0].path', '')
+        this.edit_values = _.assign({}, this.edit_values, { path: files })
+        this.show_file_chooser_dialog = false
+      }
+    }
+  }
+</script>
