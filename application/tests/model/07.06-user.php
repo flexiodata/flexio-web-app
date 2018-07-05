7ad3b3f5326acb44b6fb02d23ff079c478f20533
Aaron Williams <aaron@flex.io>
2018-07-05 15:16:32 -0500

    added some model tests

--- application/tests/model/07.06-user.php
+++ application/tests/model/07.06-user.php
@@ -20,5 +20,77 @@ class Test
 {
     public function run(&$results)
     {
+        // FUNCTION: \Flexio\Model\User::purge()
+
+
+        // SETUP
+        $model = \Flexio\Tests\Util::getModel()->user;
+
+
+        // TEST: non-eid input
+
+        // BEGIN TEST
+        $actual = '';
+        try
+        {
+            $model->purge(null);
+            $actual = \Flexio\Tests\Base::ERROR_NO_EXCEPTION;
+        }
+        catch (\Error $e)
+        {
+            $actual = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        }
+        $expected = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        \Flexio\Tests\Check::assertString('A.1', '\Flexio\Model\User::purge(); throw an error with null input',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $actual = $model->purge('');
+        $expected = false;
+        \Flexio\Tests\Check::assertBoolean('A.2', '\Flexio\Model\User::purge(); return false with invalid input',  $actual, $expected, $results);
+
+
+        // TEST: valid eid input, but object doesn't exist
+
+        // BEGIN TEST
+        $eid = \Flexio\Base\Eid::generate();
+        $actual = $model->purge($eid);
+        $expected = false;
+        \Flexio\Tests\Check::assertBoolean('B.1', '\Flexio\Model\User::purge(); return false after trying to purge an object that doesn\'t exist',  $actual, $expected, $results);
+
+
+        // TEST: valid eid input, and object exists
+
+        // BEGIN TEST
+        $info = array(
+            'username' => \Flexio\Base\Util::generateHandle(),
+            'email' => \Flexio\Tests\Util::createEmailAddress()
+        );
+        $eid = $model->create($info);
+        $model->set($eid, array('owned_by' => $eid));
+        $actual = $model->purge($eid);
+        $expected = true;
+        \Flexio\Tests\Check::assertBoolean('C.1', '\Flexio\Model\User::purge(); return true when purging an object that exists',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $info1 = array(
+            'username' => \Flexio\Base\Util::generateHandle(),
+            'email' => \Flexio\Tests\Util::createEmailAddress()
+        );
+        $eid1 = $model->create($info1);
+        $model->set($eid1, array('owned_by' => $eid1));
+        $info2 = array(
+            'username' => \Flexio\Base\Util::generateHandle(),
+            'email' => \Flexio\Tests\Util::createEmailAddress()
+        );
+        $eid2 = $model->create($info2);
+        $model->set($eid2, array('owned_by' => $eid2));
+        $info1_before_deletion = $model->get($eid1);
+        $info2_before_deletion = $model->get($eid2);
+        $delete1_result = $model->purge($eid1);
+        $info1_after_deletion = $model->get($eid1);
+        $info2_after_deletion = $model->get($eid2);
+        $actual = $info1_before_deletion['eid'] === $eid1 && $info1_after_deletion === false && $info2_before_deletion['eid'] === $eid2 && $info2_after_deletion['eid'] === $eid2;
+        $expected = true;
+        \Flexio\Tests\Check::assertBoolean('C.2', '\Flexio\Model\User::purge(); when purging, make sure object being purged is physically and that other objects are not effected',  $actual, $expected, $results);
     }
 }
--- application/tests/model/10.06-connection.php
+++ application/tests/model/10.06-connection.php
@@ -20,5 +20,68 @@ class Test
 {
     public function run(&$results)
     {
+        // FUNCTION: \Flexio\Model\Connection::purge()
+
+
+        // SETUP
+        $model = \Flexio\Tests\Util::getModel()->connection;
+
+
+        // TEST: non-eid input
+
+        // BEGIN TEST
+        $actual = '';
+        try
+        {
+            $model->purge(null);
+            $actual = \Flexio\Tests\Base::ERROR_NO_EXCEPTION;
+        }
+        catch (\Error $e)
+        {
+            $actual = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        }
+        $expected = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        \Flexio\Tests\Check::assertString('A.1', '\Flexio\Model\Connection::purge(); throw an error with null input',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $actual = $model->purge('');
+        $expected = false;
+        \Flexio\Tests\Check::assertBoolean('A.2', '\Flexio\Model\Connection::purge(); return false with invalid input',  $actual, $expected, $results);
+
+
+        // TEST: valid eid input, but object doesn't exist
+
+        // BEGIN TEST
+        $eid = \Flexio\Base\Eid::generate();
+        $actual = $model->purge($eid);
+        $expected = false;
+        \Flexio\Tests\Check::assertBoolean('B.1', '\Flexio\Model\Connection::purge(); return false after trying to purge an object that doesn\'t exist',  $actual, $expected, $results);
+
+
+        // TEST: valid eid input, and object exists
+
+        // BEGIN TEST
+        $info = array();
+        $eid = $model->create($info);
+        $model->set($eid, array('owned_by' => $eid));
+        $actual = $model->purge($eid);
+        $expected = true;
+        \Flexio\Tests\Check::assertBoolean('C.1', '\Flexio\Model\Connection::purge(); return true when purging an object that exists',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $info1 = array();
+        $eid1 = $model->create($info1);
+        $model->set($eid1, array('owned_by' => $eid1));
+        $info2 = array();
+        $eid2 = $model->create($info2);
+        $model->set($eid2, array('owned_by' => $eid2));
+        $info1_before_deletion = $model->get($eid1);
+        $info2_before_deletion = $model->get($eid2);
+        $delete1_result = $model->purge($eid1);
+        $info1_after_deletion = $model->get($eid1);
+        $info2_after_deletion = $model->get($eid2);
+        $actual = $info1_before_deletion['eid'] === $eid1 && $info1_after_deletion === false && $info2_before_deletion['eid'] === $eid2 && $info2_after_deletion['eid'] === $eid2;
+        $expected = true;
+        \Flexio\Tests\Check::assertBoolean('C.2', '\Flexio\Model\Connection::purge(); when purging, make sure object being purged is physically and that other objects are not effected',  $actual, $expected, $results);
     }
 }
