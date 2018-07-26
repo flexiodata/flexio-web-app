d8b519d77e994c2adfa0b3a47d018d4d2b29abc0
Ben Williams <ben@flex.io>
2018-07-27 00:43:25 +0200

    fixed copy tests (added process owner, which was missing)

--- application/tests/jobs/21.01-copy.php
+++ application/tests/jobs/21.01-copy.php
@@ -26,18 +26,27 @@ class Test
         $source_directory = '/home/' . $test_folder . '/';
         $target_directory = "/" . \Flexio\Tests\Base::STORAGE_DROPBOX . "/" . $test_folder . '/';
 
+        $process_owner = \Flexio\Tests\Util::getTestStorageOwner();
+
+        $idx = 1;
         foreach ($files as $filename) // copy files into home directory
         {
-            $source_filepath = \Flexio\Tests\Util::getOutputFilePath($source_directory, $filename);
-            $read = \Flexio\Tests\Task::create([["op" => "read", "path" => $source_filepath]]);
-            $write = \Flexio\Tests\Task::create([["op" => "write", "path" => $source_filepath]]);
             $stream = \Flexio\Tests\Util::createStreamFromFile($filename);
-            $process_write = \Flexio\Jobs\Process::create()->setStdin($stream)->execute($write);
-            $process_read = \Flexio\Jobs\Process::create()->execute($read);
+
+            $source_filepath = \Flexio\Tests\Util::getOutputFilePath($source_directory, $filename);
+            $write = \Flexio\Tests\Task::create([["op" => "write", "path" => $source_filepath]]);   
+            $process_write = \Flexio\Jobs\Process::create()->setOwner($process_owner)->setStdin($stream)->execute($write);
+
+            $read = \Flexio\Tests\Task::create([["op" => "read", "path" => $source_filepath]]);
+            $process_read = \Flexio\Jobs\Process::create()->setOwner($process_owner)->execute($read);
             $actual_contents = \Flexio\Base\Util::getStreamContents($process_read->getStdout());
+
             $expected_contents = \Flexio\Base\Util::getStreamContents($stream);
             $actual = md5($actual_contents);
             $expected = md5($expected_contents);
+
+            \Flexio\Tests\Check::assertString("A.$idx", 'Read/Write; check write/read to/from ' . $source_filepath, $actual, $expected, $results);
+            $idx++;
         }
 
 
@@ -50,6 +59,8 @@ class Test
         $process_list_target = \Flexio\Jobs\Process::create()->execute($list_target);
         $actual_contents = \Flexio\Base\Util::getStreamContents($process_list_target->getStdout());
         $expected_contents = \Flexio\Base\Util::getStreamContents($process_list_source->getStdout());
-        \Flexio\Tests\Check::assertArray("A.1", "Copy; compare contents of copied folder $target_directory with source folder $source_directory", $actual, $expected, $results);
+
+
+        \Flexio\Tests\Check::assertString("A.1", "Copy; compare contents of copied folder $target_directory with source folder $source_directory", $actual, $expected, $results);
     }
 }
