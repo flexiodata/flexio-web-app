13649efc442e196bf4d69a14e5da0365a8ec8375
Aaron Williams <aaron@flex.io>
2018-08-13 12:21:59 -0500

    use base email object to store email properties in notice email service implementation

--- application/services/NoticeEmail.php
+++ application/services/NoticeEmail.php
@@ -20,58 +20,53 @@ class NoticeEmail
 {
     private const EMAIL_ADDRESS_NO_REPLY = 'no-reply@flex.io';
 
-    private $from_addresses = array();
-    private $to_addresses = array();
-    private $cc_addresses = array();
-    private $bcc_addresses = array();
-    private $replyto_addresses = array();
-    private $subject = '';
-    private $msg_text = '';
-    private $msg_html = '';
-    private $msg_htmlembedded = '';
-    private $attachments = array();
+    private $email; // email properties (e.g. from, to, subject, etc)
+    private $_ses;
 
-    private $_ses = null;
+    public function __construct()
+    {
+        $this->email = \Flexio\Base\Email::create();
+        $this->_ses = null;
+    }
 
     public static function create(array $params = null) : \Flexio\Services\NoticeEmail
     {
-        $email = new self;
-
-        if (!isset($params))
-            return $email;
+        $service = new self;
 
         if (isset($params['from']))
-            $email->setFrom($params['from']);
+            $service->setFrom($params['from']);
         if (isset($params['to']))
-            $email->setTo($params['to']);
+            $service->setTo($params['to']);
         if (isset($params['cc']))
-            $email->setCC($params['cc']);
+            $service->setCC($params['cc']);
         if (isset($params['bcc']))
-            $email->setBCC($params['bcc']);
+            $service->setBCC($params['bcc']);
         if (isset($params['reply_to']))
-            $email->setReplyTo($params['reply_to']);
+            $service->setReplyTo($params['reply_to']);
         if (isset($params['subject']))
-            $email->setSubject($params['subject']);
+            $service->setSubject($params['subject']);
         if (isset($params['msg_text']))
-            $email->setMessageText($params['msg_text']);
+            $service->setMessageText($params['msg_text']);
         if (isset($params['msg_html']))
-            $email->setMessageHtml($params['msg_html']);
+            $service->setMessageHtml($params['msg_html']);
 
         if (isset($params['attachments']) && is_array($params['attachments']))
         {
             $attachments = $params['attachments'];
             foreach ($attachments as $a)
             {
-                $email->addAttachment($a);
+                $service->addAttachment($a);
             }
         }
 
-        return $email;
+        return $service;
     }
 
     public function send() : bool
     {
-        if (count($this->attachments) > 0)
+        $attachments = $this->getAttachments();
+
+        if (count($attachments) > 0)
             return $this->sendWithAttachments();
              else
             return $this->sendWithoutAttachments();
@@ -79,158 +74,117 @@ class NoticeEmail
 
     public function setFrom($addresses) : \Flexio\Services\NoticeEmail // TODO: add parameter type
     {
-        // make sure we have an array
-        if (is_string($addresses))
-            $addresses = explode(',', $addresses);
-        if (!is_array($addresses))
-            return $this;
-
-        // set the addresses
-        $this->from_addresses = self::getCleanedAddresses($addresses);
+        $this->email->setFrom($addresses);
         return $this;
     }
 
     public function getFrom() : array
     {
-        return $this->from_addresses;
+        return $this->email->getFrom();
     }
 
     public function setTo($addresses) : \Flexio\Services\NoticeEmail // TODO: add parameter type
     {
-        // make sure we have an array
-        if (is_string($addresses))
-            $addresses = explode(',', $addresses);
-        if (!is_array($addresses))
-            return $this;
-
-        // set the addresses
-        $this->to_addresses = self::getCleanedAddresses($addresses);
+        $this->email->setTo($addresses);
         return $this;
     }
 
     public function getTo() : array
     {
-        return $this->to_addresses;
+        return $this->email->getTo();
     }
 
     public function setCC($addresses) : \Flexio\Services\NoticeEmail // TODO: add parameter type
     {
-        // make sure we have an array
-        if (is_string($addresses))
-            $addresses = explode(',', $addresses);
-        if (!is_array($addresses))
-            return $this;
-
-        // set the addresses
-        $this->cc_addresses = self::getCleanedAddresses($addresses);
+        $this->email->setCC($addresses);
         return $this;
     }
 
     public function getCC() : array
     {
-        return $this->cc_addresses;
+        return $this->email->getCC();
     }
 
     public function setBCC($addresses) : \Flexio\Services\NoticeEmail // TODO: add parameter type
     {
-        // make sure we have an array
-        if (is_string($addresses))
-            $addresses = explode(',', $addresses);
-        if (!is_array($addresses))
-            return $this;
-
-        // set the addresses
-        $this->bcc_addresses = self::getCleanedAddresses($addresses);
+        $this->email->setBCC($addresses);
         return $this;
     }
 
     public function getBCC() : array
     {
-        return $this->bcc_addresses;
+        return $this->email->getBCC();
     }
 
     public function setReplyTo($addresses) : \Flexio\Services\NoticeEmail // TODO: add parameter type
     {
-        // make sure we have an array
-        if (is_string($addresses))
-            $addresses = explode(',', $addresses);
-        if (!is_array($addresses))
-            return $this;
-
-        // set the addresses
-        $this->replyto_addresses = self::getCleanedAddresses($addresses);
+        $this->email->setReplyTo($addresses);
         return $this;
     }
 
     public function getReplyTo() : array
     {
-        return $this->replyto_addresses;
+        return $this->email->getReplyTo();
     }
 
     public function setSubject(string $subject) : \Flexio\Services\NoticeEmail
     {
-        $this->subject = $subject;
+        $this->email->setSubject($subject);
         return $this;
     }
 
     public function getSubject() : string
     {
-        return $this->subject;
+        return $this->email->getSubject();
     }
 
     public function setMessageText(string $message) : \Flexio\Services\NoticeEmail
     {
-	    $this->msg_text = $message;
+        $this->email->setMessageText($message);
         return $this;
     }
 
     public function getMessageText() : string
     {
-        return $this->msg_text;
+        return $this->email->getMessageText();
     }
 
     public function setMessageHtml(string $message) : \Flexio\Services\NoticeEmail
     {
-        $this->msg_html = $message;
+        $this->email->setMessageHtml($message);
         return $this;
     }
 
     public function getMessageHtml() : string
     {
-        return $this->msg_html;
+        return $this->email->getMessageHtml();
     }
 
     public function setMessageHtmlEmbedded(string $message) : \Flexio\Services\NoticeEmail
     {
-        $this->msg_htmlembedded = $message;
+        $this->email->setMessageHtmlEmbedded($message);
         return $this;
     }
 
     public function getMessageHtmlEmbedded() : string
     {
-        return $this->msg_htmlembedded;
+        return $this->email->getMessageHtmlEmbedded();
     }
 
     public function addAttachment(array $attachment) : \Flexio\Services\NoticeEmail
     {
-        $a = array();
-        $a['name'] = $attachment['name'] ?? '';
-        $a['file'] = $attachment['file'] ?? '';
-        $a['mime_type'] = $attachment['mime_type'] ?? \Flexio\Base\ContentType::UNDEFINED;
-        $a['content'] = $attachment['content'] ?? '';
-
-        $this->attachments[] = $a;
+        $this->email->addAttachment($attachment);
         return $this;
     }
 
     public function getAttachments() : array
     {
-        return $this->attachments;
+        return $this->email->getAttachments();
     }
 
     public function clearAttachments() : \Flexio\Services\NoticeEmail
     {
-        $this->attachments = array();
+        $this->email->clearAttachments();
         return $this;
     }
 
@@ -241,44 +195,53 @@ class NoticeEmail
             return false;
 
         // make sure we have at least one valid from and to address
-        if (count($this->from_addresses) === 0)
+        $from_addresses = $this->getFrom();
+        if (count($from_addresses) === 0)
             return false;
-        if (count($this->to_addresses) === 0)
+
+        $to_addresses = $this->getTo();
+        if (count($to_addresses) === 0)
             return false;
 
         // if we don't have a reply-to address, then supply it
-        if (count($this->replyto_addresses) === 0)
+        $replyto_addresses - $this->getReplyTo();
+        if (count($replyto_addresses) === 0)
             $this->setReplyTo(self::EMAIL_ADDRESS_NO_REPLY);
 
         // build up the destination array
         $destination = array();
-        $destination['ToAddresses'] = $this->to_addresses;
-        if (count($this->cc_addresses) > 0)
-            $destination['CcAddresses'] = $this->cc_addresses;
-        if (count($this->bcc_addresses) > 0)
-            $destination['BccAddresses'] = $this->bcc_addresses;
+        $destination['ToAddresses'] = $this->getTo();
+
+        $cc_addresses = $this->getCC();
+        if (count($cc_addresses) > 0)
+            $destination['CcAddresses'] = $cc_addresses;
+
+        $bcc_addresses = $this->getBCC();
+        if (count($bcc_addresses) > 0)
+            $destination['BccAddresses'] = $bcc_addresses;
 
         // build up the message array
         $message = array(
             'Subject' => array(
-                'Data' => $this->subject,
+                'Data' => $this->getSubject(),
             ),
             'Body' => array(
                 'Text' => array(
-                    'Data' => $this->msg_text,
+                    'Data' => $this->getMessageText(),
                 )
             ),
         );
 
-        if (strlen($this->msg_html) > 0)
-            $message['Body']['Html'] = array('Data' => $this->msg_html);
+        $msg_html = $this->getMessageHtml();
+        if (strlen($msg_html) > 0)
+            $message['Body']['Html'] = array('Data' => $msg_html);
 
         // create email array
         $mail = array(
-            'Source' => implode(',', $this->from_addresses),
+            'Source' => implode(',', $this->getFrom()),
             'Destination' => $destination,
             'Message' => $message,
-            'ReplyToAddresses' => $this->replyto_addresses
+            'ReplyToAddresses' => implode(',', $this->getReplyTo())
             //'ReturnPath' => array()
         );
 
@@ -304,26 +267,35 @@ class NoticeEmail
         $old_error_settings = error_reporting(E_ALL ^ E_STRICT);
 
         $mail_mime = new \Mail_mime(array('eol' => "\n"));
-        $mail_mime->setTxtBody($this->msg_text);
-        $mail_mime->setHTMLBody($this->msg_html);
-        foreach ($this->to_addresses as $to)
+        $mail_mime->setTxtBody($this->getMessageText());
+        $mail_mime->setHTMLBody($this->getMessageHtml());
+
+        $to_addresses = $this->getTo();
+        foreach ($to_addresses as $to)
         {
             $mail_mime->addTo($to);
         }
-        foreach ($this->cc_addresses as $cc)
+
+        $cc_addresses = $this->getCC();
+        foreach ($cc_addresses as $cc)
         {
             $mail_mime->addCc($cc);
         }
-        foreach ($this->bcc_addresses as $bcc)
+
+        $bcc_addresses = $this->getBCC();
+        foreach ($bcc_addresses as $bcc)
         {
             $mail_mime->addBcc($bcc);
         }
-        foreach ($this->attachments as $attachment)
+
+        $attachments = $this->getAttachments();
+        foreach ($attachments as $a)
         {
-            $mail_mime->addAttachment($attachment['file'], $attachment['mime_type'], $attachment['name']);
+            $mail_mime->addAttachment($a['file'], $a['mime_type'], $a['name']);
         }
+
         $body = $mail_mime->get();
-        $headers = $mail_mime->txtHeaders(array('Subject' => $this->subject));
+        $headers = $mail_mime->txtHeaders(array('Subject' => $this->getSubject()));
         $message = $headers . "\r\n" . $body;
 
         // restore original error settings
@@ -332,14 +304,18 @@ class NoticeEmail
         // create email array
 
         $destination = array();
-        $destination['ToAddresses'] = implode(',',$this->to_addresses);
-        if (count($this->cc_addresses) > 0)
-            $destination['CcAddresses'] = implode(',',$this->cc_addresses);
-        if (count($this->bcc_addresses) > 0)
-            $destination['BccAddresses'] = implode(',',$this->bcc_addresses);
+        $destination['ToAddresses'] = implode(',', $this->getTo());
+
+        $cc_addresses = $this->getCC();
+        if (count($cc_addresses) > 0)
+            $destination['CcAddresses'] = implode(',', $cc_addresses);
+
+        $bcc_addresses = $this->getBCC();
+        if (count($bcc_addresses) > 0)
+            $destination['BccAddresses'] = implode(',', $bcc_addresses);
 
         $mail = array(
-            'Source' => implode(',',$this->from_addresses),
+            'Source' => implode(',', $this->getFrom()),
             'Destinations' => $destination,
             'RawMessage' => array(
                                 'Data' => $message
@@ -375,23 +351,5 @@ class NoticeEmail
         }
 
         return $this->_ses;
-    }
-
-    private static function getCleanedAddresses(array $addresses) : array
-    {
-        $cleaned_addresses = array();
-        foreach ($addresses as $a)
-        {
-            if (!is_string($a))
-                continue;
-
-            $a = trim($a);
-            if (strlen($a) === 0)
-                continue;
-
-            $cleaned_addresses[] = $a;
-        }
-
-        return $cleaned_addresses;
     }
 }
