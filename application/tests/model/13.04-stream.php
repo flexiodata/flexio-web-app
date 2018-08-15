40f6b217d83d5333de74be59a63f91ef6ac8d503
Aaron Williams <aaron@flex.io>
2018-08-15 16:27:58 -0500

    added some placeholder test files

---
+++ application/tests/model/13.04-stream.php
@@ -0,0 +1,25 @@
+<?php
+/**
+ *
+ * Copyright (c) 2018, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2018-08-15
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // FUNCTION: \Flexio\Model\Stream::purge()
+    }
+}
