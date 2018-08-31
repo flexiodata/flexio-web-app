f41e4be75abffe07a95b6f6d1505a1e437dc99bb
David Z. Williams <dave@flex.io>
2018-08-31 03:37:43 -0400

    Email task now has a formal component for the task list.

---
+++ public/src/components/BuilderItemTaskEmail.vue
@@ -0,0 +1,185 @@
+<template>
+  <div>
+    <div
+      class="tl pb3"
+      v-if="title.length > 0"
+    >
+      <h3 class="fw6 f3 mt0 mb2">{{title}}</h3>
+    </div>
+    <div
+      class="pb3 marked"
+      v-html="description"
+      v-show="show_description"
+    >
+    </div>
+    <el-form
+      ref="form"
+      class="flex-fill el-form--compact el-form__label-tiny"
+      label-position="top"
+      :model="edit_values"
+      :rules="rules"
+      @validate="onValidateItem"
+    >
+      <el-form-item
+        key="to"
+        label="To"
+        prop="to"
+      >
+        <el-input
+          placeholder="Enter email address"
+          autocomplete="off"
+          v-model="edit_values.to"
+        />
+      </el-form-item>
+      <el-form-item
+        key="subject"
+        label="Subject"
+        prop="subject"
+      >
+        <el-input
+          placeholder="Enter subject"
+          autocomplete="off"
+          v-model="edit_values.subject"
+        />
+      </el-form-item>
+      <el-form-item
+        key="body"
+        label="Message"
+        prop="body"
+      >
+        <el-input
+          type="textarea"
+          rows="4"
+          placeholder="Enter message"
+          autocomplete="off"
+          v-model="edit_values.body"
+        />
+      </el-form-item>
+    </el-form>
+  </div>
+</template>
+
+<script>
+  import marked from 'marked'
+
+  const getDefaultValues = () => {
+    return {
+      to: '',
+      subject: '',
+      body: ''
+    }
+  }
+
+  export default {
+    props: {
+      item: {
+        type: Object,
+        required: true
+      },
+      index: {
+        type: Number,
+        required: true
+      },
+      activeItemIdx: {
+        type: Number,
+        required: true
+      },
+      isNextAllowed: {
+        type: Boolean,
+        required: true
+      }
+    },
+    watch: {
+      item: {
+        handler: 'initSelf',
+        immediate: true,
+        deep: true
+      },
+      is_changed: {
+        handler: 'onChange'
+      },
+      edit_values: {
+        handler: 'onEditValuesChange',
+        immediate: true,
+        deep: true
+      },
+      form_errors(val) {
+        this.$emit('update:isNextAllowed', _.keys(val).length == 0)
+      }
+    },
+    data() {
+      return {
+        orig_values: getDefaultValues(),
+        edit_values: getDefaultValues(),
+        form_errors: {},
+        rules: {
+          to: [
+            { required: true, message: 'Please input an email address', trigger: 'blur' },
+            { type: 'email', message: 'Please input a valid email address', trigger: 'blur' }
+          ],
+          subject: [
+            { required: true, message: 'Please input a subject', trigger: 'blur' }
+          ],
+          body: [
+            { required: true, message: 'Please input a message', trigger: 'blur' }
+          ]
+        }
+      }
+    },
+    computed: {
+      show_description() {
+        return this.description.length > 0
+      },
+      title() {
+        return _.get(this.item, 'title', 'Connect')
+      },
+      description() {
+        return marked(_.get(this.item, 'description', ''))
+      },
+      is_changed() {
+        return !_.isEqual(this.edit_values, this.orig_values)
+      }
+    },
+    methods: {
+      initSelf() {
+        var form_values = _.get(this.item, 'form_values', {})
+        this.orig_values = _.assign({}, getDefaultValues(), form_values)
+        this.edit_values = _.assign({}, getDefaultValues(), form_values)
+        this.$nextTick(() => { this.validateForm(true) })
+      },
+      validateForm(clear) {
+        if (this.$refs.form) {
+          this.$refs.form.validate((valid) => {
+            this.$emit('update:isNextAllowed', valid)
+            if (clear === true) {
+              this.$refs.form.clearValidate()
+            }
+          })
+        }
+      },
+      onValidateItem(key, valid) {
+        var errors = _.assign({}, this.form_errors)
+        if (valid) {
+          errors = _.omit(errors, [key])
+        } else {
+          errors[key] = true
+        }
+        this.form_errors = _.assign({}, errors)
+      },
+      onChange(val) {
+        if (val) {
+          this.$emit('active-item-change', this.index)
+        }
+      },
+      onEditValuesChange() {
+        if (this.$refs.form) {
+          this.$refs.form.validate((valid) => {
+            this.$emit('update:isNextAllowed', valid)
+            this.$refs.form.clearValidate()
+          })
+        }
+        this.$emit('item-change', this.edit_values, this.index)
+      }
+    }
+  }
+</script>
