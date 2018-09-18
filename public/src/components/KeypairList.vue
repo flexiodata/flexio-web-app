ddf29d602a69f0aae6a5c20dc1aa47fe2f574ab1
David Z. Williams <dave@flex.io>
2018-09-18 12:00:36 -0400

    KeypairList now uses v-model.

--- public/src/components/KeypairList.vue
+++ public/src/components/KeypairList.vue
@@ -20,26 +20,64 @@
 <script>
   import KeypairItem from './KeypairItem.vue'
 
+  const newKeypairItem = (key, val) => {
+    key = _.defaultTo(key, '')
+    val = _.defaultTo(val, '')
+
+    return {
+      key,
+      val
+    }
+  }
+
   export default {
     props: {
+      value: {
+        type: Array,
+        required: true
+      },
       header: {
         type: [Boolean, Object],
         default: false
-      },
-      items: {
-        type: Array,
-        required: true
       }
     },
     components: {
       KeypairItem
     },
+    computed: {
+      items: {
+        get() {
+          return [].concat(this.value)
+        },
+        set() {
+          // do nothing
+        }
+      }
+    },
     methods: {
       onItemChange(item, index) {
+        var items = _.cloneDeep(this.value)
+
+        if (index == _.size(items) - 1) {
+          items = [].concat(items).concat(newKeypairItem())
+        }
+
+        var arr = [].concat(items)
+        arr[index] = _.assign({}, item)
+        items = [].concat(arr)
+
         this.$emit('item-change', item, index)
+        this.$emit('input', items)
       },
       onItemDelete(item, index) {
-        this.$emit('item-delete', item, index)
+        var tmp = _.cloneDeep(this.value)
+        _.pullAt(tmp, [index])
+
+        this.$nextTick(() => {
+          var items = [].concat(tmp)
+          this.$emit('item-delete', item, index)
+          this.$emit('input', items)
+        })
       }
     }
   }
