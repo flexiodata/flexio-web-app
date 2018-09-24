464bae3ececccc7158b3d5d653926866bc1fe78d
David Z. Williams <dave@flex.io>
2018-09-24 15:29:41 -0400

    Added child elements to admin prototyping area.

--- public/src/components/AdminPrototype.vue
+++ public/src/components/AdminPrototype.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    <div class="pa3">Component goes here...</div>
+  <div class="overflow-y-auto">
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a1">1</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a2">2</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a3">3</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a4">4</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a5">5</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a6">6</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a7">7</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a8">8</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a9">9</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a10">10</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a11">11</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a12">12</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a13">13</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a14">14</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a15">15</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a16">16</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a17">17</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a18">18</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a19">19</div>
+    <div class="ma3 pa3 br2 ba b--black-05 bg-near-white" ref="a20">20</div>
   </div>
 </template>
+
+<script>
+  export default {
+  }
+</script>
