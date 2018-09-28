ce2a0c142a58e9368f9a5566a7edf4923dfef6d1
David Z. Williams <dave@flex.io>
2018-09-28 15:16:00 -0400

    Added a little helper text to input area.

--- public/src/components/PipeDocument.vue
+++ public/src/components/PipeDocument.vue
@@ -161,7 +161,7 @@
                     </div>
                   </template>
                   <div class="pt3 ph3">
-                    <p class="mt0 ttu fw6 f7 moon-gray">Add optional pipe input below for testing the pipe api endpoint with POST parameters</p>
+                    <p class="mt0 ttu fw6 f7 moon-gray">Add optional pipe input for testing the pipe with POST parameters</p>
                     <ProcessInput
                       ref="process-input"
                       v-model="edit_input"
