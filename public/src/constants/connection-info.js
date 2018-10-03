4d6702490a9d92c6b2698453ee91d26f2a563e71
David Z. Williams <dave@flex.io>
2018-10-03 13:01:24 -0400

    Commented out connections we're not using.

--- public/src/constants/connection-info.js
+++ public/src/constants/connection-info.js
@@ -24,24 +24,13 @@ const BOX = {
   is_storage: true
 }
 
-const CUSTOM_API = {
+const API = {
   service_name: 'API',
   service_description: 'Connect to any REST API',
   connection_type: types.CONNECTION_TYPE_HTTP,
   icon: require('../assets/icon/icon-custom-api-128.png'),
   is_service: false,
   is_input: true,
-  is_output: false,
-  is_storage: false
-}
-
-const DOWNLOAD = {
-  service_name: 'Download',
-  service_description: 'Download files to your computer',
-  connection_type: types.CONNECTION_TYPE_DOWNLOAD,
-  icon: require('../assets/icon/icon-download-128.png'),
-  is_service: false,
-  is_input: false,
   is_output: false,
   is_storage: false
 }
@@ -65,17 +54,6 @@ const ELASTICSEARCH = {
   is_service: true,
   is_input: true,
   is_output: true,
-  is_storage: true
-}
-
-const FIREBASE = {
-  service_name: 'Firebase',
-  service_description: 'Mobile and web application development platform',
-  connection_type: types.CONNECTION_TYPE_FIREBASE,
-  icon: require('../assets/icon/icon-firebase-128.png'),
-  is_service: true,
-  is_input: true,
-  is_output: false,
   is_storage: true
 }
 
@@ -145,28 +123,6 @@ const HOME = {
   is_storage: false
 }
 
-const HTTP = {
-  service_name: 'Web Link',
-  service_description: '',
-  connection_type: types.CONNECTION_TYPE_HTTP,
-  icon: require('../assets/icon/icon-link-128.png'),
-  is_service: false,
-  is_input: true,
-  is_output: false,
-  is_storage: false
-}
-
-const MAILJET = {
-  service_name: 'Mailjet',
-  service_description: 'Send email that converts',
-  connection_type: types.CONNECTION_TYPE_MAILJET,
-  icon: require('../assets/icon/icon-mailjet-128.png'),
-  is_service: true,
-  is_input: false,
-  is_output: false,
-  is_storage: false
-}
-
 const MYSQL = {
   service_name: 'MySQL',
   service_description: "The world's most popular open source database",
@@ -178,17 +134,6 @@ const MYSQL = {
   is_storage: true
 }
 
-const PIPELINEDEALS = {
-  service_name: 'PipelineDeals',
-  service_description: 'CRM software to start, develop and grow your business',
-  connection_type: types.CONNECTION_TYPE_PIPELINEDEALS,
-  icon: require('../assets/icon/icon-pipelinedeals-128.png'),
-  is_service: true,
-  is_input: true,
-  is_output: false,
-  is_storage: false
-}
-
 const POSTGRES = {
   service_name: 'PostgreSQL',
   service_description: "The world's most advanced open source database",
@@ -198,17 +143,6 @@ const POSTGRES = {
   is_input: true,
   is_output: true,
   is_storage: true
-}
-
-const RSS = {
-  service_name: 'RSS Feed',
-  service_description: '',
-  connection_type: types.CONNECTION_TYPE_RSS,
-  icon: require('../assets/icon/icon-rss-128.png'),
-  is_service: false,
-  is_input: true,
-  is_output: false,
-  is_storage: false
 }
 
 const SMTP = {
@@ -233,17 +167,18 @@ const SFTP = {
   is_storage: true
 }
 
-const SOCRATA = {
-  service_name: 'Socrata',
-  service_description: 'Open data portal for government data',
-  connection_type: types.CONNECTION_TYPE_SOCRATA,
-  icon: require('../assets/icon/icon-socrata-128.png'),
+const TWILIO = {
+  service_name: 'Twilio',
+  service_description: 'Programmatically make and receive phone calls and send and receive text messages',
+  connection_type: types.CONNECTION_TYPE_TWILIO,
+  icon: require('../assets/icon/icon-twilio-128.png'),
   is_service: true,
-  is_input: false,
-  is_output: false,
-  is_storage: false
+  is_input: true,
+  is_output: true,
+  is_storage: true
 }
 
+/*
 const STDIN = {
   service_name: 'Stdin',
   service_description: 'Standard In',
@@ -266,17 +201,6 @@ const STDOUT = {
   is_storage: false
 }
 
-const TWILIO = {
-  service_name: 'Twilio',
-  service_description: 'Programmatically make and receive phone calls and send and receive text messages',
-  connection_type: types.CONNECTION_TYPE_TWILIO,
-  icon: require('../assets/icon/icon-twilio-128.png'),
-  is_service: true,
-  is_input: true,
-  is_output: true,
-  is_storage: true
-}
-
 const UPLOAD = {
   service_name: 'File Upload',
   service_description: 'Upload files from your computer',
@@ -288,22 +212,78 @@ const UPLOAD = {
   is_storage: false
 }
 
+const DOWNLOAD = {
+  service_name: 'Download',
+  service_description: 'Download files to your computer',
+  connection_type: types.CONNECTION_TYPE_DOWNLOAD,
+  icon: require('../assets/icon/icon-download-128.png'),
+  is_service: false,
+  is_input: false,
+  is_output: false,
+  is_storage: false
+}
+
+const FIREBASE = {
+  service_name: 'Firebase',
+  service_description: 'Mobile and web application development platform',
+  connection_type: types.CONNECTION_TYPE_FIREBASE,
+  icon: require('../assets/icon/icon-firebase-128.png'),
+  is_service: true,
+  is_input: true,
+  is_output: false,
+  is_storage: true
+}
+
+const MAILJET = {
+  service_name: 'Mailjet',
+  service_description: 'Send email that converts',
+  connection_type: types.CONNECTION_TYPE_MAILJET,
+  icon: require('../assets/icon/icon-mailjet-128.png'),
+  is_service: true,
+  is_input: false,
+  is_output: false,
+  is_storage: false
+}
+
+const RSS = {
+  service_name: 'RSS Feed',
+  service_description: '',
+  connection_type: types.CONNECTION_TYPE_RSS,
+  icon: require('../assets/icon/icon-rss-128.png'),
+  is_service: false,
+  is_input: true,
+  is_output: false,
+  is_storage: false
+}
+
+const SOCRATA = {
+  service_name: 'Socrata',
+  service_description: 'Open data portal for government data',
+  connection_type: types.CONNECTION_TYPE_SOCRATA,
+  icon: require('../assets/icon/icon-socrata-128.png'),
+  is_service: true,
+  is_input: false,
+  is_output: false,
+  is_storage: false
+}
+
+const PIPELINEDEALS = {
+  service_name: 'PipelineDeals',
+  service_description: 'CRM software to start, develop and grow your business',
+  connection_type: types.CONNECTION_TYPE_PIPELINEDEALS,
+  icon: require('../assets/icon/icon-pipelinedeals-128.png'),
+  is_service: true,
+  is_input: true,
+  is_output: false,
+  is_storage: false
+}
+*/
+
 /* exports */
 
-// TODO: what are we going to do with these?
-//export const CONNECTION_INFO_STDIN         = STDIN
-//export const CONNECTION_INFO_STDOUT        = STDOUT
-//export const CONNECTION_INFO_UPLOAD        = UPLOAD
-//export const CONNECTION_INFO_DOWNLOAD      = DOWNLOAD
-//export const CONNECTION_INFO_FIREBASE      = FIREBASE
-//export const CONNECTION_INFO_HTTP          = HTTP
-//export const CONNECTION_INFO_MAILJET       = MAILJET
-//export const CONNECTION_INFO_RSS           = RSS
-//export const CONNECTION_INFO_SOCRATA       = SOCRATA
-//export const CONNECTION_INFO_PIPELINEDEALS = PIPELINEDEALS
-
 // go out of alphabetical order here so the order is correct in the pipe add modal
-export const CONNECTION_INFO_CUSTOM_API           = CUSTOM_API
+export const CONNECTION_INFO_HOME                 = HOME
+export const CONNECTION_INFO_API                  = API
 export const CONNECTION_INFO_AMAZON_S3            = AMAZON_S3
 export const CONNECTION_INFO_BOX                  = BOX
 export const CONNECTION_INFO_DROPBOX              = DROPBOX
@@ -314,8 +294,18 @@ export const CONNECTION_INFO_GMAIL                = GMAIL
 export const CONNECTION_INFO_GOOGLECLOUDSTORAGE   = GOOGLECLOUDSTORAGE
 export const CONNECTION_INFO_GOOGLEDRIVE          = GOOGLEDRIVE
 export const CONNECTION_INFO_GOOGLESHEETS         = GOOGLESHEETS
-export const CONNECTION_INFO_HOME                 = HOME
 export const CONNECTION_INFO_MYSQL                = MYSQL
 export const CONNECTION_INFO_POSTGRES             = POSTGRES
 export const CONNECTION_INFO_SFTP                 = SFTP
 export const CONNECTION_INFO_TWILIO               = TWILIO
+
+// TODO: what are we going to do with these?
+//export const CONNECTION_INFO_STDIN         = STDIN
+//export const CONNECTION_INFO_STDOUT        = STDOUT
+//export const CONNECTION_INFO_UPLOAD        = UPLOAD
+//export const CONNECTION_INFO_DOWNLOAD      = DOWNLOAD
+//export const CONNECTION_INFO_FIREBASE      = FIREBASE
+//export const CONNECTION_INFO_MAILJET       = MAILJET
+//export const CONNECTION_INFO_RSS           = RSS
+//export const CONNECTION_INFO_SOCRATA       = SOCRATA
+//export const CONNECTION_INFO_PIPELINEDEALS = PIPELINEDEALS
