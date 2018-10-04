ecbcbff394727dd1dfab8538bbb26f6f6e0f6d2e
David Z. Williams <dave@flex.io>
2018-10-04 17:57:33 -0400

    Better text extent calculations for label switch.

--- public/src/components/LabelSwitch.vue
+++ public/src/components/LabelSwitch.vue
@@ -10,7 +10,7 @@
     />
     <transition name="el-zoom-in-center" mode="out-in">
       <span
-        class="absolute top-0 ttu b pointer"
+        class="absolute top-0 lh-1 ttu b pointer"
         :style="text_style"
         @click.stop="onChange"
         v-bind:key="value"
@@ -19,8 +19,8 @@
       </span>
     </transition>
     <div class="relative overflow-hidden">
-      <div class="absolute no-pointer-events dib ttu b invisible" style="left: 1000px; top: 1000px" ref="active-text">{{activeLabel}}</div>
-      <div class="absolute no-pointer-events dib ttu b invisible" style="left: 1000px; top: 1000px" ref="inactive-text">{{inactiveLabel}}</div>
+      <div class="absolute no-pointer-events dib lh-1 ttu b invisible" :style="hidden_text_style" ref="active-text">{{activeLabel}}</div>
+      <div class="absolute no-pointer-events dib lh-1 ttu b invisible" :style="hidden_text_style" ref="inactive-text">{{inactiveLabel}}</div>
     </div>
   </div>
 </template>
@@ -76,20 +76,35 @@
     data() {
       return {
         active_text_width: 0,
-        inactive_text_width: 0
+        active_text_height: 0,
+        inactive_text_width: 0,
+        inactive_text_height: 0
       }
     },
     computed: {
       text_width() {
-        var spacing = 28
+        var spacing = 30
         var atw = this.active_text_width + spacing
         var itw = this.inactive_text_width + spacing
         return _.isNumber(this.width) ? this.width : this.value ? atw : itw
       },
+      text_height() {
+        var ath = this.active_text_height
+        var ith = this.inactive_text_height
+        return Math.max(ath, ith)
+      },
+      hidden_text_style() {
+        var styles = []
+        styles.push('left: 1000px')
+        styles.push('top: 1000px')
+        styles.push('font-size: 14px')
+        return styles.join('; ')
+      },
       text_style() {
         var styles = []
         styles.push(this.value ? 'color: ' + this.activeLabelColor : 'color: ' + this.inactiveLabelColor)
-        styles.push('top: 3px')
+        styles.push('top: 50%')
+        styles.push('margin-top: -' + (Math.floor(this.text_height / 2)) + 'px')
         styles.push(this.value ? 'left: 8px' : 'right: 8px')
         styles.push('font-size: 14px')
         return styles.join('; ')
@@ -101,10 +116,12 @@
       },
       measureText() {
         this.$nextTick(() => {
-          var w1 = this.$refs['active-text'].clientWidth
-          var w2 = this.$refs['inactive-text'].clientWidth
-          this.active_text_width = w1
-          this.inactive_text_width = w2
+          var at = this.$refs['active-text']
+          var iat = this.$refs['inactive-text']
+          this.active_text_width = at.clientWidth
+          this.active_text_height = at.clientHeight
+          this.inactive_text_width = iat.clientWidth
+          this.inactive_text_height = iat.clientHeight
         })
       },
       updateTextWidth() {
