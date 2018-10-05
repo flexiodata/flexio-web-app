7a022d569e396abd450d168a14dddae30272d75f
David Z. Williams <dave@flex.io>
2018-10-05 11:25:11 -0400

    Line up task chooser items 5 across in pipe builder empty item. Closes #404.

--- public/src/components/PipeBuilderEmptyItem.vue
+++ public/src/components/PipeBuilderEmptyItem.vue
@@ -2,6 +2,7 @@
   <div>
     <p class="mt0 ttu fw6 f7 moon-gray">Choose a starting task (e.g., click 'Execute' to add a function)</p>
     <BuilderItemTaskChooser
+      style="margin-right: 8rem"
       :show-title="false"
       @task-chooser-select-task="selectTask"
     />
