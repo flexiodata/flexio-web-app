95dacc9f90bc3915886f83795da9384e8831d151
David Z. Williams <dave@flex.io>
2018-10-08 16:30:01 -0400

    Split out DOM utils into separate file.

---
+++ public/src/utils/dom.js
@@ -0,0 +1,32 @@
+
+const isHiddenInDOM = (el) => {
+  return (el.offsetParent === null)
+}
+
+const fallbackCss = (el_id, href) => {
+  var tmp_el = document.createElement('div')
+  tmp_el.className = 'dn' // use Tachyons as our test case (if it's not loaded, it's likely nothing else is either)
+
+  // append test element to document body
+  document.body.appendChild(tmp_el)
+
+  if (tmp_el && !isHiddenInDOM(tmp_el)) {
+    var head_el = document.head || document.getElementsByTagName('head')[0]
+
+    // create link tag
+    var link_el = document.createElement('link')
+    link_el.type = 'text/css'
+    link_el.rel = 'stylesheet'
+
+    // add link tag to the <head>
+    head_el.appendChild(link_el)
+    link_el.href = href
+  }
+
+  document.body.removeChild(tmp_el)
+}
+
+export default {
+  isHiddenInDOM,
+  fallbackCss
+}
--- public/src/utils/index.js
+++ public/src/utils/index.js
@@ -3,10 +3,6 @@ const isNumber = (value) => {
   return !isNaN(parseFloat(value)) && isFinite(value)
 }
 
-const isHiddenInDOM = (el) => {
-  return (el.offsetParent === null)
-}
-
 const pluralize = (cnt, many_str, one_str, zero_str) => {
   cnt = parseInt(''+cnt)
   if (cnt > 1)  return many_str
@@ -33,29 +29,6 @@ const afterFirst = (str, char) => {
   return afterNth(str, char, 1)
 }
 
-const fallbackCss = (el_id, href) => {
-  var tmp_el = document.createElement('div')
-  tmp_el.className = 'dn' // use Tachyons as our test case (if it's not loaded, it's likely nothing else is either)
-
-  // append test element to document body
-  document.body.appendChild(tmp_el)
-
-  if (tmp_el && !isHiddenInDOM(tmp_el)) {
-    var head_el = document.head || document.getElementsByTagName('head')[0]
-
-    // create link tag
-    var link_el = document.createElement('link')
-    link_el.type = 'text/css'
-    link_el.rel = 'stylesheet'
-
-    // add link tag to the <head>
-    head_el.appendChild(link_el)
-    link_el.href = href
-  }
-
-  document.body.removeChild(tmp_el)
-}
-
 const sanitizeMasked = (obj) => {
   return _.omitBy(obj, (val, key) => { return val === '*****' })
 }
@@ -74,12 +47,10 @@ const atobUnicode = (str) => {
 
 export default {
   isNumber,
-  isHiddenInDOM,
   pluralize,
   slugify,
   afterNth,
   afterFirst,
-  fallbackCss,
   sanitizeMasked,
   btoaUnicode,
   atobUnicode
