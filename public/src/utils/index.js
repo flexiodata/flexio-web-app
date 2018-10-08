55652356a02d6966499852839c2db44987af7ded
David Z. Williams <dave@flex.io>
2018-10-08 16:27:09 -0400

    Use `const` in util.js.

--- public/src/utils/index.js
+++ public/src/utils/index.js
@@ -1,13 +1,13 @@
 
-var isNumber = (value) => {
+const isNumber = (value) => {
   return !isNaN(parseFloat(value)) && isFinite(value)
 }
 
-var isHiddenInDOM = (el) => {
+const isHiddenInDOM = (el) => {
   return (el.offsetParent === null)
 }
 
-var pluralize = (cnt, many_str, one_str, zero_str) => {
+const pluralize = (cnt, many_str, one_str, zero_str) => {
   cnt = parseInt(''+cnt)
   if (cnt > 1)  return many_str
   if (cnt == 1) return one_str
@@ -15,7 +15,7 @@ var pluralize = (cnt, many_str, one_str, zero_str) => {
   return ''
 }
 
-var slugify = (str) => {
+const slugify = (str) => {
   str = str.replace(/\W/g, ' ')
   str = str.trim()
   str = str.replace(/\s+/g, '-')
@@ -23,17 +23,17 @@ var slugify = (str) => {
   return str
 }
 
-var afterNth = (str, char, cnt) => {
+const afterNth = (str, char, cnt) => {
   if (!isNumber(cnt)) { cnt = 1 }
   var retval = str.substr(str.indexOf('/') + 1)
   return cnt <= 1 ? retval : afterNth(retval, char, cnt-1)
 }
 
-var afterFirst = (str, char) => {
+const afterFirst = (str, char) => {
   return afterNth(str, char, 1)
 }
 
-var fallbackCss = (el_id, href) => {
+const fallbackCss = (el_id, href) => {
   var tmp_el = document.createElement('div')
   tmp_el.className = 'dn' // use Tachyons as our test case (if it's not loaded, it's likely nothing else is either)
 
@@ -56,31 +56,31 @@ var fallbackCss = (el_id, href) => {
   document.body.removeChild(tmp_el)
 }
 
-var sanitizeMasked = (obj) => {
+const sanitizeMasked = (obj) => {
   return _.omitBy(obj, (val, key) => { return val === '*****' })
 }
 
-var btoaUnicode = (str) => {
+const btoaUnicode = (str) => {
   return btoa(encodeURIComponent(str).replace(/%([0-9A-F]{2})/g, function(match, p1) {
     return String.fromCharCode(parseInt(p1, 16))
   }))
 }
 
-var atobUnicode = (str) => {
+const atobUnicode = (str) => {
   return decodeURIComponent(Array.prototype.map.call(atob(str), function(c) {
     return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2)
   }).join(''))
 }
 
 export default {
-  isNumber: isNumber,
-  isHiddenInDOM: isHiddenInDOM,
-  pluralize: pluralize,
-  slugify: slugify,
-  afterNth: afterNth,
-  afterFirst: afterFirst,
-  fallbackCss: fallbackCss,
-  sanitizeMasked: sanitizeMasked,
-  btoaUnicode: btoaUnicode,
-  atobUnicode: atobUnicode
+  isNumber,
+  isHiddenInDOM,
+  pluralize,
+  slugify,
+  afterNth,
+  afterFirst,
+  fallbackCss,
+  sanitizeMasked,
+  btoaUnicode,
+  atobUnicode
 }
