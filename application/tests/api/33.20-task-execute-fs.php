a928ea65bc7928dc37c263b88224f05c27a2b841
Ben Williams <ben@flex.io>
2018-10-12 01:02:24 +0200

    added a test for a large value with flex.fs.read/write()

--- application/tests/api/33.20-task-execute-fs.php
+++ application/tests/api/33.20-task-execute-fs.php
@@ -4,8 +4,8 @@
  * Copyright (c) 2018, Gold Prairie, Inc.  All rights reserved.
  *
  * Project:  Flex.io App
- * Author:   Aaron L. Williams
- * Created:  2018-09-20
+ * Author:   Benjamin I. Williams
+ * Created:  2018-10-11
  *
  * @package flexio
  * @subpackage Tests
@@ -66,7 +66,7 @@ import string
 import random
 
 def flex_handler(flex):
-    rand = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(20))
+    rand = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
     filename = rand
     value = rand.encode('utf-8')
 
@@ -77,7 +77,26 @@ EOD;
 
         $actual = $this->runScript($script);
         $expected = 'passed';
-        \Flexio\Tests\Check::assertString('A.1', 'flex.fs.write() and flex.fs.read()',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('A.1', 'flex.fs.write() and flex.fs.read(), simple small value',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $script = <<<EOD
+import string
+import random
+
+def flex_handler(flex):
+    rand = ''.join(random.choice(string.ascii_uppercase + string.digits) for _ in range(10))
+    filename = rand
+    value = (rand*300000).encode('utf-8')
+
+    flex.fs.write(filename, value)
+    check = flex.fs.read(filename)
+    flex.output.write('passed' if check == value else 'failed')
+EOD;
+
+        $actual = $this->runScript($script);
+        $expected = 'passed';
+        \Flexio\Tests\Check::assertString('A.2', 'flex.fs.write() and flex.fs.read(), large value (3 MB)',  $actual, $expected, $results);
 
     }
 }
