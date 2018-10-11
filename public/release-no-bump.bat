33f8ceb77e54bc3c4caff64352c6227c89ab2379
David Z. Williams <dave@flex.io>
2018-10-11 12:51:35 -0400

    Small tweaks to look and feel of Intercom trigger button.

--- public/release-no-bump.bat
+++ public/release-no-bump.bat
@@ -1,5 +1,5 @@
 call yarn
-call yarn add flexio-sdk-js
+REM call yarn add flexio-sdk-js
 call git rm dist\js\*.js
 call git rm dist\css\*.css
 call git reset HEAD dist\css\tachyons.min.css
--- public/src/components/App.vue
+++ public/src/components/App.vue
@@ -2,13 +2,15 @@
   <div id="app" class="flex flex-column fixed absolute--fill overflow-hidden">
     <AppNavbar v-if="show_intercom_button && show_navbar" />
     <router-view class="flex-fill bt b--black-05"></router-view>
-    <button
+    <el-button
+      type="primary"
+      circle
       id="open-intercom-inbox"
-      class="fixed bottom-0 right-0 pointer darken-10 bn br-100 white bg-blue"
-      style="z-index: 2147482000; padding: 12px; margin: 20px; box-shadow: 0 2px 8px rgba(0,0,0,0.4)"
+      class="fixed bottom-0 right-0"
+      style="z-index: 2147482000; padding: 12px; margin: 24px; box-shadow: 0 1px 0 rgba(255,255,255,1), 0 2px 12px rgba(0,0,0,0.4)"
       v-if="show_intercom_button"
     ><i class="material-icons md-24 relative" style="top: 1px">chat</i>
-    </button>
+    </el-button>
   </div>
 </template>
 
