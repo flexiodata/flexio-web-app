11f872f7406d51ea175a9c121006282bde6cd0bb
David Z. Williams <dave@flex.io>
2018-10-12 02:21:22 -0400

    Renamed AccountHome.vue => AppAccount.vue.

--- public/src/router/account.js
+++ public/src/router/account.js
@@ -1,10 +1,10 @@
 import store from '../store'
 import { ROUTE_ACCOUNT } from '../constants/route'
-import AccountHome from '../components/AccountHome.vue'
+import AppAccount from '../components/AppAccount.vue'
 
 export default {
   path: '/account/:section?',
   name: ROUTE_ACCOUNT,
-  component: AccountHome,
+  component: AppAccount,
   meta: { requiresAuth: true }
 }
