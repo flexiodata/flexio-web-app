5c0f7fd05e12cff2497e7720b62e76b3dc404304
David Z. Williams <dave@flex.io>
2018-10-12 10:26:07 -0400

    Cleaned up unused route.

--- public/src/router/copypipe.js
+++
@@ -1,9 +0,0 @@
-import { ROUTE_COPYPIPE } from '../constants/route'
-import CopyPipe from '../components/CopyPipe.vue'
-
-export default {
-  path: '/copypipe',
-  name: ROUTE_COPYPIPE,
-  component: CopyPipe,
-  meta: { requiresAuth: true }
-}
