3ec8562c713382449e2ba41d3cd1f50d25d34562
Aaron Williams <aaron@flex.io>
2018-10-17 16:04:51 -0500

    added some execute tests for input/output

---
+++ application/tests/api/33.08-task-execute-io.php
@@ -0,0 +1,102 @@
+<?php
+/**
+ *
+ * Copyright (c) 2018, Gold Prairie, Inc.  All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2018-10-17
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // ENDPOINT: POST /:userid/processes/:objeid/run
+
+
+        // SETUP
+        $apibase = \Flexio\Tests\Util::getTestHost() . '/v1';
+        $password = \Flexio\Base\Password::generate();
+        $userid = \Flexio\Tests\Util::createUser(null, null, $password);
+        $token = \Flexio\Tests\Util::createToken($userid);
+
+
+        // TEST: execute input/output
+
+        // BEGIN TEST
+        $script = <<<EOD
+def flex_handler(flex):
+    flex.output.write("Hello, World!")
+EOD;
+        $task = \Flexio\Tests\Task::create([
+            ["op" => "execute", "lang" => "python", "code" => base64_encode($script)]
+        ]);
+        $result = \Flexio\Tests\Util::callApi(array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid/processes",
+            'token' => $token,
+            'content_type' => 'application/json',
+            'params' => json_encode(["task" => $task])
+        ));
+        $response = json_decode($result['response'],true);
+        $objeid = $response['eid'] ?? '';
+        $result = \Flexio\Tests\Util::callApi(array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid/processes/$objeid/run",
+            'token' => $token
+        ));
+        $actual = $result;
+        $expected = '{
+            "code": 200,
+            "content_type": "text/plain;charset=UTF-8",
+            "response": "Hello, World!"
+        }';
+        \Flexio\Tests\Check::assertInArray('A.1', 'Process Execute; (python) execute task output',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $script1 = <<<EOD
+def flex_handler(flex):
+    flex.output.write("Hello, World!")
+EOD;
+    $script2 = <<<EOD
+def flex_handler(flex):
+    buf = flex.input.read()
+    flex.output.write(buf)
+EOD;
+        $task = \Flexio\Tests\Task::create([
+            ["op" => "execute", "lang" => "python", "code" => base64_encode($script1)],
+            ["op" => "execute", "lang" => "python", "code" => base64_encode($script2)]
+        ]);
+        $result = \Flexio\Tests\Util::callApi(array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid/processes",
+            'token' => $token,
+            'content_type' => 'application/json',
+            'params' => json_encode(["task" => $task])
+        ));
+        $response = json_decode($result['response'],true);
+        $objeid = $response['eid'] ?? '';
+        $result = \Flexio\Tests\Util::callApi(array(
+            'method' => 'POST',
+            'url' => "$apibase/$userid/processes/$objeid/run",
+            'token' => $token
+        ));
+        $actual = $result;
+        $expected = '{
+            "code": 200,
+            "content_type": "text/plain;charset=UTF-8",
+            "response": "Hello, World!"
+        }';
+        \Flexio\Tests\Check::assertInArray('A.1', 'Process Execute; (python) execute task chaining with input/output',  $actual, $expected, $results);
+    }
+}
+
