aeecdf13d152022e7c1f32406e73a76d8ba1e79f
Aaron Williams <aaron@flex.io>
2018-10-18 15:39:48 -0500

    include 'triggered_by' in process list info

--- application/api/Process.php
+++ application/api/Process.php
@@ -431,6 +431,7 @@ class Process
             $process_info_subset['eid_type'] = $process_info['eid_type'];
             $process_info_subset['eid_status'] = $process_info['eid_status'];
             $process_info_subset['parent'] = $process_info['parent'] ?? null;
+            $process_info_subset['triggered_by'] = $process_info['triggered_by'];
             $process_info_subset['started_by'] = $process_info['started_by'];
             $process_info_subset['started'] = $process_info['started'];
             $process_info_subset['finished'] = $process_info['finished'];
