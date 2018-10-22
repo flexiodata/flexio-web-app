a1e152293c3d4b42022833c8e46a1dc5c1403921
David Z. Williams <dave@flex.io>
2018-10-22 14:26:38 -0400

    Working on converting process actions and APIs over to using Axios. Converted `updateUser` => `v2_updateUser`.

--- public/src/components/AccountRegionForm.vue
+++ public/src/components/AccountRegionForm.vue
@@ -91,16 +91,11 @@
       saveChanges() {
         var eid = this.active_user_eid
         var attrs = _.pick(this.$data, ['timezone'])
-        this.$store.dispatch('updateUser', { eid, attrs }).then(response => {
-          if (response.ok)
-          {
-            this.show_success = true
-            setTimeout(() => { this.show_success = false }, 4000)
-          }
-           else
-          {
-            this.show_error = true
-          }
+        this.$store.dispatch('v2_action_updateUser', { eid, attrs }).then(response => {
+          this.show_success = true
+          setTimeout(() => { this.show_success = false }, 4000)
+        }).catch(error => {
+          this.show_error = true
         })
       }
     }
