2c849d847ea2753d07023c832608f0ddd5cea93c
David Z. Williams <dave@flex.io>
2018-10-26 02:00:59 -0400

    Removed semicolons.

--- public/src/components/AccountBillingForm.vue
+++ public/src/components/AccountBillingForm.vue
@@ -26,10 +26,10 @@
     methods: {
       createForm() {
         // Create a Stripe client.
-        var stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx');
+        var stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx')
 
         // Create an instance of Elements.
-        var elements = stripe.elements();
+        var elements = stripe.elements()
 
         // Custom styling can be passed to options when creating an Element.
         // (Note that this demo uses a wider set of styles than the guide below.)
@@ -48,70 +48,70 @@
             color: '#fa755a',
             iconColor: '#fa755a'
           }
-        };
+        }
 
         // Create an instance of the card Element.
-        var card = elements.create('card', {style: style});
+        var card = elements.create('card', { style: style })
 
         // Add an instance of the card Element into the `card-element` <div>.
-        card.mount('#card-element');
+        card.mount('#card-element')
 
         // Handle real-time validation errors from the card Element.
         card.addEventListener('change', function(event) {
-          var displayError = document.getElementById('card-errors');
+          var displayError = document.getElementById('card-errors')
           if (event.error) {
-            displayError.textContent = event.error.message;
+            displayError.textContent = event.error.message
           } else {
-            displayError.textContent = '';
+            displayError.textContent = ''
           }
-        });
+        })
 
         // Handle form submission.
-        var form = document.getElementById('payment-form');
+        var form = document.getElementById('payment-form')
         form.addEventListener('submit', function(event) {
-          event.preventDefault();
+          event.preventDefault()
 
           stripe.createToken(card).then(function(result) {
             if (result.error) {
               // Inform the user if there was an error.
-              var errorElement = document.getElementById('card-errors');
-              errorElement.textContent = result.error.message;
+              var errorElement = document.getElementById('card-errors')
+              errorElement.textContent = result.error.message
             } else {
               // Send the token to your server.
-              stripeTokenHandler(result.token);
+              stripeTokenHandler(result.token)
             }
-          });
-        });
+          })
+        })
       }
     }
   }
 </script>
 
-<style>
+<style lang="stylus">
   /**
    * The CSS shown here will not be introduced in the Quickstart guide, but shows
    * how you can use CSS to style your Element's container.
    */
   .StripeElement {
-    background-color: white;
-    height: 40px;
-    padding: 10px 12px;
-    border-radius: 4px;
-    border: 1px solid transparent;
-    box-shadow: 0 1px 3px 0 #e6ebf1;
-    -webkit-transition: box-shadow 150ms ease;
-    transition: box-shadow 150ms ease;
+    background-color: white
+    height: 40px
+    padding: 10px 12px
+    border-radius: 4px
+    border: 1px solid transparent
+    box-shadow: 0 1px 3px 0 #e6ebf1
+    -webkit-transition: box-shadow 150ms ease
+    transition: box-shadow 150ms ease
   }
 
   .StripeElement--focus {
-    box-shadow: 0 1px 3px 0 #cfd7df;
+    box-shadow: 0 1px 3px 0 #cfd7df
   }
 
   .StripeElement--invalid {
-    border-color: #fa755a;
+    border-color: #fa755a
   }
 
   .StripeElement--webkit-autofill {
-    background-color: #fefde5 !important;
+    background-color: #fefde5 !important
   }
 </style>
