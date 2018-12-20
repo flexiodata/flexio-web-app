96ddbb47dc7f0ab5d20c14c8d243d966349862d9
David Z. Williams <dave@flex.io>
2018-12-20 17:14:36 -0500

    Split out process status select into its own component.

--- public/src/components/AppActivity.vue
+++ public/src/components/AppActivity.vue
@@ -33,29 +33,13 @@
           />
         </div>
         <div class="flex flex-row justify-end items-center mt2">
-          <el-select
+          <ProcessStatusSelect
             style="width: 140px"
             placeholder="Status"
             size="small"
             clearable
             v-model="status_filter"
-          >
-            <el-option
-              :label="option.label"
-              :value="option.val"
-              :key="option.val"
-              v-for="option in status_options"
-            >
-            <div class="flex flex-row items-center">
-              <i class="el-icon-success dark-green" v-if="option.val == 'C'"></i>
-              <i class="el-icon-warning dark-red" v-else-if="option.val == 'F'"></i>
-              <i class="el-icon-error dark-red" v-else-if="option.val == 'X'"></i>
-              <i class="el-icon-loading blue" v-else-if="option.val == 'R'"></i>
-              <i class="el-icon-info blue" v-else></i>
-              <span class="ml2">{{option.label}}</span>
-            </div>
-            </el-option>
-          </el-select>
+          />
           <el-date-picker
             class="ml2"
             style="width: 220px"
@@ -105,36 +89,10 @@
   import moment from 'moment'
   import stickybits from 'stickybits'
   import { mapState, mapGetters } from 'vuex'
-  import * as ps from '../constants/process'
   import Spinner from 'vue-simple-spinner'
   import ProcessList from './ProcessList.vue'
+  import ProcessStatusSelect from './ProcessStatusSelect.vue'
   import ProcessDetailsPanel from './ProcessDetailsPanel.vue'
-
-  const fmtProcessStatus = (status) => {
-    switch (status) {
-      case ps.PROCESS_STATUS_PENDING   : return 'Pending'
-      case ps.PROCESS_STATUS_WAITING   : return 'Waiting'
-      case ps.PROCESS_STATUS_RUNNING   : return 'Running'
-      case ps.PROCESS_STATUS_CANCELLED : return 'Canceled'
-      case ps.PROCESS_STATUS_PAUSED    : return 'Paused'
-      case ps.PROCESS_STATUS_FAILED    : return 'Failed'
-      case ps.PROCESS_STATUS_COMPLETED : return 'Completed'
-    }
-
-    return ''
-  }
-
-  const statuses = [
-    ps.PROCESS_STATUS_COMPLETED,
-    ps.PROCESS_STATUS_FAILED,
-    ps.PROCESS_STATUS_PENDING,
-    ps.PROCESS_STATUS_RUNNING,
-    ps.PROCESS_STATUS_CANCELLED
-  ]
-
-  const status_options = _.map(statuses, (val) => {
-    return { label: fmtProcessStatus(val), val }
-  })
 
   export default {
     metaInfo: {
@@ -149,6 +107,7 @@
     components: {
       Spinner,
       ProcessList,
+      ProcessStatusSelect,
       ProcessDetailsPanel
     },
     watch: {
@@ -168,7 +127,6 @@
         sort_direction: 'desc',
         date_range: null,
         status_filter: '',
-        status_options,
         show_process_details_dialog: false,
         edit_process_eid: ''
       }
