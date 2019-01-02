012d89a46790406b33699400aef6aba69b41087b
David Z. Williams <dave@flex.io>
2019-01-02 17:30:31 -0500

    Initial Stripe Element integration.

--- public/src/components/AccountBillingForm.vue
+++ public/src/components/AccountBillingForm.vue
@@ -1,87 +1,59 @@
 <template>
   <div>
-    <form action="/charge" method="post" id="payment-form">
-      <div class="form-row">
-        <label for="card-element">
-          Credit or debit card
-        </label>
-        <div id="card-element">
-          <!-- A Stripe Element will be inserted here. -->
-        </div>
-
-        <!-- Used to display form errors. -->
-        <div id="card-errors" role="alert"></div>
-      </div>
-
-      <button>Submit Payment</button>
-    </form>
+    <Card
+      class="stripe-card"
+      stripe="pk_test_TYooMQauvdEDq54NiTphI7jx"
+      :class="{ complete }"
+      :options="stripeOptions"
+      @change="complete = $event.complete"
+    />
+    <div class="pt3">
+      <el-button
+        type="primary"
+        class="ttu fw6 pay-with-stripe"
+        @click="pay"
+        :disabled="!complete"
+      >
+        Submit Payment
+      </el-button>
+    </div>
   </div>
 </template>
 
 <script>
+  import { Card, createToken } from 'vue-stripe-elements-plus'
+
   export default {
-    mounted() {
-      this.createForm()
+    components: {
+      Card
     },
-    methods: {
-      createForm() {
-        // Create a Stripe client.
-        var stripe = Stripe('pk_test_TYooMQauvdEDq54NiTphI7jx')
-
-        // Create an instance of Elements.
-        var elements = stripe.elements()
-
-        // Custom styling can be passed to options when creating an Element.
-        // (Note that this demo uses a wider set of styles than the guide below.)
-        var style = {
-          base: {
-            color: '#32325d',
-            lineHeight: '18px',
-            fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
-            fontSmoothing: 'antialiased',
-            fontSize: '16px',
-            '::placeholder': {
-              color: '#aab7c4'
+    data() {
+      return {
+        complete: false,
+        stripeOptions: {
+          // see https://stripe.com/docs/stripe.js#element-options for details
+          style: {
+            base: {
+              color: '#32325d',
+              lineHeight: '18px',
+              fontFamily: '"Helvetica Neue", Helvetica, sans-serif',
+              fontSmoothing: 'antialiased',
+              fontSize: '16px',
+              '::placeholder': {
+                color: '#aab7c4'
+              }
+            },
+            invalid: {
+              color: '#fa755a',
+              iconColor: '#fa755a'
             }
-          },
-          invalid: {
-            color: '#fa755a',
-            iconColor: '#fa755a'
           }
         }
+      }
+    },
+    methods: {
+      pay() {
 
-        // Create an instance of the card Element.
-        var card = elements.create('card', { style: style })
-
-        // Add an instance of the card Element into the `card-element` <div>.
-        card.mount('#card-element')
-
-        // Handle real-time validation errors from the card Element.
-        card.addEventListener('change', function(event) {
-          var displayError = document.getElementById('card-errors')
-          if (event.error) {
-            displayError.textContent = event.error.message
-          } else {
-            displayError.textContent = ''
-          }
-        })
-
-        // Handle form submission.
-        var form = document.getElementById('payment-form')
-        form.addEventListener('submit', function(event) {
-          event.preventDefault()
-
-          stripe.createToken(card).then(function(result) {
-            if (result.error) {
-              // Inform the user if there was an error.
-              var errorElement = document.getElementById('card-errors')
-              errorElement.textContent = result.error.message
-            } else {
-              // Send the token to your server.
-              stripeTokenHandler(result.token)
-            }
-          })
-        })
       }
     }
   }
