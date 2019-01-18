2b4cedcb6df4d73d85a8189c9fce5cd1a07fdf22
Ben Williams <ben@flex.io>
2019-01-18 22:45:21 +0100

    added missing file

---
+++ scripts/docker/fxruntime/fxstart.go
@@ -0,0 +1,111 @@
+package main
+
+
+import (
+    zmq "github.com/pebbe/zmq4"
+    "encoding/base64"
+    "os"
+    "os/exec"
+    "bufio"
+    "fmt"
+    "encoding/json"
+)
+
+
+func main() {
+
+    engine := os.Getenv("FLEXIO_EXECUTE_ENGINE")
+    runtime_server := os.Getenv("FLEXIO_RUNTIME_SERVER")
+    runtime_key := os.Getenv("FLEXIO_RUNTIME_KEY")
+
+    requester, _ := zmq.NewSocket(zmq.REQ)
+	defer requester.Close()
+	requester.Connect(runtime_server)
+
+    var payload string
+    payload = `{
+        "version": 1,
+        "access_key": "` + runtime_key + `",
+        "method": "get_script",
+        "params": [],
+        "id": "111"
+    }`
+
+
+    requester.Send(payload, 0)
+    reply, _ := requester.Recv(0)
+
+
+    var dat map[string]interface{}
+    if err := json.Unmarshal([]byte(reply), &dat); err != nil {
+        panic(err)
+    }
+    code := dat["result"].(string)
+
+    var prog string
+    var cmdline []string
+    var dir string
+    if engine == "python" {
+        f, _ := os.Create("/fxpython/script.py")
+        f.Write([]byte(code))
+        f.Close()
+        prog = "/usr/bin/python3"
+        cmdline = []string{"-c", "import flexio as f; import script as s; f.run(s)"}
+        dir = "/fxpython"
+    } else if engine == "nodejs" {
+        f, _ := os.Create("/fxnodejs/script.js")
+        f.Write([]byte(code))
+        f.Close()
+        prog = "/usr/bin/node"
+        cmdline = []string{"-e", "var f = require('./flexio'); var s = require('./script'); f.run(s)"}
+        dir = "/fxnodejs"
+    } else {
+        panic("Error: unknown execution engine")
+    }
+
+    fmt.Printf("Code is %s\n", code)
+    fmt.Printf("Running %s %s\n", prog, cmdline)
+
+    os.Chdir(dir)
+    //prog = "/bin/ls"
+    //cmdline = []string{"-al"}
+
+    cmd := exec.Command(prog, cmdline...)
+    cmd.Dir = dir
+    //out, err := cmd.Output()
+    cmd_reader, _ := cmd.StdoutPipe()
+    scanner := bufio.NewScanner(cmd_reader)
+    go func() {
+        for scanner.Scan() {
+            //fmt.Printf(scanner.Text())
+
+            payload = `{
+                "version": 1,
+                "access_key": "` + runtime_key + `",
+                "method": "write",
+                "params": [1, "~199$*#991/bin.b64:` + base64.StdEncoding.EncodeToString(scanner.Bytes()) + `"],
+                "id": "199$*#991"
+            }`
+
+            requester.Send(payload, 0)
+            reply, _ = requester.Recv(0)
+        
+        }
+    }()
+    cmd.Start()
+    cmd.Wait()
+
+
+
+    payload = `{
+        "version": 1,
+        "access_key": "` + runtime_key + `",
+        "method": "exit_loop",
+        "params": [],
+        "id": "111"
+    }`
+
+    requester.Send(payload, 0)
+    reply, _ = requester.Recv(0)
+
+}
