6a1a407933d2ecad13499fbba0df4c73c7771ed9
David Z. Williams <dave@flex.io>
2019-02-06 15:19:06 -0500

    Pluralize the free trial message number of days.

--- public/src/components/FreeTrialNotice.vue
+++ public/src/components/FreeTrialNotice.vue
@@ -1,8 +1,9 @@
 <template>
-  <div class="f7 dark-green" v-if="days_left > 0">You have {{days_left}} days left in your free trial</div>
+  <div class="f7 dark-green" v-if="days_left >= 0">{{msg}}</div>
 </template>
 
 <script>
+  import util from '../utils'
   import moment from 'moment'
   import { mapGetters } from 'vuex'
 
@@ -23,6 +24,10 @@
       days_left() {
         var diff = Math.ceil(TRIAL_LENGTH - this.days_since_sign_up)
         return Math.max(diff, 0)
+      },
+      msg() {
+        var days = util.pluralize(this.days_left, 'days', 'day', 'days')
+        return 'You have ' + this.days_left + ' ' + days + ' left in your free trial'
       }
     },
     methods: {
