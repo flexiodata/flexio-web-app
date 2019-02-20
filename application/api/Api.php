439ef9501981bebcdc68596233c023a9fef097f8
David Z. Williams <dave@flex.io>
2019-02-20 02:35:34 -0500

    Added CORS permissions for /api/v2/session, /api/v2/signup and /api/v2/login.

--- application/api/Api.php
+++ application/api/Api.php
@@ -163,7 +163,16 @@ class Api
         }
         else
         {
-            if (substr($request_http_host, 0, 4) == 'api.' && substr($request_http_host, -8) == '.flex.io')
+            //if (substr($request_http_host, 0, 4) == 'api.' && substr($request_http_host, -8) == '.flex.io')
+            $is_api_api = substr($request_http_host, 0, 4) == 'api.' && substr($request_http_host, -8) == '.flex.io';
+            $is_www_api = substr($request_http_host, 0, 4) == 'www.' && substr($request_http_host, -8) == '.flex.io';
+            $uri = $server_request->getUri();
+
+            if ($is_api_api ||
+                ($is_www_api && substr($uri, 0, 15) == '/api/v2/session') ||
+                ($is_www_api && substr($uri, 0, 14) == '/api/v2/signup') ||
+                ($is_www_api && substr($uri, 0, 13) == '/api/v2/login')
+               )
             {
                 header('Access-Control-Allow-Origin: *');
                 header('Access-Control-Allow-Methods: GET, PUT, POST, DELETE, PATCH, HEAD');
