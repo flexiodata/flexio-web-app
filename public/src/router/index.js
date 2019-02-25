0ee48059dea322cd30ab0bcdbd208f23482a19c1
David Z. Williams <dave@flex.io>
2019-02-25 15:03:27 -0500

    Working on route handling for user-based routes.

--- public/src/router/index.js
+++ public/src/router/index.js
@@ -2,6 +2,8 @@ import Vue from 'vue'
 import Router from 'vue-router'
 import Meta from 'vue-meta'
 
+import * as rn from '../constants/route'
+
 import account from './account'
 import admin from './admin'
 import builder from './builder'
@@ -10,13 +12,10 @@ import byuser from './byuser'
 import pipes from './pipes'
 import onboard from './onboard'
 
-import {
-  ROUTE_SIGNIN_PAGE,
-  ROUTE_SIGNUP_PAGE,
-  ROUTE_FORGOTPASSWORD_PAGE,
-  ROUTE_RESETPASSWORD_PAGE,
-  ROUTE_INITSESSION_PAGE
-} from '../constants/route'
+import AppPipes from '../components/AppPipes.vue'
+import AppConnections from '../components/AppConnections.vue'
+import AppStorage from '../components/AppStorage.vue'
+import AppActivity from '../components/AppActivity.vue'
 
 import SignInPage from '../components/SignInPage.vue'
 import SignUpPage from '../components/SignUpPage.vue'
@@ -25,9 +24,14 @@ import ResetPasswordPage from '../components/ResetPasswordPage.vue'
 import InitSessionPage from '../components/InitSessionPage.vue'
 import PageNotFound from '../components/PageNotFound.vue'
 
-const basepath_redirect =   {
+const basepath_redirect = {
   path: '/',
   redirect: '/pipes'
+}
+
+const page_not_found = {
+  path: "*",
+  component: PageNotFound
 }
 
 // use VueRouter for handling browser history
@@ -36,21 +40,27 @@ Vue.use(Router)
 // use VueMeta for page metadata
 Vue.use(Meta)
 
+// any route containing a 'meta' node below will require authorization
+const meta = { requiresAuth: true }
+
 const routes = [
   basepath_redirect,
+  { path: '/signin',         name: rn.ROUTE_SIGNIN_PAGE,          component: SignInPage              },
+  { path: '/signup',         name: rn.ROUTE_SIGNUP_PAGE,          component: SignUpPage              },
+  { path: '/forgotpassword', name: rn.ROUTE_FORGOTPASSWORD_PAGE,  component: ForgotPasswordPage      },
+  { path: '/resetpassword',  name: rn.ROUTE_RESETPASSWORD_PAGE,   component: ResetPasswordPage       },
+  { path: '/initsession',    name: rn.ROUTE_INITSESSION_PAGE,     component: InitSessionPage         },
+  { path: '/pipes',          name: rn.ROUTE_PIPE_LIST_PAGE,       component: AppPipes,          meta },
+  { path: '/connections',    name: rn.ROUTE_CONNECTION_LIST_PAGE, component: AppConnections,    meta },
+  { path: '/storage',        name: rn.ROUTE_STORAGE_PAGE,         component: AppStorage,        meta },
+  { path: '/activity',       name: rn.ROUTE_ACTIVITY_LIST_PAGE,   component: AppActivity,       meta },
+  onboard,
   account,
   admin,
   builder,
-  home,
   pipes,
   byuser,
-  onboard,
-  { path: '/signin',         name: ROUTE_SIGNIN_PAGE,         component: SignInPage         },
-  { path: '/signup',         name: ROUTE_SIGNUP_PAGE,         component: SignUpPage         },
-  { path: '/forgotpassword', name: ROUTE_FORGOTPASSWORD_PAGE, component: ForgotPasswordPage },
-  { path: '/resetpassword',  name: ROUTE_RESETPASSWORD_PAGE,  component: ResetPasswordPage  },
-  { path: '/initsession',    name: ROUTE_INITSESSION_PAGE,    component: InitSessionPage    },
-  { path: "*",                                                component: PageNotFound       }
+  page_not_found
 ]
 
 export default new Router({
