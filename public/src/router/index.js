b81010c5a568e301a663df43585e859e46caa6de
David Z. Williams <dave@flex.io>
2019-02-26 12:42:27 -0500

    Moved all trivial (non-children) routes into main route/index.js file.

--- public/src/router/index.js
+++ public/src/router/index.js
@@ -3,36 +3,24 @@ import Router from 'vue-router'
 import Meta from 'vue-meta'
 
 import * as rn from '../constants/route'
-
-import account from './account'
-import admin from './admin'
-import builder from './builder'
-import home from './home'
-import byuser from './byuser'
-import pipes from './pipes'
-import onboard from './onboard'
+import admin_routes from './admin'
 
 import AppPipes from '../components/AppPipes.vue'
 import AppConnections from '../components/AppConnections.vue'
 import AppStorage from '../components/AppStorage.vue'
 import AppActivity from '../components/AppActivity.vue'
+import AppAccount from '../components/AppAccount.vue'
+
+import PipeDocument from '../components/PipeDocument.vue'
+import BuilderDocument from '../components/BuilderDocument.vue'
 
 import SignInPage from '../components/SignInPage.vue'
 import SignUpPage from '../components/SignUpPage.vue'
 import ForgotPasswordPage from '../components/ForgotPasswordPage.vue'
 import ResetPasswordPage from '../components/ResetPasswordPage.vue'
 import InitSessionPage from '../components/InitSessionPage.vue'
+import OnboardingPage from '../components/OnboardingPage.vue'
 import PageNotFound from '../components/PageNotFound.vue'
-
-const basepath_redirect = {
-  path: '/',
-  redirect: '/pipes'
-}
-
-const page_not_found = {
-  path: "*",
-  component: PageNotFound
-}
 
 // use VueRouter for handling browser history
 Vue.use(Router)
@@ -44,23 +32,22 @@ Vue.use(Meta)
 const meta = { requiresAuth: true }
 
 const routes = [
-  basepath_redirect,
-  { path: '/signin',         name: rn.ROUTE_SIGNIN_PAGE,          component: SignInPage              },
-  { path: '/signup',         name: rn.ROUTE_SIGNUP_PAGE,          component: SignUpPage              },
-  { path: '/forgotpassword', name: rn.ROUTE_FORGOTPASSWORD_PAGE,  component: ForgotPasswordPage      },
-  { path: '/resetpassword',  name: rn.ROUTE_RESETPASSWORD_PAGE,   component: ResetPasswordPage       },
-  { path: '/initsession',    name: rn.ROUTE_INITSESSION_PAGE,     component: InitSessionPage         },
-  { path: '/pipes',          name: rn.ROUTE_PIPE_LIST_PAGE,       component: AppPipes,          meta },
-  { path: '/connections',    name: rn.ROUTE_CONNECTION_LIST_PAGE, component: AppConnections,    meta },
-  { path: '/storage',        name: rn.ROUTE_STORAGE_PAGE,         component: AppStorage,        meta },
-  { path: '/activity',       name: rn.ROUTE_ACTIVITY_LIST_PAGE,   component: AppActivity,       meta },
-  onboard,
-  account,
-  admin,
-  builder,
-  pipes,
-  byuser,
-  page_not_found
+  { path: '/', redirect: '/pipes' }, // base path redirect
+  { path: '/signin',                                     name: rn.ROUTE_SIGNIN_PAGE,          component: SignInPage                },
+  { path: '/signup',                                     name: rn.ROUTE_SIGNUP_PAGE,          component: SignUpPage                },
+  { path: '/forgotpassword',                             name: rn.ROUTE_FORGOTPASSWORD_PAGE,  component: ForgotPasswordPage        },
+  { path: '/resetpassword',                              name: rn.ROUTE_RESETPASSWORD_PAGE,   component: ResetPasswordPage         },
+  { path: '/initsession',                                name: rn.ROUTE_INITSESSION_PAGE,     component: InitSessionPage           },
+  { path: '/:user_identifier?/pipes',                    name: rn.ROUTE_PIPE_LIST_PAGE,       component: AppPipes,            meta },
+  { path: '/:user_identifier?/pipes/:identifier/:view?', name: rn.ROUTE_PIPE_PAGE,            component: PipeDocument,        meta },
+  { path: '/:user_identifier?/connections',              name: rn.ROUTE_CONNECTION_LIST_PAGE, component: AppConnections,      meta },
+  { path: '/:user_identifier?/storage',                  name: rn.ROUTE_STORAGE_PAGE,         component: AppStorage,          meta },
+  { path: '/:user_identifier?/activity',                 name: rn.ROUTE_ACTIVITY_LIST_PAGE,   component: AppActivity,         meta },
+  { path: '/account/:section?',                          name: rn.ROUTE_ACCOUNT_PAGE,         component: AppAccount,          meta },
+  { path: '/builder/:template',                          name: rn.ROUTE_BUILDER_PAGE,         component: BuilderDocument,     meta },
+  { path: '/onboard',                                    name: rn.ROUTE_ONBOARD_PAGE,         component: OnboardingPage,      meta },
+  admin_routes,
+  { path: "*", component: PageNotFound } // 404
 ]
 
 export default new Router({
