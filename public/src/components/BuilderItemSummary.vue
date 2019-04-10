5fd69b90e1baa59793c6b3a55093c2c48388753f
David Z. Williams <dave@flex.io>
2019-04-10 11:28:01 -0400

    Updated summary item to work with process result data.

--- public/src/components/BuilderItemSummary.vue
+++ public/src/components/BuilderItemSummary.vue
@@ -7,16 +7,19 @@
       <div class="dib mv3 pv1">
         <i class="el-icon-success v-mid f1" style="color: #13ce66"></i>
       </div>
-      <h3 class="fw6 f3 mt0 mb4">You're all set!</h3>
-      <p class="mv4 f4">Your pipe is configured and ready to be run.</p>
-      <div class="mt4">
+      <div
+        class="pb3 marked"
+        v-html="description"
+        v-show="show_description"
+      >
+      </div>
+      <div>
         <el-button
           class="ttu fw6"
           type="primary"
-          size="large"
-          @click="openPipe"
+          @click="onActionClick"
         >
-          View pipe
+          {{action_btn_label}}
         </el-button>
       </div>
     </div>
@@ -24,6 +27,9 @@
 </template>
 
 <script>
+  import marked from 'marked'
+  import { mapState } from 'vuex'
+
   export default {
     props: {
       item: {
@@ -51,6 +57,9 @@
       }
     },
     computed: {
+      ...mapState({
+        process_result: state => state.builder.process_result
+      }),
       is_active() {
         return this.index == this.activeItemIdx
       },
@@ -60,13 +69,39 @@
       is_after_active() {
         return this.index > this.activeItemIdx
       },
+      show_description() {
+        return this.description.length > 0
+      },
       title() {
-        return _.get(this.item, 'title', 'Summary')
+        return _.get(this.item, 'title', 'Success')
+      },
+      description() {
+        return marked(_.get(this.item, 'description', ''))
+      },
+      action_btn_label() {
+        return _.get(this.item, 'action_button.label', 'View pipe')
+      },
+      action_btn_href() {
+        return _.get(this.item, 'action_button.href', '')
       }
     },
     methods: {
       openPipe() {
         this.$emit('open-pipe')
+      },
+      onActionClick() {
+        var href = this.action_btn_href
+
+        if (href.length > 0) {
+          if (href.indexOf('{{') == 0) {
+            href = href.replace('{{', '')
+            href = href.replace('}}', '')
+            href = _.get(this, href, '#')
+          }
+          window.location.href = href
+        } else {
+          this.openPipe()
+        }
       }
     }
   }
