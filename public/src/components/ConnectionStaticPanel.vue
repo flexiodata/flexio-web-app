5f5177abbbc834e191f616f852fb7793e1d0324e
David Z. Williams <dave@flex.io>
2019-07-12 16:51:34 -0400

    Show connection name in connection static header.

--- public/src/components/ConnectionStaticPanel.vue
+++ public/src/components/ConnectionStaticPanel.vue
@@ -5,7 +5,7 @@
         <div class="flex-fill flex flex-row mr4-l mb3 mb0-l">
           <ServiceIcon class="flex-none mt1 br2 square-4" :type="ctype" :url="url" :empty-cls="''" />
           <div class="flex-fill flex flex-column" style="margin-left: 12px">
-            <div class="f4 fw6 lh-title">{{connection.short_description}}</div>
+            <div class="f4 fw6 lh-title">{{connection.name}}</div>
             <div class="f6 fw4 mt1 lh-copy silver" v-if="cdesc.length > 0">{{cdesc}}</div>
             <div class="f6 fw4 mt1" v-else><em class="moon-gray">(No description)</em></div>
           </div>
