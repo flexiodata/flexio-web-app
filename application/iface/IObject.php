8abf6cebea51fb19d7daab8bf6e6da288579ae36
Aaron Williams <aaron@flex.io>
2019-07-16 12:23:57 -0500

    removed old rights logic

--- application/iface/IObject.php
+++ application/iface/IObject.php
@@ -36,7 +36,6 @@ interface IObject
 
     public function allows(string $access_code, string $action);
     public function grant(string $access_code, string $access_type, array $actions);
-    public function revoke(string $access_code, string $access_type, array $actions = null);
 
     public function setRights(array $rights);
     public function getRights();
