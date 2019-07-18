623a54aea46f542b10e9ba317df8e6771cfe3931
Aaron Williams <aaron@flex.io>
2019-07-18 13:19:42 -0500

    added api scaffolding for team list

---
+++ application/api/Team.php
@@ -0,0 +1,55 @@
+<?php
+/**
+ *
+ * Copyright (c) 2019, Gold Prairie LLC. All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2019-07-18
+ *
+ * @package flexio
+ * @subpackage Api
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Api;
+
+
+class Team
+{
+    public static function list(\Flexio\Api\Request $request) : void
+    {
+        $query_params = $request->getQueryParams();
+        $requesting_user_eid = $request->getRequestingUser();
+        $owner_user_eid = $request->getOwnerFromUrl();
+
+        $validator = \Flexio\Base\Validator::create();
+        if (($validator->check($query_params, array(
+                'start'       => array('type' => 'integer', 'required' => false),
+                'tail'        => array('type' => 'integer', 'required' => false),
+                'limit'       => array('type' => 'integer', 'required' => false),
+                'created_min' => array('type' => 'date',    'required' => false),
+                'created_max' => array('type' => 'date',    'required' => false)
+            ))->hasErrors()) === true)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INVALID_SYNTAX);
+
+        $validated_query_params = $validator->getParams();
+
+        // check the rights on the owner; ability to get a list of teams is governed
+        // currently by user read privileges
+        $owner_user = \Flexio\Object\User::load($owner_user_eid);
+        if ($owner_user->getStatus() === \Model::STATUS_DELETED)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNAVAILABLE);
+        if ($owner_user->allows($requesting_user_eid, \Flexio\Object\Action::TYPE_WRITE) === false)
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::INSUFFICIENT_RIGHTS);
+
+        // get the teams
+        $result = array();
+
+        // TODO: fill out
+
+        $request->setResponseCreated(\Flexio\Base\Util::getCurrentTimestamp());
+        \Flexio\Api\Response::sendContent($result);
+    }
+}
