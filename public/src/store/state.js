2f324b587ccfceeda105993163093bbdff6e8d65
David Z. Williams <dave@flex.io>
2019-07-18 15:29:57 -0400

    Removed a ton of old cruft (unused mutations and actions).

--- public/src/store/state.js
+++ public/src/store/state.js
@@ -1,26 +1,24 @@
 export default () => {
   return {
+    active_user_eid: '',
+    active_document_identifier: '',
+    routed_user: '',
+
     // TODO: move all of these into `objects_fetched` and `objects_fetching` objects?
     user_fetched: false,
     user_fetching: false,
-    processes_fetched: false,
-    processes_fetching: false,
-    process_summary_fetched: false,
-    process_summary_fetching: false,
-    projects_fetched: false,
-    projects_fetching: false,
     pipes_fetched: false,
     pipes_fetching: false,
     connections_fetched: false,
     connections_fetching: false,
+    members_fetched: false,
+    members_fetching: false,
     tokens_fetched: false,
     tokens_fetching: false,
-    trash_fetched: false,
-    trash_fetching: false,
-
-    active_user_eid: '',
-    active_document_identifier: '',
-    routed_user: '',
+    processes_fetched: false,
+    processes_fetching: false,
+    process_summary_fetched: false,
+    process_summary_fetching: false,
 
     objects: {}
   }
