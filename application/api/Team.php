77b0c15c0b450081f0e1c8891e851ca33dccd928
Aaron Williams <aaron@flex.io>
2019-07-18 13:51:41 -0500

    implemented api call for listing teams

--- application/api/Team.php
+++ application/api/Team.php
@@ -44,12 +44,53 @@ class Team
         if ($owner_user->allows($requesting_user_eid, \Flexio\Object\Action::TYPE_WRITE) === false)
             throw new \Flexio\Base\Exception(\Flexio\Base\Error::INSUFFICIENT_RIGHTS);
 
-        // get the teams
+        // get the teams (items where the owner is a member of a team)
         $result = array();
 
-        // TODO: fill out
+        $filter = array('member_eid' => $owner_user_eid);
+        $filter = array_merge($validated_query_params, $filter); // give precedence to fixed owner/status
+        $teams = \Flexio\System\System::getModel()->team->list($filter);
+
+        foreach ($teams as $t)
+        {
+            // TODO: check permissions for each item?
+            $result[] = self::formatProperties($t);
+        }
 
         $request->setResponseCreated(\Flexio\Base\Util::getCurrentTimestamp());
         \Flexio\Api\Response::sendContent($result);
+    }
+
+    private static function formatProperties(array $properties) : array
+    {
+        // sanity check: if the data record is missing, then owned_by (eid is
+        // normally used) will be null
+        if (!isset($properties['owned_by']))
+            throw new \Flexio\Base\Exception(\Flexio\Base\Error::READ_FAILED);
+
+        // get the team info
+        $team_info = array();
+        $team_info['eid'] = $properties['owned_by'];
+        $team_info['eid_type'] = \Model::TYPE_USER;
+
+        try
+        {
+            $user = \Flexio\Object\User::load($properties['owned_by']);
+            $user_info = $user->get();
+            $team_info['eid_status'] = $user_info['eid_status'];
+            $team_info['username'] = $user_info['username'];
+            $team_info['first_name'] = $user_info['first_name'];
+            $team_info['last_name'] = $user_info['last_name'];
+            $team_info['email'] = $user_info['email'];
+            $team_info['email_hash'] = $user_info['email_hash'];
+            $team_info['created'] = $user_info['created'];
+            $team_info['updated'] = $user_info['updated'];
+        }
+        catch (\Exception $e)
+        {
+        }
+
+        // return the info
+        return $team_info;
     }
 }
