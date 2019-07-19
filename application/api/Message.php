696141db42e0de2e54c53140ee4ef42367ff6711
Aaron Williams <aaron@flex.io>
2019-07-19 12:16:13 -0500

    created function for sending team email invitation

--- application/api/Message.php
+++ application/api/Message.php
@@ -79,6 +79,54 @@ class Message
         return $email->send();
     }
 
+    public static function sendTeamInvitationEmail(array $params) : bool
+    {
+        $validator = \Flexio\Base\Validator::create();
+        if (($validator->check($params, array(
+                'email'       => array('type' => 'email',  'required' => true),
+                'from_name'   => array('type' => 'string', 'required' => true),
+                'from_email'  => array('type' => 'email',  'required' => true),
+                'object_name' => array('type' => 'string', 'required' => true),
+                'message'     => array('type' => 'string', 'required' => false)
+            ))->hasErrors()) === true)
+            return false;
+
+        $validated_params = $validator->getParams();
+        $to = $validated_params['email'];
+        $from_name = $validated_params['from_name'];
+        $from_email = $validated_params['from_email'];
+        $object_name = $validated_params['object_name'];
+        $message = $validated_params['message'] ?? '';
+        $share_link = self::getBaseUrl() . "/app/$object_name";
+
+        // get text template from the application res directory
+        $msg_text = self::getTextEmail('project-share', [
+            'name' => $from_name,
+            'from_email' => $from_email,
+            'message' => (strlen($message) == 0) ? '' : "\n$message\n",
+            'object_name' => $object_name,
+            'share_link' => $share_link
+        ]);
+
+        // get html template from the application res directory
+        $msg_html = self::getHtmlEmail('project-share', [
+            'name' => $from_name,
+            'from_email' => $from_email,
+            'message' => (strlen($message) == 0) ? '' : "$message<br>",
+            'object_name' => $object_name,
+            'share_link' => $share_link
+        ]);
+
+        $email = \Flexio\Services\NoticeEmail::create(array(
+            'from' => "$from_name via Flex.io <no-reply@flex.io>",
+            'to' => $to,
+            'subject' => "${from_name} invited you into the \"${object_name}\" team",
+            'msg_text' => $msg_text,
+            'msg_html' => $msg_html
+        ));
+        return $email->send();
+    }
+
     public static function sendSharePipeEmail(array $params) : bool
     {
         $validator = \Flexio\Base\Validator::create();
