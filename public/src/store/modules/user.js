753e45e197f1bdda9d592b9ee2d9c31a1e0062f6
David Z. Williams <dave@flex.io>
2019-07-20 23:52:04 -0400

    Added modules for splitting out object types in Vuex.

---
+++ public/src/store/modules/user.js
@@ -0,0 +1,22 @@
+const getDefaultState = () => {
+  return {
+    fetching: false,
+    fetched: false
+  }
+}
+
+const state = getDefaultState()
+
+const mutations = {}
+
+const actions = {}
+
+const getters = {}
+
+export default {
+  namespaced: true,
+  state,
+  mutations,
+  actions,
+  getters
+}
