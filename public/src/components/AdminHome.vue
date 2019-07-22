8cbb42413d44b788d7e1a2945f03c3fb42fca0e1
David Z. Williams <dave@flex.io>
2019-07-22 11:23:27 -0400

    Show Flex.io logo/link back to main app area in admin sidebar.

--- public/src/components/AdminHome.vue
+++ public/src/components/AdminHome.vue
@@ -1,6 +1,11 @@
 <template>
   <div class="flex flex-column flex-row-l items-stretch bg-nearer-white">
     <div class="flex flex-row flex-column-l flex-none items-stretch bb bb-0-l br-l b--black-05 pv0 pt4-l ph1-l overflow-auto trans-pm">
+      <router-link to="/pipes" class="mt1 ph1 pl3-l pr5-l pv2">
+        <div class="dib hint--bottom" aria-label="Home">
+          <img src="../assets/logo-flexio-navbar.png" alt="Flex.io">
+        </div>
+      </router-link>
       <router-link
         class="flex-auto flex-none-l no-underline truncate tc tl-l link f5 fw6 mt1 ph1 pl3-l pr5-l pv2 bb bb-0-l bw1 bw2-l css-nav-item"
         :to="item.route"
