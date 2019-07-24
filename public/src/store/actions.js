b125b3e00732d18c8888ccb4dc9e6298af00cd9a
David Z. Williams <dave@flex.io>
2019-07-24 15:00:23 -0400

    Fetch all module names and dynamically commit `RESET_STATE` for each module.

--- public/src/store/actions.js
+++ public/src/store/actions.js
@@ -1,14 +1,14 @@
+const files = require.context('./modules', false, /\.js$/)
+const modules_names = []
+
+files.keys().forEach(key => {
+  if (key === './index.js') return
+  modules_names.push(key.replace(/(\.\/|\.js)/g, ''))
+})
+
 export default {
   'resetState' ({ commit }) {
     commit('RESET_STATE')
-    commit('connections/RESET_STATE')
-    commit('members/RESET_STATE')
-    commit('pipedocument/RESET_STATE')
-    commit('pipes/RESET_STATE')
-    commit('processes/RESET_STATE')
-    commit('streams/RESET_STATE')
-    commit('teams/RESET_STATE')
-    commit('tokens/RESET_STATE')
-    commit('users/RESET_STATE')
+    _.each(modules_names, module => { commit(`${module}/RESET_STATE`) })
   },
 }
