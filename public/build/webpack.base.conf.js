03d6361491dbd3e80b2342f2dd966cedd2133bdb
David Z. Williams <dave@flex.io>
2019-07-29 23:35:36 -0400

    Lock down NPM version: element-ui@2.4.0 and vue@2.5.17.

--- public/build/webpack.base.conf.js
+++ public/build/webpack.base.conf.js
@@ -79,7 +79,7 @@ module.exports = {
         test: /\.(woff2?|eot|ttf|otf)(\?.*)?$/,
         loader: 'url-loader',
         options: {
-          limit: 10000,
+          limit: 12000,
           name: 'fonts/[name].[hash:7].[ext]'
         }
       }
