1716aa665f1830a098eb1d827540b0ced161d6ce
David Z. Williams <dave@flex.io>
2019-09-03 12:08:31 -0400

    Pipe deploy toggle switch is back.

--- public/src/components/PipeDocument.vue
+++ public/src/components/PipeDocument.vue
@@ -11,7 +11,28 @@
   <div v-else-if="is_fetched">
     <!-- header -->
     <div class="flex flex-row">
-      {{pipe.name}}
+      <div class="flex-fill flex flex-row items-center">
+        <div class="f4 fw6 lh-title">{{pipe.name}}</div>
+        <LabelSwitch
+          class="ml3 hint--bottom"
+          active-color="#13ce66"
+          :aria-label="is_deployed ? 'Turn function off' : 'Turn function on'"
+          :width="58"
+          v-model="is_deployed"
+          v-require-rights:pipe.update
+        />
+      </div>
+      <div class="flex-none">
+        <el-button
+          class="ttu fw6"
+          style="min-width: 5rem"
+          size="small"
+          type="primary"
+          v-require-rights:pipe.update
+        >
+          Edit
+        </el-button>
+      </div>
     </div>
 
     <!-- content -->
@@ -24,6 +45,11 @@
 <script>
   import { mapState } from 'vuex'
   import Spinner from 'vue-simple-spinner'
+  import LabelSwitch from '@/components/LabelSwitch'
+
+  const DEPLOY_MODE_UNDEFINED = ''
+  const DEPLOY_MODE_BUILD     = 'B'
+  const DEPLOY_MODE_RUN       = 'R'
 
   export default {
     props: {
@@ -33,7 +59,8 @@
       }
     },
     components: {
-      Spinner
+      Spinner,
+      LabelSwitch
     },
     watch: {
       pipeEid: {
@@ -57,7 +84,27 @@
         return _.get(this.pipe, 'vuex_meta.is_fetched', false)
       },
       is_fetching() {
-        return _.get(this.pipe, 'vuex_meta.is_fetching', false) || this.is_local_fetching
+        return _.get(this.pipe, 'vuex_meta.is_fetching', false) || this.is_local_fetching /* Vuex pipe `is_fetching` isn't yet implemented */
+      },
+      is_deployed: {
+        get() {
+          return _.get(this.pipe, 'deploy_mode') == DEPLOY_MODE_RUN ? true : false
+        },
+        set(value) {
+          if (value === false) {
+            this.$confirm('This function is turned on and is possibly being used in a production environment. Are you sure you want to continue?', 'Really turn function off?', {
+              confirmButtonClass: 'ttu fw6',
+              cancelButtonClass: 'ttu fw6',
+              confirmButtonText: 'Turn function off',
+              cancelButtonText: 'Cancel',
+              type: 'warning'
+            }).then(() => {
+              this.deployPipe(value)
+            })
+          } else {
+            this.deployPipe(value)
+          }
+        }
       },
     },
     methods: {
@@ -71,6 +118,31 @@
             this.is_local_fetching = false
           })
         }
+      },
+      deployPipe(is_deployed) {
+        var team_name = this.active_team_name
+        var eid = this.pipe.eid
+
+        var deploy_mode = is_deployed === false ? DEPLOY_MODE_BUILD : DEPLOY_MODE_RUN
+        var attrs = { deploy_mode }
+
+        this.$store.dispatch('pipes/update', { team_name, eid, attrs }).then(response => {
+          var updated_pipe = response.data
+
+          this.$message({
+            message: 'The function was updated successfully.',
+            type: 'success'
+          })
+
+          // change the object name in the route
+          if (updated_pipe.name != this.pipe.name) {
+            var object_name = updated_pipe.name
+
+            var new_route = _.pick(this.$route, ['name', 'meta', 'params', 'path'])
+            new_route.params = _.assign({}, new_route.params, { object_name })
+            this.$router.replace(new_route)
+          }
+        })
       }
     }
   }
