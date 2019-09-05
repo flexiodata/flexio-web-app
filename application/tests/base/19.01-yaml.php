dc9eed22e691eac34f375c8e29bf2069be5449e4
Aaron Williams <aaron@flex.io>
2019-09-05 13:11:25 -0500

    updated tests

--- application/tests/base/19.01-yaml.php
+++ application/tests/base/19.01-yaml.php
@@ -1,11 +1,11 @@
 <?php
 /**
  *
- * Copyright (c) 2016, Gold Prairie LLC. All rights reserved.
+ * Copyright (c) 2019, Gold Prairie LLC. All rights reserved.
  *
  * Project:  Flex.io App
  * Author:   Aaron L. Williams
- * Created:  2016-11-01
+ * Created:  2019-09-04
  *
  * @package flexio
  * @subpackage Tests
@@ -20,7 +20,7 @@ class Test
 {
     public function run(&$results)
     {
-        // TEST: yaml parse
+        // TEST: yaml extract
 
         // BEGIN TEST
         $content = <<<EOD
@@ -42,14 +42,14 @@ EOD;
         \Flexio\Tests\Check::assertArray('A.2', '\Flexio\Base\Yaml::parse(); basic test',  $actual, $expected, $results);
 
 
-        // TEST: front matter extract
+        // TEST: yaml extract
 
         // BEGIN TEST
         $content = <<<EOD
 EOD;
         $actual = \Flexio\Base\Yaml::extract($content);
         $expected = '';
-        \Flexio\Tests\Check::assertString('B.1', '\Flexio\Base\Yaml::extract(); basic test', $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.1', '\Flexio\Base\Yaml::extract(); extract yaml from front matter in comments', $actual, $expected, $results);
 
         // BEGIN TEST
         $content = <<<EOD
@@ -61,7 +61,7 @@ EOD;
         $expected = <<<EOD
 name: test
 EOD;
-        \Flexio\Tests\Check::assertString('B.2', '\Flexio\Base\Yaml::extract(); basic test',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.2', '\Flexio\Base\Yaml::extract(); extract yaml from front matter in comments',  $actual, $expected, $results);
 
         // BEGIN TEST
         $content = <<<EOD
@@ -75,7 +75,7 @@ EOD;
         $expected = <<<EOD
 name: test
 EOD;
-        \Flexio\Tests\Check::assertString('B.3', '\Flexio\Base\Yaml::extract(); basic test',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.3', '\Flexio\Base\Yaml::extract(); extract yaml from front matter in comments',  $actual, $expected, $results);
 
         // BEGIN TEST
         $content = <<<EOD
@@ -89,7 +89,7 @@ EOD;
         $expected = <<<EOD
 name: test
 EOD;
-        \Flexio\Tests\Check::assertString('B.4', '\Flexio\Base\Yaml::extract(); basic test',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.4', '\Flexio\Base\Yaml::extract(); extract yaml from front matter in comments',  $actual, $expected, $results);
 
         // BEGIN TEST
         $content = <<<EOD
@@ -103,7 +103,7 @@ EOD;
         $expected = <<<EOD
 name: test
 EOD;
-        \Flexio\Tests\Check::assertString('B.5', '\Flexio\Base\Yaml::extract(); basic test',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.5', '\Flexio\Base\Yaml::extract(); extract yaml from front matter in comments',  $actual, $expected, $results);
 
         // BEGIN TEST
         $content = <<<EOD
@@ -117,7 +117,7 @@ EOD;
         $expected = <<<EOD
 name: test
 EOD;
-        \Flexio\Tests\Check::assertString('B.6', '\Flexio\Base\Yaml::extract(); basic test',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.6', '\Flexio\Base\Yaml::extract(); extract yaml from front matter in comments',  $actual, $expected, $results);
 
         // BEGIN TEST
         $content = <<<EOD
@@ -131,7 +131,7 @@ EOD;
         $expected = <<<EOD
 name: test
 EOD;
-        \Flexio\Tests\Check::assertString('B.7', '\Flexio\Base\Yaml::extract(); basic test',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.7', '\Flexio\Base\Yaml::extract(); extract yaml from front matter in comments',  $actual, $expected, $results);
 
         // BEGIN TEST
         $content = <<<EOD
@@ -145,7 +145,7 @@ EOD;
         $expected = <<<EOD
 name: test
 EOD;
-        \Flexio\Tests\Check::assertString('B.8', '\Flexio\Base\Yaml::extract(); basic test',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.8', '\Flexio\Base\Yaml::extract(); extract yaml from front matter in comments',  $actual, $expected, $results);
 
         // BEGIN TEST
         $content = <<<EOD
@@ -159,7 +159,7 @@ EOD;
         $expected = <<<EOD
 name: test
 EOD;
-        \Flexio\Tests\Check::assertString('B.9', '\Flexio\Base\Yaml::extract(); basic test',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.9', '\Flexio\Base\Yaml::extract(); extract yaml from front matter in comments',  $actual, $expected, $results);
 
         // BEGIN TEST
         $content = <<<EOD
@@ -173,7 +173,7 @@ EOD;
         $expected = <<<EOD
 name: test
 EOD;
-        \Flexio\Tests\Check::assertString('B.10', '\Flexio\Base\Yaml::extract(); basic test',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.10', '\Flexio\Base\Yaml::extract(); extract yaml from front matter in comments',  $actual, $expected, $results);
 
 
         // BEGIN TEST
@@ -194,7 +194,7 @@ colors:
   - green
   - blue
 EOD;
-        \Flexio\Tests\Check::assertString('B.11', '\Flexio\Base\Yaml::extract(); basic test',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.11', '\Flexio\Base\Yaml::extract(); extract yaml from front matter in comments',  $actual, $expected, $results);
     }
 }
 
