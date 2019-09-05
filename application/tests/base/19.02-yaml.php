2f04a64bed604b4d2c25697a31d45c1919358838
Aaron Williams <aaron@flex.io>
2019-09-05 13:16:30 -0500

    added basic yaml parse test

---
+++ application/tests/base/19.02-yaml.php
@@ -0,0 +1,41 @@
+<?php
+/**
+ *
+ * Copyright (c) 2019, Gold Prairie LLC. All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2019-09-05
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // TEST: yaml parse
+
+        // BEGIN TEST
+        $content = <<<EOD
+# ---
+# name: test
+# ---
+EOD;
+        $yaml = \Flexio\Base\Yaml::extract($content);
+        $actual = \Flexio\Base\Yaml::parse($yaml);
+        $expected = '
+        {
+            "name": "test"
+        }
+        ';
+        \Flexio\Tests\Check::assertArray('A.1', '\Flexio\Base\Yaml::parse(); extract and parse yaml from front matter in comments',  $actual, $expected, $results);
+    }
+}
+
