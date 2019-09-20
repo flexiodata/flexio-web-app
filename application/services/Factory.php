043a594d429d27097e70d34f3d74dd8c0deb0a71
Aaron Williams <aaron@flex.io>
2019-09-20 17:32:10 -0500

    streamlined service creation

--- application/services/Factory.php
+++ application/services/Factory.php
@@ -59,213 +59,82 @@ class Factory
         // get the connection type and the corresponding service
         $connection_type = $connection_properties['connection_type'] ?? '';
         $connection_info = $connection_properties['connection_info'] ?? array();
-
-        switch ($connection_type)
-        {
-            default:
-                return false;
-
-            case self::TYPE_HTTP:
-                    $auth_params = array(
-                        // TODO: add parameters for basic auth?
-                    );
-                    $service = \Flexio\Services\Http::create($auth_params);
-                break;
-
-            case self::TYPE_FTP:
-                    $auth_params = array(
-                        'host' => $connection_info['host'] ?? '',
-                        'port' => $connection_info['port'] ?? '',
-                        'username' => $connection_info['username'] ?? '',
-                        'password' => $connection_info['password'] ?? '',
-                        'base_path' => $connection_info['base_path'] ?? ''
-                    );
-                    $service = \Flexio\Services\Ftp::create($auth_params);
-                break;
-
-            case self::TYPE_SFTP:
-                    $auth_params = array(
-                        'host' => $connection_info['host'] ?? '',
-                        'port' => $connection_info['port'] ?? '',
-                        'username' => $connection_info['username'] ?? '',
-                        'password' => $connection_info['password'] ?? '',
-                        'base_path' => $connection_info['base_path'] ?? ''
-                    );
-                    $service = \Flexio\Services\Sftp::create($auth_params);
-                break;
-
-            case self::TYPE_MYSQL:
-                    $auth_params = array(
-                        'host' => $connection_info['host'] ?? '',
-                        'port' => $connection_info['port'] ?? '',
-                        'username' => $connection_info['username'] ?? '',
-                        'password' => $connection_info['password'] ?? '',
-                        'database' => $connection_info['database'] ?? ''
-                    );
-                    $service = \Flexio\Services\MySql::create($auth_params);
-                break;
-
-            case self::TYPE_POSTGRES:
-                    $auth_params = array(
-                        'host' => $connection_info['host'] ?? '',
-                        'port' => $connection_info['port'] ?? '',
-                        'username' => $connection_info['username'] ?? '',
-                        'password' => $connection_info['password'] ?? '',
-                        'database' => $connection_info['database'] ?? ''
-                    );
-                    $service = \Flexio\Services\Postgres::create($auth_params);
-                break;
-
-            case self::TYPE_ELASTICSEARCH:
-                    $auth_params = array(
-                        'host' => $connection_info['host'] ?? '',
-                        'port' => $connection_info['port'] ?? '',
-                        'username' => $connection_info['username'] ?? '',
-                        'password' => $connection_info['password'] ?? '',
-                        'database' => $connection_info['database'] ?? ''
-                    );
-                    $service = \Flexio\Services\ElasticSearch::create($auth_params);
-                break;
-
-            case self::TYPE_DROPBOX:
-                    $auth_params = array(
-                        'access_token' => $connection_info['access_token'] ?? '',
-                        'base_path' => $connection_info['base_path'] ?? ''
-                    );
-                    $service = \Flexio\Services\Dropbox::create($auth_params);
-                break;
-
-            case self::TYPE_BOX:
-                    $auth_params = array(
-                        'access_token' => $connection_info['access_token'] ?? '',
-                        'refresh_token' => $connection_info['refresh_token'] ?? '',
-                        'base_path' => $connection_info['base_path'] ?? ''
-                    );
-                    if (isset($connection_info['expires'])) $auth_params['expires'] = $connection_info['expires'];
-                    $service = \Flexio\Services\Box::create($auth_params);
-                break;
-
-
-            case self::TYPE_GMAIL:
-                    $auth_params = array(
-                    'access_token' => $connection_info['access_token'] ?? '',
-                    'refresh_token' => $connection_info['refresh_token'] ?? ''
-                    );
-                    if (isset($connection_info['expires'])) $auth_params['expires'] = $connection_info['expires'];
-                    $service = \Flexio\Services\Gmail::create($auth_params);
-            break;
-
-            case self::TYPE_GOOGLEDRIVE:
-                    $auth_params = array(
-                        'access_token' => $connection_info['access_token'] ?? '',
-                        'refresh_token' => $connection_info['refresh_token'] ?? '',
-                        'base_path' => $connection_info['base_path'] ?? ''
-                    );
-                    if (isset($connection_info['expires'])) $auth_params['expires'] = $connection_info['expires'];
-                    $service = \Flexio\Services\GoogleDrive::create($auth_params);
-                break;
-
-            case self::TYPE_GOOGLESHEETS:
-                    $auth_params = array(
-                        'access_token' => $connection_info['access_token'] ?? '',
-                        'refresh_token' => $connection_info['refresh_token'] ?? ''
-                    );
-                    if (isset($connection_info['expires'])) $auth_params['expires'] = $connection_info['expires'];
-                    $service = \Flexio\Services\GoogleSheets::create($auth_params);
-                break;
-
-            case self::TYPE_GOOGLECLOUDSTORAGE:
-                $auth_params = array(
-                    'access_token' => $connection_info['access_token'] ?? '',
-                    'refresh_token' => $connection_info['refresh_token'] ?? '',
-                    'region' => $connection_info['region'] ?? '',
-                    'bucket' => $connection_info['bucket'] ?? '',
-                    'base_path' => $connection_info['base_path'] ?? ''
-                );
-                if (isset($connection_info['expires'])) $auth_params['expires'] = $connection_info['expires'];
-                $service = \Flexio\Services\GoogleCloudStorage::create($auth_params);
-            break;
-
-            case self::TYPE_GITHUB:
-                $auth_params = array(
-                    'access_token' => $connection_info['access_token'] ?? '',
-                    'refresh_token' => $connection_info['refresh_token'] ?? '',
-                    'owner' => $connection_info['owner'] ?? '',
-                    'repository' => $connection_info['repository'] ?? '',
-                    'base_path' => $connection_info['base_path'] ?? ''
-                );
-                if (isset($connection_info['expires'])) $auth_params['expires'] = $connection_info['expires'];
-                $service = \Flexio\Services\GitHub::create($auth_params);
-                break;
-
-            case self::TYPE_LINKEDIN:
-                $auth_params = array(
-                    'access_token' => $connection_info['access_token'] ?? '',
-                    'refresh_token' => $connection_info['refresh_token'] ?? '',
-                    'base_path' => $connection_info['base_path'] ?? ''
-                );
-                if (isset($connection_info['expires'])) $auth_params['expires'] = $connection_info['expires'];
-                $service = \Flexio\Services\LinkedIn::create($auth_params);
-            break;
-
-            case self::TYPE_SMTP:
-            case self::TYPE_EMAIL:
-                $service = \Flexio\Services\Email::create($connection_info);
-                break;
-
-            case self::TYPE_AMAZONS3:
-                    $auth_params = array(
-                        'region' => $connection_info['region'] ?? '',
-                        'bucket' => $connection_info['bucket'] ?? '',
-                        'accesskey' => $connection_info['aws_key'] ?? '',
-                        'secretkey' => $connection_info['aws_secret'] ?? '',
-                        'base_path' => $connection_info['base_path'] ?? ''
-                    );
-                    $service = \Flexio\Services\AmazonS3::create($auth_params);
-                break;
-
-            case self::TYPE_SOCRATA:
-                   $auth_params = array(
-                        'host' => $connection_info['host'] ?? '',
-                        'port' => $connection_info['port'] ?? '',
-                        'username' => $connection_info['username'] ?? '',
-                        'password' => $connection_info['password'] ?? ''
-                    );
-                    $service = \Flexio\Services\Socrata::create($auth_params);
-                break;
-
-            case self::TYPE_PIPELINEDEALS:
-                   $auth_params = array(
-                        'access_token' => $connection_info['access_token'] ?? ''
-                    );
-                    $service = \Flexio\Services\PipelineDeals::create($auth_params);
-                break;
-
-            case self::TYPE_MAILJET:
-                   $auth_params = array(
-                        'username' => $connection_info['username'] ?? '',
-                        'password' => $connection_info['password'] ?? ''
-                    );
-                    $service = \Flexio\Services\MailJet::create($auth_params);
-                break;
-
-            case self::TYPE_TWILIO:
-                    $auth_params = array(
-                        'username' => $connection_info['username'] ?? '',
-                        'password' => $connection_info['password'] ?? ''
-                    );
-                    $service = \Flexio\Services\Twilio::create($auth_params);
-                break;
-        }
-
-        if (!$service)
-            return false;
+        $service = self::createService($connection_type, $connection_info);
 
         // if we have a connection hash, the cache the connection
         if ($connection_hash !== false)
             $g_store->connections[$connection_hash] = $service;
 
         return $service;
+    }
+
+    private static function createService(string $connection_type, array $connection_info) // TODO: add return type
+    {
+        switch ($connection_type)
+        {
+            case self::TYPE_HTTP:
+                return \Flexio\Services\Http::create($connection_info);
+
+            case self::TYPE_FTP:
+                return \Flexio\Services\Ftp::create($connection_info);
+
+            case self::TYPE_SFTP:
+                return \Flexio\Services\Sftp::create($connection_info);
+
+            case self::TYPE_MYSQL:
+                return \Flexio\Services\MySql::create($connection_info);
+
+            case self::TYPE_POSTGRES:
+                return \Flexio\Services\Postgres::create($connection_info);
+
+            case self::TYPE_ELASTICSEARCH:
+                return \Flexio\Services\ElasticSearch::create($connection_info);
+
+            case self::TYPE_DROPBOX:
+                return \Flexio\Services\Dropbox::create($connection_info);
+
+            case self::TYPE_BOX:
+                return \Flexio\Services\Box::create($connection_info);
+
+            case self::TYPE_GMAIL:
+                return \Flexio\Services\Gmail::create($connection_info);
+
+            case self::TYPE_GOOGLEDRIVE:
+                return \Flexio\Services\GoogleDrive::create($connection_info);
+
+            case self::TYPE_GOOGLESHEETS:
+                return \Flexio\Services\GoogleSheets::create($connection_info);
+
+            case self::TYPE_GOOGLECLOUDSTORAGE:
+                return \Flexio\Services\GoogleCloudStorage::create($connection_info);
+
+            case self::TYPE_GITHUB:
+                return \Flexio\Services\GitHub::create($connection_info);
+
+            case self::TYPE_LINKEDIN:
+                return \Flexio\Services\LinkedIn::create($connection_info);
+
+            case self::TYPE_SMTP:
+            case self::TYPE_EMAIL:
+                return \Flexio\Services\Email::create($connection_info);
+
+            case self::TYPE_AMAZONS3:
+                return \Flexio\Services\AmazonS3::create($connection_info);
+
+            case self::TYPE_SOCRATA:
+                return \Flexio\Services\Socrata::create($connection_info);
+
+            case self::TYPE_PIPELINEDEALS:
+                return \Flexio\Services\PipelineDeals::create($connection_info);
+
+            case self::TYPE_MAILJET:
+                return \Flexio\Services\MailJet::create($connection_info);
+
+            case self::TYPE_TWILIO:
+                return \Flexio\Services\Twilio::create($connection_info);
+        }
+
+        throw new \Flexio\Base\Exception(\Flexio\Base\Error::UNAVAILABLE);
     }
 
     private static function createConnectionHash(array $connection_info) : string
