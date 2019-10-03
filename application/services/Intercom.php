4cf85fc450c0cce23388ef9c723df02b008651a2
Aaron Williams <aaron@flex.io>
2019-10-03 17:09:27 -0500

    in intercom service, set the state manually in the redirect url

--- application/services/Intercom.php
+++ application/services/Intercom.php
@@ -154,6 +154,18 @@ class Intercom implements \Flexio\IFace\IConnection,
 
         // STEP 4: generate the URL to make request to authorize our application
         $url = $oauthprovider->getAuthorizationUrl();
+
+        // TODO: for some reason, the state parameter isn't being set;
+        // replace it manually
+
+        $url_base = strtok($url,'?');
+        $url_query = parse_url($url, PHP_URL_QUERY);
+
+        $url_query_params = array();
+        parse_str($url_query, $url_query_params);
+        $url_query_params['state'] = $params['state'];
+        $url = $url_base . '?' . http_build_query($url_query_params);
+
         $this->authorization_uri = $url;
 
         return false;
