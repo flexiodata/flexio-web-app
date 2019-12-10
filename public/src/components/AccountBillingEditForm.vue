5733218274c5cca3c9ae2063e116a3965b921f11
David Z. Williams <dave@flex.io>
2019-12-10 00:24:34 -0500

    Gray out payment/billing forms and buttons when submitting.

--- public/src/components/AccountBillingEditForm.vue
+++ public/src/components/AccountBillingEditForm.vue
@@ -1,242 +1,245 @@
 <template>
   <div>
-    <el-form
-      class="mb3 pb1 el-form--cozy el-form__label-tiny"
-      ref="billingContactForm"
-      size="small"
-      :model="billing_info"
-      :rules="billing_contact_form_rules"
-      @validate="onValidateItem"
-      @submit.prevent.native
-      v-if="editMode == 'all' || editMode == 'contact'"
-    >
-      <h4 class="mb3 pb1 f7 ttu fw6 bb b--black-10">Billing contact</h4>
-
-      <el-form-item
-        class="w-50-l pr1-l"
-        style="max-width: 30rem"
-        label="Name"
-        key="billing_name"
-        prop="billing_name"
+    <div :class="is_submitting ? 'o-40 no-pointer-events': ''">
+      <el-form
+        class="mb3 pb1 el-form--cozy el-form__label-tiny"
+        ref="billingContactForm"
+        size="small"
+        :model="billing_info"
+        :rules="billing_contact_form_rules"
+        @validate="onValidateItem"
+        @submit.prevent.native
+        v-if="editMode == 'all' || editMode == 'contact'"
       >
-        <el-input
-          type="text"
-          ref="billingNameInput"
-          v-model.trim="billing_info.billing_name"
-        />
-      </el-form-item>
+        <h4 class="mb3 pb1 f7 ttu fw6 bb b--black-10">Billing contact</h4>
 
-      <el-form-item
-        class="w-50-l pr1-l"
-        style="max-width: 30rem"
-        label="Company"
-        key="billing_company"
-        prop="billing_company"
-      >
-        <el-input
-          type="text"
-          v-model.trim="billing_info.billing_company"
-        />
-      </el-form-item>
-
-      <el-form-item
-        class="w-50-l pr1-l"
-        style="max-width: 30rem"
-        label="Email"
-        key="billing_email"
-        prop="billing_email"
-      >
-        <el-input
-          type="email"
-          placeholder="accounting@example.com"
-          v-model.trim="billing_info.billing_email"
-        />
-        <div class="f8 i light-silver lh-copy" style="margin-top: 2px">
-          All future invoices will be sent to this email address
-        </div>
-      </el-form-item>
-    </el-form>
-
-    <el-form
-      class="mb3 pb1 el-form--cozy el-form__label-tiny"
-      ref="billingAddressForm"
-      size="small"
-      :model="billing_info"
-      :rules="billing_address_form_rules"
-      @validate="onValidateItem"
-      @submit.prevent.native
-      v-if="editMode == 'all' || editMode == 'address'"
-    >
-      <h4 class="mb3 pb1 f7 ttu fw6 bb b--black-10">Billing address</h4>
-
-      <el-form-item
-        class="w-50-l pr1-l"
-        style="max-width: 30rem"
-        label="Address"
-        key="billing_address1"
-        prop="billing_address1"
-      >
-        <el-input
-          type="text"
-          placeholder="Street"
-          ref="billingAddress1Input"
-          v-model.trim="billing_info.billing_address1"
-        />
-      </el-form-item>
-
-      <el-form-item
-        class="w-50-l pr1-l"
-        style="max-width: 30rem"
-        key="billing_address2"
-        prop="billing_address2"
-      >
-        <el-input
-          type="text"
-          placeholder="Apt/Suite"
-          v-model.trim="billing_info.billing_address2"
-        />
-      </el-form-item>
-
-      <el-form-item
-        class="w-50-l el-form-item__label-clear"
-        style="max-width: 30rem"
-        label="Country"
-        key="billing_country"
-        prop="billing_country"
-      >
-        <CountrySelect
-          v-model.trim="billing_info.billing_country"
-          @change="onCountryChange"
-        />
-      </el-form-item>
-
-      <div class="flex flex-column flex-row-l flex-wrap-l nl1 nr1">
-        <div class="w-50-l ph1">
-          <el-form-item
-            label="City"
-            key="billing_city"
-            prop="billing_city"
-          >
-            <el-input
-              type="text"
-              v-model.trim="billing_info.billing_city"
-            />
-          </el-form-item>
-        </div>
-        <div class="mw5 w-30-l ph1">
-          <el-form-item
-            label="State/Province"
-            key="billing_state"
-            prop="billing_state"
-          >
-            <StateSelect
-              class="w-100"
-              v-model.trim="billing_info.billing_state"
-              v-if="billing_info.billing_country == 'US'"
-            />
-            <el-input
-              type="text"
-              v-model="billing_info.billing_state"
-              v-else
-            />
-          </el-form-item>
-        </div>
-        <div class="mw5 w-20-l ph1">
-          <el-form-item
-            label="Zip/Postal"
-            key="billing_postal_code"
-            prop="billing_postal_code"
-          >
-            <el-input
-              type="text"
-              v-model.trim="billing_info.billing_postal_code"
-            />
-          </el-form-item>
-        </div>
-      </div>
-
-      <el-form-item
-        label="Other Billing Information"
-        key="billing_other"
-        prop="billing_other"
-      >
-        <el-input
-          type="textarea"
-          rows="4"
-          v-model.trim="billing_info.billing_other"
-        />
-        <div class="f8 i light-silver lh-copy" style="margin-top: 2px">
-          If you need to add a tax ID, VAT information or anything else to your invoice, you can do so here
-        </div>
-      </el-form-item>
-    </el-form>
-
-    <el-form
-      class="mb3 pb1 el-form--cozy el-form__label-tiny"
-      ref="billingPaymentMethodForm"
-      size="small"
-      :model="billing_info"
-      @submit.prevent.native
-      v-if="editMode == 'all' || editMode == 'card'"
-    >
-      <h4 class="mb3 pb1 f7 ttu fw6 bb b--black-10">Payment method</h4>
-
-      <div
-        class="el-form-item el-form-item--small w-50-l pr1-l"
-        style="max-width: 30rem"
-      >
-        <label class="db f8 mb1" for="card-number">
-          Card Number
-        </label>
-        <div>
-          <CardNumber class="stripe-element card-number"
-            ref="cardNumber"
-            :stripe="stripe"
-            :options="stripe_opts"
-            @change="number = $event.complete"
+        <el-form-item
+          class="w-50-l pr1-l"
+          style="max-width: 30rem"
+          label="Name"
+          key="billing_name"
+          prop="billing_name"
+        >
+          <el-input
+            type="text"
+            ref="billingNameInput"
+            v-model.trim="billing_info.billing_name"
           />
-        </div>
-      </div>
+        </el-form-item>
 
-      <div class="flex flex-column flex-row-l flex-wrap-l nl1 nr1">
-        <div class="w-20-l ph1" style="min-width: 10rem">
-          <div class="el-form-item el-form-item--small">
-            <label class="db f8 mb1" for="card-expiry">
-              Expiration Date
-            </label>
-            <div>
-              <CardExpiry class="stripe-element card-expiry"
-                ref="cardExpiry"
-                :stripe="stripe"
-                :options="stripe_opts"
-                @change="expiry = $event.complete"
+        <el-form-item
+          class="w-50-l pr1-l"
+          style="max-width: 30rem"
+          label="Company"
+          key="billing_company"
+          prop="billing_company"
+        >
+          <el-input
+            type="text"
+            v-model.trim="billing_info.billing_company"
+          />
+        </el-form-item>
+
+        <el-form-item
+          class="w-50-l pr1-l"
+          style="max-width: 30rem"
+          label="Email"
+          key="billing_email"
+          prop="billing_email"
+        >
+          <el-input
+            type="email"
+            placeholder="accounting@example.com"
+            v-model.trim="billing_info.billing_email"
+          />
+          <div class="f8 i light-silver lh-copy" style="margin-top: 2px">
+            All future invoices will be sent to this email address
+          </div>
+        </el-form-item>
+      </el-form>
+
+      <el-form
+        class="mb3 pb1 el-form--cozy el-form__label-tiny"
+        ref="billingAddressForm"
+        size="small"
+        :model="billing_info"
+        :rules="billing_address_form_rules"
+        @validate="onValidateItem"
+        @submit.prevent.native
+        v-if="editMode == 'all' || editMode == 'address'"
+      >
+        <h4 class="mb3 pb1 f7 ttu fw6 bb b--black-10">Billing address</h4>
+
+        <el-form-item
+          class="w-50-l pr1-l"
+          style="max-width: 30rem"
+          label="Address"
+          key="billing_address1"
+          prop="billing_address1"
+        >
+          <el-input
+            type="text"
+            placeholder="Street"
+            ref="billingAddress1Input"
+            v-model.trim="billing_info.billing_address1"
+          />
+        </el-form-item>
+
+        <el-form-item
+          class="w-50-l pr1-l"
+          style="max-width: 30rem"
+          key="billing_address2"
+          prop="billing_address2"
+        >
+          <el-input
+            type="text"
+            placeholder="Apt/Suite"
+            v-model.trim="billing_info.billing_address2"
+          />
+        </el-form-item>
+
+        <el-form-item
+          class="w-50-l el-form-item__label-clear"
+          style="max-width: 30rem"
+          label="Country"
+          key="billing_country"
+          prop="billing_country"
+        >
+          <CountrySelect
+            v-model.trim="billing_info.billing_country"
+            @change="onCountryChange"
+          />
+        </el-form-item>
+
+        <div class="flex flex-column flex-row-l flex-wrap-l nl1 nr1">
+          <div class="w-50-l ph1">
+            <el-form-item
+              label="City"
+              key="billing_city"
+              prop="billing_city"
+            >
+              <el-input
+                type="text"
+                v-model.trim="billing_info.billing_city"
               />
+            </el-form-item>
+          </div>
+          <div class="mw5 w-30-l ph1">
+            <el-form-item
+              label="State/Province"
+              key="billing_state"
+              prop="billing_state"
+            >
+              <StateSelect
+                class="w-100"
+                v-model.trim="billing_info.billing_state"
+                v-if="billing_info.billing_country == 'US'"
+              />
+              <el-input
+                type="text"
+                v-model="billing_info.billing_state"
+                v-else
+              />
+            </el-form-item>
+          </div>
+          <div class="mw5 w-20-l ph1">
+            <el-form-item
+              label="Zip/Postal"
+              key="billing_postal_code"
+              prop="billing_postal_code"
+            >
+              <el-input
+                type="text"
+                v-model.trim="billing_info.billing_postal_code"
+              />
+            </el-form-item>
+          </div>
+        </div>
+
+        <el-form-item
+          label="Other Billing Information"
+          key="billing_other"
+          prop="billing_other"
+        >
+          <el-input
+            type="textarea"
+            rows="4"
+            v-model.trim="billing_info.billing_other"
+          />
+          <div class="f8 i light-silver lh-copy" style="margin-top: 2px">
+            If you need to add a tax ID, VAT information or anything else to your invoice, you can do so here
+          </div>
+        </el-form-item>
+      </el-form>
+
+      <el-form
+        class="mb3 pb1 el-form--cozy el-form__label-tiny"
+        ref="billingPaymentMethodForm"
+        size="small"
+        :model="billing_info"
+        @submit.prevent.native
+        v-if="editMode == 'all' || editMode == 'card'"
+      >
+        <h4 class="mb3 pb1 f7 ttu fw6 bb b--black-10">Payment method</h4>
+
+        <div
+          class="el-form-item el-form-item--small w-50-l pr1-l"
+          style="max-width: 30rem"
+        >
+          <label class="db f8 mb1" for="card-number">
+            Card Number
+          </label>
+          <div>
+            <CardNumber class="stripe-element card-number"
+              ref="cardNumber"
+              :stripe="stripe"
+              :options="stripe_opts"
+              @change="number = $event.complete"
+            />
+          </div>
+        </div>
+
+        <div class="flex flex-column flex-row-l flex-wrap-l nl1 nr1">
+          <div class="w-20-l ph1" style="min-width: 10rem">
+            <div class="el-form-item el-form-item--small">
+              <label class="db f8 mb1" for="card-expiry">
+                Expiration Date
+              </label>
+              <div>
+                <CardExpiry class="stripe-element card-expiry"
+                  ref="cardExpiry"
+                  :stripe="stripe"
+                  :options="stripe_opts"
+                  @change="expiry = $event.complete"
+                />
+              </div>
+            </div>
+          </div>
+          <div class="w-20-l ph1" style="min-width: 10rem">
+            <div class="el-form-item el-form-item--small">
+              <label class="db f8 mb1" for="card-cvc">
+                CVC
+              </label>
+              <div>
+                <CardCvc class="stripe-element card-cvc"
+                  ref="cardCvc"
+                  :stripe="stripe"
+                  :options="stripe_opts"
+                  @change="cvc = $event.complete"
+                />
+              </div>
             </div>
           </div>
         </div>
-        <div class="w-20-l ph1" style="min-width: 10rem">
-          <div class="el-form-item el-form-item--small">
-            <label class="db f8 mb1" for="card-cvc">
-              CVC
-            </label>
-            <div>
-              <CardCvc class="stripe-element card-cvc"
-                ref="cardCvc"
-                :stripe="stripe"
-                :options="stripe_opts"
-                @change="cvc = $event.complete"
-              />
-            </div>
-          </div>
-        </div>
-      </div>
-    </el-form>
+      </el-form>
+    </div>
 
     <ButtonBar
       class="mt4"
       :cancel-button-visible="has_payment_method"
-      :submit-button-text="!has_payment_method ? 'Set up billing' : 'Save changes'"
-      :submit-button-disabled="has_errors"
+      :cancel-button-disabled="is_submitting"
+      :submit-button-text="is_submitting ? 'Submitting...' : !has_payment_method ? 'Submit' : 'Save changes'"
+      :submit-button-disabled="is_submitting || has_errors"
       @cancel-click="$emit('cancel-click')"
       @submit-click="onSubmit"
     />
@@ -306,6 +309,7 @@
         ],
       },
       form_errors: {},
+      is_submitting: false,
       complete: false,
       number: false,
       expiry: false,
@@ -451,6 +455,8 @@
           return
         }
 
+        this.is_submitting = true
+
         if (this.editMode == 'all' || this.editMode == 'card') {
           // createToken returns a Promise which resolves in a result object with
           // either a token or an error key.
@@ -462,11 +468,17 @@
 
             api.updateBilling('me', payload).then(response => {
               this.$emit('billing-updated', response.data)
+            }).finally(() => {
+              this.is_submitting = false
             })
+          }).catch(error => {
+            this.is_submitting = false
           })
         } else {
           api.updateBilling('me', payload).then(response => {
             this.$emit('billing-updated', response.data)
+          }).finally(() => {
+            this.is_submitting = false
           })
         }
       },
