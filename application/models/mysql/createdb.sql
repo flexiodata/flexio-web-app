84f67cb706ec6f2e482190c9bbc93f1767cacd72
Aaron Williams <aaron@flex.io>
2020-01-22 14:05:11 -0600

    added additional index on the user table

--- application/models/mysql/createdb.sql
+++ application/models/mysql/createdb.sql
@@ -99,6 +99,8 @@ CREATE INDEX idx_user_owned_by ON tbl_user (owned_by);
 CREATE INDEX idx_user_created_by ON tbl_user (created_by);
 CREATE INDEX idx_user_created ON tbl_user (created);
 CREATE INDEX idx_user_trial_end_date ON tbl_user (trial_end_date);
+CREATE INDEX idx_user_stripe_customer_id ON tbl_user (stripe_customer_id);
+CREATE INDEX idx_user_stripe_subscription_id ON tbl_user (stripe_subscription_id);
 
 
 
---
+++ scripts/update/update1144_adduserstripeinfoindex.php
@@ -0,0 +1,68 @@
+<?php
+/*!
+ *
+ * Copyright (c) 2020, Gold Prairie LLC. All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2020-01-22
+ *
+ * @package flexio
+ * @subpackage Database_Update
+ */
+
+
+include_once __DIR__.'/../stub.php';
+
+
+if ($argc != 5)
+{
+    echo '{ "success": false, "msg": "Usage: php update*.php <host> <username> <password> <database_name>" }';
+    exit(0);
+}
+
+
+$params = array('host' => $argv[1],
+                'port' => 5432,
+                'username' => $argv[2],
+                'password' => $argv[3],
+                'dbname' => $argv[4]);
+
+try
+{
+    $db = \Flexio\Base\Db::factory('PDO_POSTGRES', $params);
+    $conn = $db->getConnection();
+}
+catch (\Exception $e)
+{
+    echo($e->getMessage());
+    $db = null;
+}
+
+if (is_null($db))
+{
+    echo '{ "success": false, "msg": "Could not connect to database." }';
+    exit(0);
+}
+
+
+try
+{
+    // STEP 1: add indexes for stripe info on the user table
+    $sql = "CREATE INDEX idx_user_stripe_customer_id ON tbl_user (stripe_customer_id);";
+    $db->exec($sql);
+    $sql = "CREATE INDEX idx_user_stripe_subscription_id ON tbl_user (stripe_subscription_id);";
+    $db->exec($sql);
+}
+catch(\Exception $e)
+{
+    echo '{ "success": false, "msg":' . json_encode($e->getMessage()) . '}';
+    exit(0);
+}
+
+
+// update the version number
+$current_version = \Flexio\System\System::getUpdateVersionFromFilename(__FILE__);
+\Flexio\System\System::getModel()->setDbVersionNumber($current_version);
+
+echo '{ "success": true, "msg": "Operation completed successfully." }';
