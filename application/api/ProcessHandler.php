383d816efff6ce7130acefbc37982a66c8799375
Aaron Williams <aaron@flex.io>
2020-02-10 14:57:47 -0600

    added ProcessHandler.php for consolidating process callbacks

---
+++ application/api/ProcessHandler.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * Copyright (c) 2020, Gold Prairie LLC. All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2020-02-10
+ *
+ * @package flexio
+ * @subpackage Api
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Api;
+
+
+class ProcessHandler
+{
+    // TODO: add callback handlers to configure processes
+}
