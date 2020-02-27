d59a9efeb627abbb6dd9252ee7cbf7e1ea7a826f
Aaron Williams <aaron@flex.io>
2020-02-27 14:03:40 -0600

    added scaffolding for ConnectionMount class in prep for moving mount functionality out of connection object implementation

---
+++ application/api/ConnectionMount.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * Copyright (c) 2020, Gold Prairie LLC. All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2020-02-27
+ *
+ * @package flexio
+ * @subpackage Api
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Api;
+
+
+class ConnectionMount
+{
+}
+
