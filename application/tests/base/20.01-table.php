43cc5cd0ee0f6922a1139a1b01d55e653f6b210d
Aaron Williams <aaron@flex.io>
2020-03-04 20:52:10 -0600

    added initial tests for \Flexio\Base\Table

---
+++ application/tests/base/20.01-table.php
@@ -0,0 +1,126 @@
+<?php
+/**
+ *
+ * Copyright (c) 2020, Gold Prairie LLC. All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2020-03-04
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // TEST: table create
+
+        // BEGIN TEST
+        $actual = '';
+        try
+        {
+            \Flexio\Base\Table::create();
+            $actual = \Flexio\Tests\Base::ERROR_NO_EXCEPTION;
+        }
+        catch (\Error $e)
+        {
+            $actual = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        }
+        $expected = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        \Flexio\Tests\Check::assertString('A.1', '\Flexio\Base\Table::create(); throw an exception if two-dimensional array of scalars isn\'t passed', $actual, $expected, $results);
+
+        // BEGIN TEST
+        $actual = '';
+        try
+        {
+            \Flexio\Base\Table::create([]);
+            $actual = \Flexio\Tests\Base::ERROR_NO_EXCEPTION;
+        }
+        catch (\Flexio\Base\Exception $e)
+        {
+            $actual = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        }
+        $expected = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        \Flexio\Tests\Check::assertString('A.2', '\Flexio\Base\Table::create(); throw an exception if two-dimensional array of scalars isn\'t passed', $actual, $expected, $results);
+
+        // BEGIN TEST
+        $actual = '';
+        try
+        {
+            \Flexio\Base\Table::create(["a","b"]);
+            $actual = \Flexio\Tests\Base::ERROR_NO_EXCEPTION;
+        }
+        catch (\Flexio\Base\Exception $e)
+        {
+            $actual = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        }
+        $expected = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        \Flexio\Tests\Check::assertString('A.3', '\Flexio\Base\Table::create(); throw an exception if two-dimensional array of scalars isn\'t passed', $actual, $expected, $results);
+
+        // BEGIN TEST
+        $actual = '';
+        try
+        {
+            \Flexio\Base\Table::create([[]]);
+            $actual = \Flexio\Tests\Base::ERROR_NO_EXCEPTION;
+        }
+        catch (\Flexio\Base\Exception $e)
+        {
+            $actual = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        }
+        $expected = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        \Flexio\Tests\Check::assertString('A.4', '\Flexio\Base\Table::create(); throw an exception if two-dimensional array of scalars isn\'t passed', $actual, $expected, $results);
+
+        // BEGIN TEST
+        $actual = '';
+        try
+        {
+            \Flexio\Base\Table::create([[1,2],[3]]);
+            $actual = \Flexio\Tests\Base::ERROR_NO_EXCEPTION;
+        }
+        catch (\Flexio\Base\Exception $e)
+        {
+            $actual = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        }
+        $expected = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        \Flexio\Tests\Check::assertString('A.5', '\Flexio\Base\Table::create(); if two-dimensional array has different number of columsn, throw an exception',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $actual = '';
+        try
+        {
+            \Flexio\Base\Table::create([[1],[2,3]]);
+            $actual = \Flexio\Tests\Base::ERROR_NO_EXCEPTION;
+        }
+        catch (\Flexio\Base\Exception $e)
+        {
+            $actual = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        }
+        $expected = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        \Flexio\Tests\Check::assertString('A.6', '\Flexio\Base\Table::create(); if two-dimensional array has different number of columsn, throw an exception',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $actual = '';
+        try
+        {
+            \Flexio\Base\Table::create([[1,2],[3],[4,5]]);
+            $actual = \Flexio\Tests\Base::ERROR_NO_EXCEPTION;
+        }
+        catch (\Flexio\Base\Exception $e)
+        {
+            $actual = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        }
+        $expected = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        \Flexio\Tests\Check::assertString('A.7', '\Flexio\Base\Table::create(); if two-dimensional array has different number of columsn, throw an exception',  $actual, $expected, $results);
+
+
+
+    }
+}
---
+++ application/tests/base/20.02-table.php
@@ -0,0 +1,241 @@
+<?php
+/**
+ *
+ * Copyright (c) 2020, Gold Prairie LLC. All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2020-03-04
+ *
+ * @package flexio
+ * @subpackage Tests
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Tests;
+
+
+class Test
+{
+    public function run(&$results)
+    {
+        // SETUP
+        $data_single_col_single_row = [
+            ["col1"]
+        ];
+
+        $data_single_col = [
+            ["col1"],
+            ["val1"],
+            ["val4"],
+            ["val7"]
+        ];
+
+        $data_single_row = [
+            ["col1","col2","col3"]
+        ];
+
+        $data_basic = [
+            ["col1","col2","col3"],
+            ["val1","val2","val3"],
+            ["val4","val5","val6"],
+            ["val7","val8","val9"],
+        ];
+
+
+        // TEST: column count
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_single_col_single_row);
+        $actual = $table->getColCount();
+        $expected = 1;
+        \Flexio\Tests\Check::assertNumber('A.1', '\Flexio\Base\Table::getColCount()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_single_col);
+        $actual = $table->getColCount();
+        $expected = 1;
+        \Flexio\Tests\Check::assertNumber('A.2', '\Flexio\Base\Table::getColCount()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_single_row);
+        $actual = $table->getColCount();
+        $expected = 3;
+        \Flexio\Tests\Check::assertNumber('A.3', '\Flexio\Base\Table::getColCount()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getColCount();
+        $expected = 3;
+        \Flexio\Tests\Check::assertNumber('A.4', '\Flexio\Base\Table::getColCount()',  $actual, $expected, $results);
+
+
+        // TEST: row count
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_single_col_single_row);
+        $actual = $table->getRowCount();
+        $expected = 1;
+        \Flexio\Tests\Check::assertNumber('A.1', '\Flexio\Base\Table::getRowCount()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_single_col);
+        $actual = $table->getRowCount();
+        $expected = 4;
+        \Flexio\Tests\Check::assertNumber('A.2', '\Flexio\Base\Table::getRowCount()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_single_row);
+        $actual = $table->getRowCount();
+        $expected = 1;
+        \Flexio\Tests\Check::assertNumber('A.3', '\Flexio\Base\Table::getRowCount()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRowCount();
+        $expected = 4;
+        \Flexio\Tests\Check::assertNumber('A.4', '\Flexio\Base\Table::getRowCount()',  $actual, $expected, $results);
+
+
+        // TEST: table get row
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRow(0);
+        $expected = ["col1","col2","col3"];
+        \Flexio\Tests\Check::assertNumber('B.1', '\Flexio\Base\Table::getRow()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRow(1);
+        $expected = ["val1","val2","val3"];
+        \Flexio\Tests\Check::assertNumber('B.2', '\Flexio\Base\Table::getRow()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRow(2);
+        $expected = ["val4","val5","val6"];
+        \Flexio\Tests\Check::assertNumber('B.3', '\Flexio\Base\Table::getRow()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRow(3);
+        $expected = ["val7","val8","val9"];
+        \Flexio\Tests\Check::assertNumber('B.4', '\Flexio\Base\Table::getRow()',  $actual, $expected, $results);
+
+
+        // TEST: table get column
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getCol(0);
+        $expected = ["col1","val1","val4","val7"];
+        \Flexio\Tests\Check::assertNumber('C.1', '\Flexio\Base\Table::getCol()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getCol(1);
+        $expected = ["col2","val2","val5","val8"];
+        \Flexio\Tests\Check::assertNumber('C.2', '\Flexio\Base\Table::getCol()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getCol(2);
+        $expected = ["col3","val3","val6","val9"];
+        \Flexio\Tests\Check::assertNumber('C.3', '\Flexio\Base\Table::getCol()',  $actual, $expected, $results);
+
+
+        // TEST: table range
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRange();
+        $expected = [
+            ["col1","col2","col3"],
+            ["val1","val2","val3"],
+            ["val4","val5","val6"],
+            ["val7","val8","val9"],
+        ];
+        \Flexio\Tests\Check::assertArray('D.1', '\Flexio\Base\Table::range(); basic test',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRange(-1,-1,100,100);
+        $expected = [
+            ["col1","col2","col3"],
+            ["val1","val2","val3"],
+            ["val4","val5","val6"],
+            ["val7","val8","val9"],
+        ];
+        \Flexio\Tests\Check::assertArray('D.2', '\Flexio\Base\Table::range(); basic test',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRange(0,0,2,3);
+        $expected = [
+            ["col1","col2","col3"],
+            ["val1","val2","val3"],
+            ["val4","val5","val6"],
+            ["val7","val8","val9"],
+        ];
+        \Flexio\Tests\Check::assertArray('D.3', '\Flexio\Base\Table::range(); basic test',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRange(2,3,0,0);
+        $expected = [
+            ["col1","col2","col3"],
+            ["val1","val2","val3"],
+            ["val4","val5","val6"],
+            ["val7","val8","val9"],
+        ];
+        \Flexio\Tests\Check::assertArray('D.4', '\Flexio\Base\Table::range(); basic test',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRange(0,0,0,0);
+        $expected = [
+            ["col1"]
+        ];
+        \Flexio\Tests\Check::assertArray('D.5', '\Flexio\Base\Table::range(); basic test',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRange(null,null,1,1);
+        $expected = [
+            ["col1","col2"],
+            ["val1","val2"],
+        ];
+        \Flexio\Tests\Check::assertArray('D.6', '\Flexio\Base\Table::range(); basic test',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRange(1,1,null,null);
+        $expected = [
+            ["val2","val3"],
+            ["val5","val6"],
+            ["val8","val9"],
+        ];
+        \Flexio\Tests\Check::assertArray('D.7', '\Flexio\Base\Table::range(); basic test',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRange(0,null,0,null);
+        $expected = [
+            ["col1"],
+            ["val1"],
+            ["val4"],
+            ["val7"],
+        ];
+        \Flexio\Tests\Check::assertArray('D.8', '\Flexio\Base\Table::range(); basic test',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $table = \Flexio\Base\Table::create($data_basic);
+        $actual = $table->getRange(null,0,null,0);
+        $expected = [
+            ["col1","col2","col3"],
+        ];
+        \Flexio\Tests\Check::assertArray('D.9', '\Flexio\Base\Table::range(); basic test',  $actual, $expected, $results);
+    }
+}
