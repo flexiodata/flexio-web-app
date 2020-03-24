d20a1a18cac5dcfb99b805c07b647b8cc3b2dda6
Aaron Williams <aaron@flex.io>
2020-03-24 13:09:19 -0500

    function organization

--- application/jobs/Base.php
+++ application/jobs/Base.php
@@ -79,6 +79,144 @@ class Base implements \Flexio\IFace\IJob
         return $this;
     }
 
+
+    public static function addEids(array $task) : array
+    {
+        // adds an eid to a task; note: eids are no longer added to
+        // tasks, but keep the function here along with the associated
+        // tests in case it's needed in the future
+
+        if (isset($task['op']))
+        {
+            if (!isset($task['eid']))
+                $task['eid'] = \Flexio\Base\Eid::generate();
+        }
+
+        foreach ($task as $key => &$value)
+        {
+            if (!is_array($value))
+                continue;
+
+            $value = self::addEids($value);
+        }
+
+        return $task;
+    }
+
+    public static function fixEmptyParams(array $task) : array
+    {
+        // TODO: temporary fix for making sure empty params are stored as
+        // objects; this happens because we're decoding JSON into an associative
+        // array in the ApiController; should look for a more comprehensive solution
+        // since this can affect other parameters in other API payloads
+
+        if (isset($task['params']) && is_array($task['params']) && count($task['params']) == 0)
+            $task['params'] = (object)array();
+
+        foreach ($task as $key => &$value)
+        {
+            if (!is_array($value))
+                continue;
+
+            $value = self::fixEmptyParams($value);
+        }
+
+        return $task;
+    }
+
+    public static function flattenParams(array $task) : array
+    {
+        // helper function for iterating through a task and moving all key
+        // values from the params node to the top-level; used currently to
+        // convert tasks coming in through the old format into the new format
+        // until the tasks are migrated to the new format in the SDK, etc
+
+        if (isset($task['params']))
+        {
+            if (is_array($task['params']))
+            {
+                foreach ($task['params'] as $key => $value)
+                {
+                    $task[$key] = $value;
+                }
+            }
+        }
+
+        if (is_array($task))
+        {
+            foreach ($task as $key => &$value)
+            {
+                if (!is_array($value))
+                    continue;
+
+                $value = self::flattenParams($value);
+            }
+        }
+
+        return $task;
+    }
+
+    private static function replaceParameterTokensRecurse(&$info, $process, &$value) : void
+    {
+        if (is_array($value))
+        {
+            // don't replace subsequences
+            if (isset($value['op']) && $value['op'] == 'sequence')
+                return;
+
+            foreach ($value as $k => &$v)
+            {
+                self::replaceParameterTokensRecurse($info, $process, $v);
+            }
+        }
+         else
+        {
+            if (is_string($value))
+            {
+                $re = '/\$\{.*?}/';
+
+                preg_match_all($re, $value, $matches, PREG_OFFSET_CAPTURE, 0);
+
+                if (isset($matches[0]))
+                {
+                    $differential = 0; // keep track of the offsets when we replace due to the difference of the token lengths vs value length
+
+                    foreach ($matches[0] as $match)
+                    {
+                        $token = $match[0];
+                        $token_len = strlen($token);
+                        $offset = $match[1];
+
+                        $varname = substr($token, 2, -1);  // turn '${myvar}' into 'myvar'
+
+                        $stream = self::getParameterStream($process, $varname);
+                        $replacement = '';
+
+                        if ($stream !== null)
+                        {
+                            $streamreader = $stream->getReader();
+                            while (($chunk = $streamreader->read()) !== false)
+                                $replacement .= $chunk;
+                        }
+
+                        // use true/false text for boolean value replacements in a string
+                        if ($replacement === true)
+                            $replacement = 'true';
+                        if ($replacement === false)
+                            $replacement = 'false';
+
+                        // TODO: need to handle replacements of non-string variable types
+                        if (!is_string($replacement))
+                            continue;
+
+                        $value = substr_replace($value, $replacement, $offset + $differential, $token_len);
+                        $differential += (strlen($replacement) - $token_len);
+                    }
+                }
+            }
+        }
+    }
+
     private static function getParameterStream($process, string $varname, array $info = null) : ?\Flexio\Base\Stream
     {
         if ($info === null)
@@ -204,143 +342,6 @@ class Base implements \Flexio\IFace\IJob
 
 
         return self::ensureStream($data);
-    }
-
-    private static function replaceParameterTokensRecurse(&$info, $process, &$value) : void
-    {
-        if (is_array($value))
-        {
-            // don't replace subsequences
-            if (isset($value['op']) && $value['op'] == 'sequence')
-                return;
-
-            foreach ($value as $k => &$v)
-            {
-                self::replaceParameterTokensRecurse($info, $process, $v);
-            }
-        }
-         else
-        {
-            if (is_string($value))
-            {
-                $re = '/\$\{.*?}/';
-
-                preg_match_all($re, $value, $matches, PREG_OFFSET_CAPTURE, 0);
-
-                if (isset($matches[0]))
-                {
-                    $differential = 0; // keep track of the offsets when we replace due to the difference of the token lengths vs value length
-
-                    foreach ($matches[0] as $match)
-                    {
-                        $token = $match[0];
-                        $token_len = strlen($token);
-                        $offset = $match[1];
-
-                        $varname = substr($token, 2, -1);  // turn '${myvar}' into 'myvar'
-
-                        $stream = self::getParameterStream($process, $varname);
-                        $replacement = '';
-
-                        if ($stream !== null)
-                        {
-                            $streamreader = $stream->getReader();
-                            while (($chunk = $streamreader->read()) !== false)
-                                $replacement .= $chunk;
-                        }
-
-                        // use true/false text for boolean value replacements in a string
-                        if ($replacement === true)
-                            $replacement = 'true';
-                        if ($replacement === false)
-                            $replacement = 'false';
-
-                        // TODO: need to handle replacements of non-string variable types
-                        if (!is_string($replacement))
-                            continue;
-
-                        $value = substr_replace($value, $replacement, $offset + $differential, $token_len);
-                        $differential += (strlen($replacement) - $token_len);
-                    }
-                }
-            }
-        }
-    }
-
-    public static function addEids(array $task) : array
-    {
-        // adds an eid to a task; note: eids are no longer added to
-        // tasks, but keep the function here along with the associated
-        // tests in case it's needed in the future
-
-        if (isset($task['op']))
-        {
-            if (!isset($task['eid']))
-                $task['eid'] = \Flexio\Base\Eid::generate();
-        }
-
-        foreach ($task as $key => &$value)
-        {
-            if (!is_array($value))
-                continue;
-
-            $value = self::addEids($value);
-        }
-
-        return $task;
-    }
-
-    public static function fixEmptyParams(array $task) : array
-    {
-        // TODO: temporary fix for making sure empty params are stored as
-        // objects; this happens because we're decoding JSON into an associative
-        // array in the ApiController; should look for a more comprehensive solution
-        // since this can affect other parameters in other API payloads
-
-        if (isset($task['params']) && is_array($task['params']) && count($task['params']) == 0)
-            $task['params'] = (object)array();
-
-        foreach ($task as $key => &$value)
-        {
-            if (!is_array($value))
-                continue;
-
-            $value = self::fixEmptyParams($value);
-        }
-
-        return $task;
-    }
-
-    public static function flattenParams(array $task) : array
-    {
-        // helper function for iterating through a task and moving all key
-        // values from the params node to the top-level; used currently to
-        // convert tasks coming in through the old format into the new format
-        // until the tasks are migrated to the new format in the SDK, etc
-
-        if (isset($task['params']))
-        {
-            if (is_array($task['params']))
-            {
-                foreach ($task['params'] as $key => $value)
-                {
-                    $task[$key] = $value;
-                }
-            }
-        }
-
-        if (is_array($task))
-        {
-            foreach ($task as $key => &$value)
-            {
-                if (!is_array($value))
-                    continue;
-
-                $value = self::flattenParams($value);
-            }
-        }
-
-        return $task;
     }
 
     private static function ensureStream($stream, string $content_type = null) : \Flexio\Base\Stream
