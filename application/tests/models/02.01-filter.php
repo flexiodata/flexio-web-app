06d5a49a5335732b5173699831143a389a649cc3
Aaron Williams <aaron@flex.io>
2020-03-27 13:25:59 -0500

    added additional tests

--- application/tests/models/02.01-filter.php
+++ application/tests/models/02.01-filter.php
@@ -43,18 +43,50 @@ class Test
         \Flexio\Tests\Check::assertString('A.1', '\Flexio\Model\Filter::build(); throw an exception if filter parameters are included that aren\'t allowed',  $actual, $expected, $results);
 
         // BEGIN TEST
+        $actual = '';
+        try
+        {
+            $allowed_items = array('owned_by' => 'A');
+            $filter = array('eid_status' => 'A');
+            $file_expr = \Filter::build($db, $filter, $allowed_items);
+            $actual = \Flexio\Tests\Base::ERROR_NO_EXCEPTION;
+        }
+        catch (\Flexio\Base\Exception $e)
+        {
+            $actual = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        }
+        $expected = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        \Flexio\Tests\Check::assertString('A.2', '\Flexio\Model\Filter::build(); throw an exception if filter parameters are included that aren\'t allowed',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $actual = '';
+        try
+        {
+            $allowed_items = array('owned_by' => 'A');
+            $filter = array('OWNED_BY' => 'A');
+            $file_expr = \Filter::build($db, $filter, $allowed_items);
+            $actual = \Flexio\Tests\Base::ERROR_NO_EXCEPTION;
+        }
+        catch (\Flexio\Base\Exception $e)
+        {
+            $actual = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        }
+        $expected = \Flexio\Tests\Base::ERROR_EXCEPTION;
+        \Flexio\Tests\Check::assertString('A.3', '\Flexio\Model\Filter::build(); throw an exception if filter parameters are included that aren\'t allowed',  $actual, $expected, $results);
+
+        // BEGIN TEST
         $allowed_items = array();
         $filter = array();
         $actual = \Filter::build($db, $filter, $allowed_items);
         $expected = '(true)';
-        \Flexio\Tests\Check::assertString('A.2', '\Flexio\Model\Filter::build(); don\'t require filter/allowed parameters',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('A.4', '\Flexio\Model\Filter::build(); don\'t require filter/allowed parameters',  $actual, $expected, $results);
 
         // BEGIN TEST
         $allowed_items = array('owned_by');
         $filter = array();
         $actual = \Filter::build($db, $filter, $allowed_items);
         $expected = "(true)";
-        \Flexio\Tests\Check::assertString('A.3', '\Flexio\Model\Filter::build(); don\'t require allowed parameters to have associated filter items',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('A.5', '\Flexio\Model\Filter::build(); don\'t require allowed parameters to have associated filter items',  $actual, $expected, $results);
 
 
         // TEST: build function
