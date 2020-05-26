afe7554c84bf7d0d62d15f3e1057581a0e083959
Aaron Williams <aaron@flex.io>
2020-05-26 16:04:54 -0500

    created StreamNative scaffolding; implementation will wrap native stream

---
+++ application/base/StreamNative.php
@@ -0,0 +1,22 @@
+<?php
+/**
+ *
+ * Copyright (c) 2020, Flex Research LLC. All rights reserved.
+ *
+ * Project:  Flex.io App
+ * Author:   Aaron L. Williams
+ * Created:  2020-05-26
+ *
+ * @package flexio
+ * @subpackage Base
+ */
+
+
+declare(strict_types=1);
+namespace Flexio\Base;
+
+
+class StreamNative implements \Flexio\IFace\IStream
+{
+}
+
