0c3f79b3c7a7008e7e85701fb5221708e3e0e572
Aaron Williams <aaron@flex.io>
2020-06-01 17:20:08 -0500

    updated stream tests

--- application/tests/base/17.02-stream.php
+++ application/tests/base/17.02-stream.php
@@ -20,28 +20,23 @@ class Test
 {
     public function run(&$results)
     {
-        // TEST: stream read/writer; basic file read/write
+        // TEST: stream read()
 
         // BEGIN TEST
         $stream_info = array();
         $stream_info['path'] = \Flexio\Base\Util::generateHandle();
         $stream_info['mime_type'] = \Flexio\Base\ContentType::TEXT;
-        $stream_info['structure'] = array();
         $stream = \Flexio\Base\Stream::create($stream_info);
-        $writer = $stream->getWriter();
-        $writer->write("");
-        $writer->close();
         $reader = $stream->getReader();
         $actual = $reader->read();
         $reader->close();
         $expected = "";
-        \Flexio\Tests\Check::assertString('A.1', 'StreamReader/StreamWriter; check basic file read/write ',  $actual, $expected, $results, \Flexio\Tests\Base::FLAG_ERROR_SUPPRESS);
+        \Flexio\Tests\Check::assertString('A.1', 'StreamReader/StreamWriter; check read()',  $actual, $expected, $results, \Flexio\Tests\Base::FLAG_ERROR_SUPPRESS);
 
         // BEGIN TEST
         $stream_info = array();
         $stream_info['path'] = \Flexio\Base\Util::generateHandle();
         $stream_info['mime_type'] = \Flexio\Base\ContentType::TEXT;
-        $stream_info['structure'] = array();
         $stream = \Flexio\Base\Stream::create($stream_info);
         $writer = $stream->getWriter();
         $writer->write("A");
@@ -50,13 +45,15 @@ class Test
         $actual = $reader->read();
         $reader->close();
         $expected = "A";
-        \Flexio\Tests\Check::assertString('A.2', 'StreamReader/StreamWriter; check basic file read/write ',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('A.2', 'StreamReader/StreamWriter; check read()',  $actual, $expected, $results);
+
+
+        // TEST: stream read/writer; multiple
 
         // BEGIN TEST
         $stream_info = array();
         $stream_info['path'] = \Flexio\Base\Util::generateHandle();
         $stream_info['mime_type'] = \Flexio\Base\ContentType::TEXT;
-        $stream_info['structure'] = array();
         $stream = \Flexio\Base\Stream::create($stream_info);
         $writer = $stream->getWriter();
         $writer->write("abcdefg");
@@ -65,23 +62,90 @@ class Test
         $actual = $reader->read();
         $reader->close();
         $expected = "abcdefg";
-        \Flexio\Tests\Check::assertString('A.3', 'StreamReader/StreamWriter; check basic file read/write ',  $actual, $expected, $results);
-
-
-
-        // TEST: basic content query
+        \Flexio\Tests\Check::assertString('B.1', 'StreamReader/StreamWriter; check read()',  $actual, $expected, $results);
 
         // BEGIN TEST
         $stream_info = array();
         $stream_info['path'] = \Flexio\Base\Util::generateHandle();
         $stream_info['mime_type'] = \Flexio\Base\ContentType::TEXT;
-        $stream_info['structure'] = array();
         $stream = \Flexio\Base\Stream::create($stream_info);
         $writer = $stream->getWriter();
         $writer->write("abcdefg");
         $writer->close();
-        $actual = \Flexio\Tests\Util::getStreamContents($stream, 0, PHP_INT_MAX, 1);
+        $reader = $stream->getReader();
+        $actual = $reader->read(0);
+        $reader->close();
+        $expected = "";
+        \Flexio\Tests\Check::assertString('B.2', 'StreamReader/StreamWriter; check read()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $stream_info = array();
+        $stream_info['path'] = \Flexio\Base\Util::generateHandle();
+        $stream_info['mime_type'] = \Flexio\Base\ContentType::TEXT;
+        $stream = \Flexio\Base\Stream::create($stream_info);
+        $writer = $stream->getWriter();
+        $writer->write("abcdefg");
+        $writer->close();
+        $reader = $stream->getReader();
+        $actual = $reader->read(1);
+        $reader->close();
+        $expected = "a";
+        \Flexio\Tests\Check::assertString('B.3', 'StreamReader/StreamWriter; check read()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $stream_info = array();
+        $stream_info['path'] = \Flexio\Base\Util::generateHandle();
+        $stream_info['mime_type'] = \Flexio\Base\ContentType::TEXT;
+        $stream = \Flexio\Base\Stream::create($stream_info);
+        $writer = $stream->getWriter();
+        $writer->write("abcdefg");
+        $writer->close();
+        $reader = $stream->getReader();
+        $actual = $reader->read(2);
+        $reader->close();
+        $expected = "ab";
+        \Flexio\Tests\Check::assertString('B.4', 'StreamReader/StreamWriter; check read()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $stream_info = array();
+        $stream_info['path'] = \Flexio\Base\Util::generateHandle();
+        $stream_info['mime_type'] = \Flexio\Base\ContentType::TEXT;
+        $stream = \Flexio\Base\Stream::create($stream_info);
+        $writer = $stream->getWriter();
+        $writer->write("abcdefg");
+        $writer->close();
+        $reader = $stream->getReader();
+        $actual = $reader->read(6);
+        $reader->close();
+        $expected = "abcdef";
+        \Flexio\Tests\Check::assertString('B.5', 'StreamReader/StreamWriter; check read()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $stream_info = array();
+        $stream_info['path'] = \Flexio\Base\Util::generateHandle();
+        $stream_info['mime_type'] = \Flexio\Base\ContentType::TEXT;
+        $stream = \Flexio\Base\Stream::create($stream_info);
+        $writer = $stream->getWriter();
+        $writer->write("abcdefg");
+        $writer->close();
+        $reader = $stream->getReader();
+        $actual = $reader->read(7);
+        $reader->close();
         $expected = "abcdefg";
-        \Flexio\Tests\Check::assertString('B.1', 'Stream; check basic content query',  $actual, $expected, $results);
+        \Flexio\Tests\Check::assertString('B.6', 'StreamReader/StreamWriter; check read()',  $actual, $expected, $results);
+
+        // BEGIN TEST
+        $stream_info = array();
+        $stream_info['path'] = \Flexio\Base\Util::generateHandle();
+        $stream_info['mime_type'] = \Flexio\Base\ContentType::TEXT;
+        $stream = \Flexio\Base\Stream::create($stream_info);
+        $writer = $stream->getWriter();
+        $writer->write("abcdefg");
+        $writer->close();
+        $reader = $stream->getReader();
+        $actual = $reader->read(8);
+        $reader->close();
+        $expected = "abcdefg";
+        \Flexio\Tests\Check::assertString('B.7', 'StreamReader/StreamWriter; check read()',  $actual, $expected, $results);
     }
 }
--- application/tests/base/17.03-stream.php
+++ application/tests/base/17.03-stream.php
@@ -20,5 +20,31 @@ class Test
 {
     public function run(&$results)
     {
+        // TEST: stream readRow()
+
+        // BEGIN TEST
+        $stream_info = array();
+        $stream_info['path'] = \Flexio\Base\Util::generateHandle();
+        $stream_info['mime_type'] = \Flexio\Base\ContentType::TEXT;
+        $stream = \Flexio\Base\Stream::create($stream_info);
+        $reader = $stream->getReader();
+        $actual = $reader->readRow();
+        $reader->close();
+        $expected = "";
+        \Flexio\Tests\Check::assertString('A.1', 'StreamReader/StreamWriter; check readRow()',  $actual, $expected, $results, \Flexio\Tests\Base::FLAG_ERROR_SUPPRESS);
+
+        // BEGIN TEST
+        $stream_info = array();
+        $stream_info['path'] = \Flexio\Base\Util::generateHandle();
+        $stream_info['mime_type'] = \Flexio\Base\ContentType::TEXT;
+        $stream = \Flexio\Base\Stream::create($stream_info);
+        $writer = $stream->getWriter();
+        $writer->write("A");
+        $writer->close();
+        $reader = $stream->getReader();
+        $actual = $reader->readRow();
+        $reader->close();
+        $expected = "A";
+        \Flexio\Tests\Check::assertString('A.2', 'StreamReader/StreamWriter; check readRow()',  $actual, $expected, $results);
     }
 }
