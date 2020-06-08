40ee41035c1818fde2a77c31256961f0136463e4
David Z. Williams <dave@flex.io>
2020-06-08 14:29:54 -0400

    Last step of the onboarding is basically done.

--- public/src/components/AppOnboarding.vue
+++ public/src/components/AppOnboarding.vue
@@ -9,7 +9,7 @@
           <img src="../assets/logo-square-80x80.png" alt="Flex.io" class="br-100 ba bw1 b--white" style="width: 84px; box-shadow: 0 0 3px rgba(0,0,0,0.4)">
         </div>
 
-        <h1 class="fw6 f2 tc pb2">{{title}}</h1>
+        <h1 class="fw6 f2 tc pb2">{{display_title}}</h1>
 
         <!-- step heading -->
         <el-steps
@@ -109,6 +109,49 @@
           <p class="center mw7 f8 silver">* The Microsoft Excel 365 add-in will only function with an Excel for Office 365 subscription</p>
         </div>
 
+        <div v-if="active_step == 'complete'">
+          <p class="center mw7">You're all set and ready to go. Here's a set of resources to help get you started. If you need anything, please let us know!</p>
+          <div class="flex-l flex-row-l flex-wrap-l nl3 nr3">
+            <a
+              class="flex-fill flex flex-row db ma3 pv3 ph4 br2 fw4 dark-gray no-underline w-third-l last-step-item"
+              target="_blank"
+              :href="item.link"
+              :key="item.title"
+              v-for="item in last_step_items"
+            >
+              <div>
+                <h3 class="mt2 mb0 fw6">{{item.title}}</h3>
+                <p class="flex-fill f6 lh-copy mv3">{{item.description}}</p>
+                <div class="mb3">
+                  <span class="flex flex-row items-center blue fw6">
+                    <span>View</span>
+                    <i class="material-icons ml1">arrow_right_alt</i>
+                  </span>
+                </div>
+              </div>
+            </a>
+          </div>
+          <h3 class="mt5 mb4 tc">Select an example sheet</h3>
+          <TemplateList
+            :icon="t.icon"
+            :templates="t.templates"
+            v-for="t in all_templates"
+          />
+          <h3 class="mt5 mb4 tc">Get started with the Flex.io App</h3>
+          <p class="center mw7">Go off road. Access just about any data with your own code and share integrations with your team without sharing your credentials.</p>
+          <div class="mv4 tc">
+            <button
+              class="db dib-ns pv3 ph5 tc b ttu blue br2 ba b--blue hover-bg-blue hover-white"
+              @click="onNextStepClick"
+            >
+              Open the Flex App
+            </button>
+          </div>
+          <p class="tc">Want some code examples? Fork one of our open source function packs from GitHub: <a class="ml1 v-mid" href="https://github.com/flexiodata?utf8=%E2%9C%93&q=functions" target="_blank" title="View function packs on Github"><svg xmlns="http://www.w3.org/2000/svg" fill="black" width="32" height="32" viewBox="0 0 16 16"><path d="M8 .198c-4.418 0-8 3.582-8 8 0 3.535 2.292 6.533 5.47 7.59.4.075.548-.173.548-.384 0-.19-.008-.82-.01-1.49-2.227.485-2.696-.943-2.696-.943-.364-.924-.888-1.17-.888-1.17-.726-.497.055-.486.055-.486.802.056 1.225.824 1.225.824.714 1.223 1.872.87 2.328.665.072-.517.28-.87.508-1.07-1.776-.202-3.644-.888-3.644-3.954 0-.874.313-1.588.824-2.148-.083-.202-.357-1.015.077-2.117 0 0 .672-.215 2.2.82.64-.177 1.323-.266 2.003-.27.68.004 1.365.093 2.004.27 1.527-1.035 2.198-.82 2.198-.82.435 1.102.162 1.916.08 2.117.512.56.822 1.274.822 2.147 0 3.072-1.872 3.748-3.653 3.946.288.248.544.735.544 1.48 0 1.07-.01 1.933-.01 2.196 0 .213.145.462.55.384 3.178-1.06 5.467-4.057 5.467-7.59 0-4.418-3.58-8-8-8z"></path></svg></a></p>
+
+          <p class="tc">Need help with a script? <span @click="onNeedHelpClick">Let us know and we'll point you in the right direction!</span></p>
+        </div>
+
         <!-- button bar for the entire onboarding wizard -->
         <ButtonBar
           class="mt5"
@@ -116,7 +159,7 @@
           :utility-button-visible="active_step_idx != step_order.length - 1"
           :utility-button-text="active_step_idx == 0 ? 'Skip setup' : '← Start over'"
           :cancel-button-visible="false"
-          :submit-button-visible="active_step != 'setup'"
+          :submit-button-visible="active_step != 'setup' && active_step != 'complete'"
           :submit-button-text="active_step_idx == step_order.length - 1 ? 'Finish Setup' : 'Continue'"
           @utility-click="onUtilityButtonClick"
           @submit-click="onNextStepClick"
@@ -139,6 +182,7 @@
   import ServiceIconWrapper from '@/components/ServiceIconWrapper'
   import AddonDownloadButtonPanel from '@/components/AddonDownloadButtonPanel'
   import MemberInvitePanel from '@/components/MemberInvitePanel'
+  import TemplateList from '@/components/TemplateList'
 
   const getNameSuffix = (length) => {
     return randomstring.generate({
@@ -152,12 +196,26 @@
     return {
       is_fetching_config: false,
       route_title: '',
+      custom_title: '',
       active_step: 'integrations', // 'integrations', 'setup', 'addons', 'members'
       active_integration_idx: 0,
       active_setup_template: null,
       selected_integrations: [],
       output_mounts: [],
-      email_invites: []
+      email_invites: [],
+      last_step_items: [{
+        title: 'Quick Start',
+        description: 'Get a sheet with a set of quick start examples',
+        link: 'https://www.flex.io/resources/getting-started/template'
+      },{
+        title: 'Getting Started Guide',
+        description: 'Learn how to work with Flex.io in your spreadsheet',
+        link: 'https://www.flex.io/resources/getting-started'
+      },{
+        title: 'Explore Integrations',
+        description: 'See how you can use Flex with other web services',
+        link: 'https://www.flex.io/explore'
+      }]
     }
   }
 
@@ -178,6 +236,7 @@
       ServiceIconWrapper,
       AddonDownloadButtonPanel,
       MemberInvitePanel,
+      TemplateList
     },
     data() {
       return getDefaultState()
@@ -227,6 +286,21 @@
         } else {
           return 'Integration Setup'
         }
+      },
+      display_title() {
+        if (this.custom_title.length > 0) {
+          return this.custom_title
+        } else {
+          return this.title
+        }
+      },
+      all_templates() {
+        return _.map(this.output_mounts, m => {
+          return {
+            icon:  _.get(m, 'icon', ''),
+            templates: _.get(m, 'setup_template.templates', [])
+          }
+        })
       }
     },
     mounted() {
@@ -324,6 +398,10 @@
           if (this.active_step == 'setup') {
             this.fetchIntegrationConfig()
           }
+
+          if (this.active_step == 'complete') {
+            this.custom_title = 'All systems go!'
+          }
         }
       },
       onMemberInviteSubmit() {
@@ -333,6 +411,11 @@
           message: "Your co-workers have been sent an invitation to join your team.",
           type: 'success'
         })
+      },
+      onNeedHelpClick() {
+        if (window.Intercom) {
+          window.Intercom('showNewMessage')
+        }
       },
       fetchIntegrationConfig() {
         this.is_fetching_config = true
@@ -449,3 +532,12 @@
     }
   }
 </script>
+
+<style lang="stylus">
+  .last-step-item
+    background-color: rgba(255,255,255,0.2)
+    box-shadow: 0 8px 24px -1px rgba(0,0,0,0.075)
+    transition: all 0.2s ease-out
+    &:hover
+      box-shadow: 0 8px 24px 0 rgba(0,0,0,0.2)
+</style>
