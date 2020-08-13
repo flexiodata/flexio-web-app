76b0c0062304fa799454ff76219ba52ab890a1b2
Aaron Williams <aaron@flex.io>
2020-08-13 16:37:24 -0500

    updated demo pipes to use linked getting started implementation

--- scripts/demo/flex-sample-contacts.py
+++ scripts/demo/flex-sample-contacts.py
@@ -2,9 +2,11 @@
 # ---
 # name: flex-sample-contacts
 # deployed: true
-# config: index
 # title: Flex Sample Contacts
 # description: Returns sample contact information
+# task:
+#   op: redirect
+#   path: https://api.flex.io/v1/integration-getting-started/pipes/flex-sample-contacts/run
 # params:
 #   - name: properties
 #     type: string
@@ -50,93 +52,3 @@
 #   - '"first_name, last_name, email"'
 #   - '"first_name, city, state, zipcode", "CA"'
 # ---
-
-import csv
-import json
-import requests
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-from contextlib import closing
-from collections import OrderedDict
-from datetime import *
-
-def flex_handler(flex):
-
-    # configuration
-    params = {
-        'url': 'https://raw.githubusercontent.com/flexiodata/data/master/sample/sample-contacts.csv'
-    }
-
-    # set the output type to ndjson for loading into an index
-    flex.output.content_type = 'application/x-ndjson'
-
-    # get the data for each line in each file and write it to
-    # stdout with one json object per-line (ndjson) for loading
-    # into an index
-    for row in get_data(params):
-        item = json.dumps(row) + "\n"
-        flex.output.write(item)
-
-def get_data(params):
-
-    # get the data
-    headers = {
-        'User-Agent': 'Flex.io Web App'
-    }
-    request = requests_retry_session().get(params['url'], stream=True, headers=headers)
-    with closing(request) as r:
-        # get each line and return a dictionary item for each line
-        f = (line.decode('utf-8-sig') for line in r.iter_lines())
-        reader = csv.DictReader(f, delimiter=',', quotechar='"')
-        for row in reader:
-            data = get_item(row)
-            yield data
-
-def get_item(row):
-
-    # convert keys to lowercase and make sure the values are formatted
-    row = {k.lower(): v for k, v in row.items()}
-
-    item = OrderedDict()
-    item['id'] = to_number(row.get('id'))
-    item['first_name'] = row.get('first_name')
-    item['last_name'] = row.get('last_name')
-    item['street_address'] = row.get('street_address')
-    item['city'] = row.get('city')
-    item['state'] = row.get('state')
-    item['zipcode'] = row.get('zipcode','').rjust(5,'0') # pad zipcodes
-    item['phone'] = row.get('phone')
-    item['email'] = row.get('email')
-    item['birthday'] = to_date(row.get('birthday'))
-    return item
-
-def requests_retry_session(
-    retries=3,
-    backoff_factor=0.3,
-    status_forcelist=(500, 502, 504),
-    session=None,
-):
-    session = session or requests.Session()
-    retry = Retry(
-        total=retries,
-        read=retries,
-        connect=retries,
-        backoff_factor=backoff_factor,
-        status_forcelist=status_forcelist,
-    )
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    return session
-
-def to_date(value):
-    try:
-        return datetime.strptime(value, '%m/%d/%Y').strftime('%Y-%m-%d')
-    except:
-        return value
-
-def to_number(value):
-    try:
-        return float(value)
-    except:
-        return value
--- scripts/demo/flex-zipcode-stats.py
+++ scripts/demo/flex-zipcode-stats.py
@@ -2,9 +2,11 @@
 # ---
 # name: flex-zipcode-stats
 # deployed: true
-# config: index
 # title: Flex Zipcode Stats
 # description: Returns zipcode statistic information
+# task:
+#   op: redirect
+#   path: https://api.flex.io/v1/integration-getting-started/pipes/flex-zipcode-stats/run
 # params:
 #   - name: properties
 #     type: string
@@ -47,85 +49,3 @@
 #   - '"zipcode, population_2010"'
 #   - '"", "CA"'
 # ---
-
-import csv
-import json
-import requests
-from requests.adapters import HTTPAdapter
-from requests.packages.urllib3.util.retry import Retry
-from contextlib import closing
-from collections import OrderedDict
-
-def flex_handler(flex):
-
-    # configuration
-    params = {
-        'url': 'https://raw.githubusercontent.com/flexiodata/data/master/sample/zipcode-stats.csv'
-    }
-
-    # set the output type to ndjson for loading into an index
-    flex.output.content_type = 'application/x-ndjson'
-
-    # get the data for each line in each file and write it to
-    # stdout with one json object per-line (ndjson) for loading
-    # into an index
-    for row in get_data(params):
-        item = json.dumps(row) + "\n"
-        flex.output.write(item)
-
-def get_data(params):
-
-    # get the data
-    headers = {
-        'User-Agent': 'Flex.io Web App'
-    }
-    request = requests_retry_session().get(params['url'], stream=True, headers=headers)
-    with closing(request) as r:
-        # get each line and return a dictionary item for each line
-        f = (line.decode('utf-8-sig') for line in r.iter_lines())
-        reader = csv.DictReader(f, delimiter=',', quotechar='"')
-        for row in reader:
-            data = get_item(row)
-            yield data
-
-def get_item(row):
-
-    # convert keys to lowercase and make sure the values are formatted
-    row = {k.lower(): v for k, v in row.items()}
-
-    item = OrderedDict()
-    item['zipcode'] = row.get('zipcode')
-    item['city'] = row.get('city')
-    item['state'] = row.get('state')
-    item['state_name'] = row.get('state_name')
-    item['latitude'] = to_number(row.get('latitude'))
-    item['longitude'] = to_number(row.get('longitude'))
-    item['population_2010'] = to_number(row.get('population_2010'))
-    item['land_sq_miles'] = to_number(row.get('land_sq_miles'))
-    item['density_per_sq_mile'] = to_number(row.get('density_per_sq_mile'))
-    return item
-
-def requests_retry_session(
-    retries=3,
-    backoff_factor=0.3,
-    status_forcelist=(500, 502, 504),
-    session=None,
-):
-    session = session or requests.Session()
-    retry = Retry(
-        total=retries,
-        read=retries,
-        connect=retries,
-        backoff_factor=backoff_factor,
-        status_forcelist=status_forcelist,
-    )
-    adapter = HTTPAdapter(max_retries=retry)
-    session.mount('http://', adapter)
-    session.mount('https://', adapter)
-    return session
-
-def to_number(value):
-    try:
-        return float(value)
-    except:
-        return value
